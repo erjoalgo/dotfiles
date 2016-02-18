9ec2ea8bb47e39703d2027b50afd91c28104c01c
Ernesto Alfonso <erjoalgo@gmail.com>
2016-02-17 16:40:56 -0800

    simplify quicklisp installation. add make

--- install_stumpwm.py
+++ install_stumpwm.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import sys, pexpect, subprocess, os
 from os.path import join, exists
-from os import chdir
+from os import chdir, mkdir
 
 call = subprocess.call
 # call(["sudo", "apt-get", "install", "-y", "sbcl", "cl-swank", "curl", "autoconf"])
-call(["sudo", "apt-get", "install", "-y", "sbcl", "curl", "autoconf"])
+call(["sudo", "apt-get", "install", "-y", "sbcl", "curl", "autoconf", "make"])
 call("curl -O http://beta.quicklisp.org/quicklisp.lisp".split(" "))
 
 home = os.getenv("HOME")
@@ -34,13 +34,18 @@ sbcl_lines = [
 
 # p = pexpect.spawn("sbcl")
 if not exists(join(programs, "quicklisp")):
-    p = pexpect.spawn("sbcl --load quicklisp.lisp")
-    p.logfile = sys.stdout
-    p.expect(".*")
-    map(p.sendline, sbcl_lines)
+    print ( "installing lisp packages via quicklisp" )
+    call(["sbcl", "--load", "quicklisp.lisp", "--eval", "(progn {})".format(" ".join(sbcl_lines))])
+    # p = pexpect.spawn("sbcl --load quicklisp.lisp")
+    # p.logfile = sys.stdout
+    # p.expect(".*")
+    # map(p.sendline, sbcl_lines)
     call("rm quicklisp.lisp".split(" "))
 
+if not exists(programs):
+    mkdir(programs)
 chdir(programs)
+
 if not exists("stumpwm"):
     call("git clone https://github.com/stumpwm/stumpwm.git".split(" "))
 
