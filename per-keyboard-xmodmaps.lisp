904e09d6e9dd8d3bc243746a1380664f80570287
Ernesto Alfonso <erjoalgo@gmail.com>
2016-02-20 13:12:21 -0800

    simpler xmodmap

--- per-keyboard-xmodmaps.lisp
+++ per-keyboard-xmodmaps.lisp
@@ -1,31 +1,7 @@
-(setq XMODMAP-RULES-FN "per-keyboard-xmodmaps-rules.lisp")
-(setq xmodmap-rules-verbose t)
+(defvar HOSTNAME (string-trim-whitespace (run-shell-command "hostname" t) ))
+(defvar per-keyboard-xmodmap-path
+  (stumpwm-merger (format nil "xmodmap/per-keyboard/~A" HOSTNAME)))
 
-(fset string-trim-whitespace (curry 'string-trim
-				    '(#\Space #\Newline #\Backspace #\Tab 
-				      #\Linefeed #\Page #\Return #\Rubout)))
-
-
-(defun run-xmodmap-subcmd (subcmd)
-  (let* ((cmd (format nil "xmodmap -e '~A'" subcmd)))
-    (when xmodmap-rules-verbose
-      (echo-format "running xmodmap command: ~A" cmd))
-    (run-shell-command cmd)
-    (print cmd)
-    )
-  
-  )
-
-(defun load-xmodmap-rules (key)
-  (let* (
-	 (rules (read-from-string (file-string (stumpwm-merger XMODMAP-RULES-FN))))
-	 (matching (assoc key rules :test 'equal))
-	 )
-    (if matching
-	(mapcar 'run-xmodmap-subcmd (cadr matching))
-	(echo-format "no such entry for ~A" key)
-	))
-  )
-  
-(setq HOSTNAME (string-trim-whitespace (run-shell-command "hostname" t) ))
+(when (probe-file per-keyboard-xmodmap-path)
+  (run-shell-command (format nil "xmodmap ~A" per-keyboard-xmodmap-path)))
 (load-xmodmap-rules HOSTNAME)
