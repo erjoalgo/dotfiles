1eae250bafed5f3d378e566dbbd808fd773990ce
Ernesto Alfonso <erjoalgo@gmail.com>
2016-02-20 13:22:45 -0800

    xmodmap in .xinitrc

--- per-keyboard-xmodmaps.lisp
+++
@@ -1,7 +0,0 @@
-(defvar HOSTNAME (string-trim-whitespace (run-shell-command "hostname" t) ))
-(defvar per-keyboard-xmodmap-path
-  (stumpwm-merger (format nil "xmodmap/per-keyboard/~A" HOSTNAME)))
-
-(when (probe-file per-keyboard-xmodmap-path)
-  (run-shell-command (format nil "xmodmap ~A" per-keyboard-xmodmap-path)))
-(load-xmodmap-rules HOSTNAME)
