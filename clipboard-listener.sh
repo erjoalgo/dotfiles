e824547a701776050f842b7c9e18a6a43b492729
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-13 13:33:36 -0700

    clipboard-listener.sh

---
+++ clipboard-listener.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#listen for incoming text on a given port
+#and paste it to the clipboard
+#use to transfer snippets/urls between machines
+PORT="${1:-1234}"
+while true; do
+    nc -lp "${PORT}" | xsel -ib;
+done
