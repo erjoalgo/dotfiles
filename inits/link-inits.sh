805cf54c8388b20189dc6fefc35ab9fa30951883
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-12 18:25:55 -0800

    fixes to link-inits.sh

--- inits/link-inits.sh
+++ inits/link-inits.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
-STUMPWM_TOP="${HOME}/repos/stumpwm/"
-INITS_TOP="${STUMPWM_TOP}/inits/"
+STUMPWM_TOP="${HOME}/repos/stumpwm"
+INITS_TOP="${STUMPWM_TOP}/inits"
 
-ln -s "${INITS_TOP}/zathurarc" "${HOME}/.config/zathura/"
-ln -s "${INITS_TOP}/roxterm.sourceforge.net" "${HOME}/.config/"
+ln -sf "${INITS_TOP}/zathurarc" "${HOME}/.config/zathura/"
+ln -sf "${INITS_TOP}/roxterm.sourceforge.net" "${HOME}/.config/"
 
-ln -s "${INITS_TOP}/.bash_aliases" "${HOME}/"
-ln -s "${INITS_TOP}/.pythonrc.py" "${HOME}/"
-ln -s "${INITS_TOP}/.inputrc" "${HOME}/" 
-ln -s "${INITS_TOP}/.my_bashrc.sh" "${HOME}/" 
+ln -sf "${INITS_TOP}/.bash_aliases" "${HOME}/"
+ln -sf "${INITS_TOP}/.pythonrc.py" "${HOME}/"
+ln -sf "${INITS_TOP}/.inputrc" "${HOME}/" 
+ln -sf "${INITS_TOP}/.my_bashrc.sh" "${HOME}/" 
+ln -sf "${INITS_TOP}/my_bash_funs.sh" "${HOME}/" 
+
+ln -sf "${INITS_TOP}/.xinitrc" "${HOME}/" 
+ln -sf "${STUMPWM_TOP}/.stumpwmrc" "${HOME}/" 
 
