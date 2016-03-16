7507737ad64e6e4936ddcf72199674c84cb0630f
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-16 16:13:30 -0700

    remove useless funs

--- inits/my_bash_funs.sh
+++ inits/my_bash_funs.sh
@@ -6,11 +6,6 @@ function findiregex {
     find ${dir} -iregex ".*$*.*" 
 }
 
-function re_source {
-    xdotool type 'source ~/.bashrc'
-    xdotool key Return
-}
-
 function abspath {
      echo $(readlink -f "${1}")
 }
@@ -30,9 +25,6 @@ function lnabs_t {
     ln -s -t ${target} "$(readlink -f "${relative}")"
     }
 
-function addsong {
-    lnabs $1 /home/ernesto/Music/phone_music_sync/ 
-}
 function plusx {
     #echo -e $(find $1 -iregex ".*$2.*")
     dest=${HOME}/bin/$(basename $1)
@@ -41,43 +33,11 @@ function plusx {
     chmod +x $1
     #re_source
 }
-function greppysources {
-    find /home/ernesto/Projects/ -iregex '.*py'  | xargs -d'\n' grep $*
-}
-function clip {
-    echo $* | xs
-}
-function addmusic {
-    lnabs $1 /home/ernesto/Music/phone_music_sync/ 
-    }
-function playmusic {
-    findiregex /home/ernesto/Music "$*" | xargsn mpg321
-}
+
 function xargsn {
     xargs -d'\n' -L 1 $*
-    }
-
-function cdls {
-    cd $1 && ls 
-    }
-
-function replace_when_differ {
-    if diff $1 $2;
-    then
-	true
-    else
-	cp $1 $2
-    fi
 }
 
-function add_single_file {
-    if diff $1 $2;
-    then
-	true
-    else
-	cp $1 $2
-    fi
-}
 
 
 #http://superuser.com/questions/150117/how-to-get-parent-pid-of-a-given-process-in-gnu-linux-from-command-line
@@ -90,6 +50,7 @@ function genpasswd {
     [ "$l" == "" ] && l=16;
     tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs;
 }
+
 function test_port	{
     HOST="${1}"
     PORT="${2}"
@@ -98,6 +59,7 @@ function test_port	{
     # cat <&6
     exec 6<>/dev/tcp/"${HOST}/${PORT}"
 }
+
 function straceall {
     set -x
     NAME="${1}"
