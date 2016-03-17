184f528860e8e86400bf4ec2878f46832b0cdd76
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-16 19:56:16 -0700

    un-debug

--- clipboard-listener.sh
+++ clipboard-listener.sh
@@ -4,6 +4,8 @@
 #and paste it to the clipboard
 #use to transfer snippets/urls between machines
 PORT="${1:-1234}"
+echo "listening on port ${PORT}"
 while true; do
     nc -lp "${PORT}" | xsel -ib;
+    echo "got  $(xsel -o)"
 done
