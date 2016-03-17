c6ead6982c11d4473e1e207c559bbfec259dc745
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-16 18:53:11 -0700

    clean up

--- inits/.my_startups.sh
+++ inits/.my_startups.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -x
 
-#exec 2> "${HOME}/logs/my_startups/my_startup"  # send stderr from rc.local to a log file
-#exec 1>&2                      # send stdout to the same log file
-
 echo $$> "${HOME}/.my_startups.pid"
 {
     sleep 30
@@ -11,33 +8,41 @@ echo $$> "${HOME}/.my_startups.pid"
 	sleep 30
     done
 }&
-##/usr/bin/keynav &
-if ! ps ax|grep emacs|grep -v grep; then
-    #emacs --real-session & &>"${HOME}/logs/my_startups/emacs"
-    emacs
-fi
-x-terminal-emulator &
+
+
+
 /usr/lib/notify-osd/notify-osd &
-##${HOME}/Projects/bin/mount_ffram
-xbacklight -set 70
-# "${HOME}/unix_utils/brightnesschange.py" .3 &> "${HOME}/logs/my_startups/brightnesschange"
-# firefox -P default &
+
+emacs &
+x-terminal-emulator &
+##/usr/bin/keynav &
+
+#xbacklight -set 70
 firefox &
+
 sudo modprobe -r pcspkr
-# if ! ping 162.228.201.6 -c 3; then
-if ! wget -S -O - root.erjoalgo.com/test_online; then
+
+#TODO test this works
+INTERNET_TEST="curl http://erjoalgo.com:7036"
+
+if ! ${INTERNET_TEST}; then
     sudo wifi -y -t ac
+    
+    if ! ${INTERNET_TEST}; then
+	sudo dhclient -v eth2
+    fi
+    
 fi
-if ! wget -S -O - root.erjoalgo.com/test_online; then
-    sudo dhclient -v eth2
-fi
-# pidgin&
+
+
+
 rm "${HOME}/.my_startups.pid"
-#extkeyboard.xmodmap
+
 XSCRIPTS="${HOME}/x-scripts.sh"
+
 if test -e "${XSCRIPTS}"; then
     echo "running x scripts at: ${XSCRIPTS}"
     ${XSCRIPTS}
 fi
-notify-send "my_startups.sh done"
 
+notify-send "my_startups.sh done"
