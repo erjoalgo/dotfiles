4d26a7c41dd030d745286ede6e1c16da3ab00a00
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-22 14:33:28 -0700

    verbose flag

--- clipboard-listener.sh
+++ clipboard-listener.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+while getopts "v" OPT; do
+    case ${OPT} in
+	v)
+	    VERBOSE=true
+	    ;;
+    esac
+done
 #listen for incoming text on a given port
 #and paste it to the clipboard
 #use to transfer snippets/urls between machines
@@ -7,7 +14,9 @@ PORT="${1:-1234}"
 echo "listening on port ${PORT}"
 while true; do
     MESSAGE=$(nc -lp "${PORT}")
-    #echo "got  "${MESSAGE}""
+    if test -n "${VERBOSE}"; then
+	echo "got  ${MESSAGE}"
+    fi
     echo "${MESSAGE}" | xsel -ip;
     #echo "last clipboard:  $(xsel -o)"
 done
