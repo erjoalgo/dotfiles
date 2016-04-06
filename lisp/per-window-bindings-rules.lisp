c11ddf59964d9ffe7bcd3c0588c7ed797a3509a1
Ernesto Alfonso <erjoalgo@gmail.com>
2016-04-06 12:25:09 -0700

    setf *per-window-bindings-rules*

--- lisp/per-window-bindings-rules.lisp
+++ lisp/per-window-bindings-rules.lisp
@@ -1,4 +1,5 @@
-`(
+(setf *per-window-bindings-rules*
+      `(
   ;;(("Iceweasel" "Navigator" "Firefox" www-browser "Chromium")
   (,*browser-classes*
    ("F2" (send-fake-key (current-window) (kbd "C-TAB")))
@@ -49,4 +50,4 @@
   ;;("SunPageDown" (send-fake-key (current-window) (kbd "S-SunPageUp")) (run-shell-command "xdotool key shift+Next"))
   ;;("F1" (send-fake-key (current-window) (kbd "C-ISO_Left_Tab")))
   ;;("F4" (send-fake-key (current-window) (kbd "C-F4")))
-  )
+  ))
