33f957a8c1a79ad1e295d55aec0685b7bb097a86
Ernesto Alfonso <erjoalgo@gmail.com>
2017-01-03 12:49:02 -0800

    send-mail-on-ssh-login.sh: pam script to send mail on ssh login

---
+++ send-mail-on-ssh-login.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+# EMAIL=${1:-erjoalgo@gmail.com}
+EMAIL=${1}
+test -n "${EMAIL}" || exit ${LINENO}
+
+if test "$PAM_TYPE" = "open_session" ; then
+   SUBJECT="$PAM_SERVICE login on $(hostname -s) for account $PAM_USER"
+   cat <<EOF | mutt -s "${SUBJECT}" "${EMAIL}"
+        User: $PAM_USER
+        Remote Host: $PAM_RHOST
+        Service: $PAM_SERVICE
+        TTY: $PAM_TTY
+        Date: $(date)
+        Server: $(uname -a)
+EOF
+fi
+# add the following line to /etc/pam.d/sshd
+# session optional pam_exec.so /usr/local/bin/send-mail-on-ssh-login.sh erjoalgo@gmail.com
