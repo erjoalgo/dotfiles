137d7a0f0905a010bd32b3114c2cc74be08a37e5
Ernesto Alfonso <erjoalgo@gmail.com>
2017-04-09 19:15:06 -0700

    add move-last-n.sh

---
+++ move-last-n.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+DOWNLOADS="${HOME}/Downloads"
+DEST="${1}"
+LAST_N_MINS=5
+
+if test -z "${DEST}"; then
+    echo "usage: move-last-n.sh DEST"
+    exit ${LINENO}
+fi
+
+for FILE in $(find "${DOWNLOADS}" -cmin -${LAST_N_MINS} -type f); do
+    BASE=$(basename "${FILE}")
+    mv -t "${DEST}" "${FILE}"
+    FN="${DEST}/${BASE}"
+    FN_EXT="${FN##*.}"
+    for EXT_PROGRAM in \
+	pdf,zathura \
+	    jpg,eog\
+	; do
+	IFS=","
+	set -- $EXT_PROGRAM
+	EXT="${1}"
+	PROGRAM="${2}"
+	if test "${EXT}" = "${FN_EXT}"; then
+	    ${PROGRAM} "${FN}"
+	    break
+	fi
+    done
+done
