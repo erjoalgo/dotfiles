d8e2791ef389021429083df8d8ee220fe48b40fd
vagrant <ernesto.alfonsogonzalez@ge.com>
2017-04-24 09:51:37 -0700

    git-https-to-ssh: {BRANCH,REMOTE}

--- git-https-to-ssh.sh
+++ git-https-to-ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 # echo 'https://github.com/erjoalgo/localForage.git' |
-BRANCH="${1:-origin}"
-SSH_URL=$(git remote -vv | grep "${BRANCH}" | head -1 | awk '{print $2}' | sed 's,https://,ssh://git@,')
-git remote set-url "${BRANCH}" "${SSH_URL}"
+REMOTE="${1:-origin}"
+SSH_URL=$(git remote -vv | grep "${REMOTE}" | head -1 | awk '{print $2}' | sed 's,https://,ssh://git@,')
+git remote set-url "${REMOTE}" "${SSH_URL}"
