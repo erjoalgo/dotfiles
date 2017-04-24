ae7caa45ced93a479c5dd2f9da59b56be2a9518d
vagrant <ernesto.alfonsogonzalez@ge.com>
2017-04-24 09:54:01 -0700

    git-https-to-ssh: keep https-origin

--- git-https-to-ssh.sh
+++ git-https-to-ssh.sh
@@ -2,4 +2,5 @@
 # echo 'https://github.com/erjoalgo/localForage.git' |
 REMOTE="${1:-origin}"
 SSH_URL=$(git remote -vv | grep "${REMOTE}" | head -1 | awk '{print $2}' | sed 's,https://,ssh://git@,')
-git remote set-url "${REMOTE}" "${SSH_URL}"
+git remote rename "${REMOTE}" "https-origin"
+git remote add "${REMOTE}" "${SSH_URL}"
