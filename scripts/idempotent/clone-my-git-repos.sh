1ee36c4bfc5251a5d58f0469327dfc30f102de38
vagrant <ernesto.alfonsogonzalez@ge.com>
2017-05-03 19:37:38 -0700

    add idempotent/clone-my-git-repos.sh

---
+++ scripts/idempotent/clone-my-git-repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+REPOS="${HOME}git"
+if test -d "${REPOS}"; then
+    mkdir "${REPOS}"
+fi
+
+for REPO in \
+    erjoalgo-stumpwmrc \
+    erjoalgo-gnus-scripts \
+    dotemacs \
+    erjoalgo-firefox-addons \
+    erjoalgo-vimfx-config \
+    ;do
+
+    test -d ${REPO} ||  \
+	git clone "https://github.com/erjoalgo/${REPO}" || exit ${LINENO}
+
+done
