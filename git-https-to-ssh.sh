d9082c47c24ca5f44b111e5076ca543444ce51df
Ernesto Alfonso <erjoalgo@gmail.com>
2017-05-04 06:05:14 -0700

    git-https-to-ssh: don't break if http

--- git-https-to-ssh.sh
+++ git-https-to-ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 # echo 'https://github.com/erjoalgo/localForage.git' |
 REMOTE="${1:-origin}"
-SSH_URL=$(git remote -vv | grep "${REMOTE}" | head -1 | awk '{print $2}' | sed 's,https://,ssh://git@,')
+SSH_URL=$(git remote -vv | grep "${REMOTE}" | head -1 | awk '{print $2}' | sed 's,https*://,ssh://git@,')
 git remote rename "${REMOTE}" "https-origin"
 git remote add "${REMOTE}" "${SSH_URL}"
