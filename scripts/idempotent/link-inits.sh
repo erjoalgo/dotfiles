e076c7f6165c9e180252e14efa6150888448c79f
vagrant <ernesto.alfonsogonzalez@ge.com>
2017-05-03 19:45:12 -0700

    idempotent/link-inits: make more concise

--- scripts/idempotent/link-inits.sh
+++ scripts/idempotent/link-inits.sh
@@ -3,26 +3,27 @@
 STUMPWM_TOP=$(dirname $(dirname $(readlink -f $0)))
 INITS_TOP="${STUMPWM_TOP}/inits"
 
+for LINK in \
+    .bash_aliases \
+    pythonrc.py \
+    inputrc \
+    my-bashrc \
+    my-bash-funs \
+    xmodmap \
+    xinitrc \
+    bash_profile \
+    my-startups.sh \
+    my-bash-completions \
+    gitconfig \
+    ; do \
+
+    ln -sf "${INITS_TOP}/${LINK}" "${HOME}/"
+
+done
+    
 ln -sf "${INITS_TOP}/zathurarc" "${HOME}/.config/zathura/"
 ln -sf "${INITS_TOP}/roxterm.sourceforge.net" "${HOME}/.config/"
-
-ln -sf "${INITS_TOP}/.bash_aliases" "${HOME}/"
-ln -sf "${INITS_TOP}/.pythonrc.py" "${HOME}/"
-ln -sf "${INITS_TOP}/.inputrc" "${HOME}/" 
-ln -sf "${INITS_TOP}/.my-bashrc" "${HOME}/" 
-ln -sf "${INITS_TOP}/.my-bash-funs" "${HOME}/" 
-ln -sf "${INITS_TOP}/.xmodmap" "${HOME}/" 
-
-
-ln -sf "${INITS_TOP}/.xinitrc" "${HOME}/" 
-ln -sf "${INITS_TOP}/.bash_profile" "${HOME}/" 
-ln -sf "${INITS_TOP}/.my-startups.sh" "${HOME}/" 
-
-ln -sf "${INITS_TOP}/.my-bash-completions" "${HOME}"
-ln -s "${INITS_TOP}/.gitconfig" "${HOME}"
-
 ln -sf "${INITS_TOP}/keynavs/.keynavrc.mine" "${HOME}/.keynavrc"
-
 ln -sf "${STUMPWM_TOP}/lisp/.stumpwmrc" "${HOME}/"
 ln -sf "${HOME}/git/dotemacs/lisp/.emacs" "${HOME}/"
 
