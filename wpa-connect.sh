d3ad768e253723add8d8f59209f380787e4531ae
Ernesto Alfonso <erjoalgo@gmail.com>
2017-05-05 11:27:18 -0700

    wpa-connect: rename {NETWORK,ESSID}

--- wpa-connect.sh
+++ wpa-connect.sh
@@ -12,8 +12,8 @@ if test 0 -ne $? || test -z ${IFACE}; then
 fi
 
 IWLIST_OUT=$(sudo iwlist ${IFACE} scan)
-NETWORKS=$(grep ESSID <<< "${IWLIST_OUT}" | sed 's/.*ESSID:"\(.*\)".*/\1/g')
-if test 0 -ne $? || test -z ${NETWORKS}; then
+ESSIDS=$(grep ESSID <<< "${IWLIST_OUT}" | sed 's/.*ESSID:"\(.*\)".*/\1/g')
+if test 0 -ne $? || test -z ${ESSIDS}; then
     sudo iwlist ${IFACE} scan
     echo "couldn't scan for wireless networks" && exit ${LINENO}
 fi
@@ -22,37 +22,37 @@ NETWORKS_DIR=/tmp/wpa
 test -d ${NETWORKS_DIR} || mkdir -p ${NETWORKS_DIR}
 cd ${NETWORKS_DIR}
 
-COUNT=$(wc -l <<< "${NETWORKS}")
+COUNT=$(wc -l <<< "${ESSIDS}")
 case ${COUNT} in
     1)
-	NETWORK="${NETWORKS}"
+	ESSID="${ESSIDS}"
 	;;
     0)
 	echo "no networks found" && exit ${LINENO}
 	;;
     *)
 	IFS=$'\n'
-	select NETWORK in ${NETWORKS}; do
+	select ESSID in ${ESSIDS}; do
 	    break
 	done
 esac
 
-echo "selected ${NETWORK}"
-ENC=$(grep -B1 -F "${NETWORK}" <<< "${IWLIST_OUT}"  | head -1 \
+echo "selected ${ESSID}"
+ENC=$(grep -B1 -F "${ESSID}" <<< "${IWLIST_OUT}"  | head -1 \
 	 | sed 's/.*Encryption key:\(.*\)/\1/')
 
 
 case "${ENC}" in
     on) #assume wpa
-	read -p "enter password for ${NETWORK}: " PASS
-	wpa_passphrase "${NETWORK}" "${PASS}" > "${NETWORK}"
+	read -p "enter password for ${ESSID}: " PASS
+	wpa_passphrase "${ESSID}" "${PASS}" > "${ESSID}"
 	
 	sudo pkill -e wpa_supplicant
 	sudo pkill -e dhclient
 
 	cat <<EOF | expect -df -
 set timeout -1
-eval spawn sudo wpa_supplicant -i ${IFACE} -c ${NETWORK} -D nl80211,wext &
+eval spawn sudo wpa_supplicant -i ${IFACE} -c ${ESSID} -D nl80211,wext &
 # expect "Established DTLS connection"
 # expect -re "CTRL-EVENT-CONNECTED - Connection to 00:1a:1e:87:3c:01 completed"
 # expect -re "CTRL-EVENT-CONNECTED - Connection to [0-9a-z:]+ completed"
@@ -61,14 +61,14 @@ disconnect
 exit
 
 EOF
-	# sudo wpa_supplicant -i ${IFACE} -c ${NETWORK} -D nl80211,wext &
+	# sudo wpa_supplicant -i ${IFACE} -c ${ESSID} -D nl80211,wext &
 	# sleep 10
 
 	echo "post CTRL-EVENT-CONNECTED expect"
 	;;
 
     off)
-	sudo iwconfig ${IFACE} essid "${NETWORK}" || exit ${LINENO}
+	sudo iwconfig ${IFACE} essid "${ESSID}" || exit ${LINENO}
 	;;
     *)
 	echo "unknown encrption ${ENC}" && exit ${LINENO}
