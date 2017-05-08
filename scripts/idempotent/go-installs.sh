b4ca475704dc6bf447a3f19c1b9312f6e4ca8701
Ernesto Alfonso <erjoalgo@gmail.com>
2017-05-08 11:52:15 -0700

    go-installs: maybe install go itself

--- scripts/idempotent/go-installs.sh
+++ scripts/idempotent/go-installs.sh
@@ -1,7 +1,23 @@
 #!/bin/bash -x
 
+
 if ! command -v go; then
-    echo "go isn't installed" && exit "${LINENO}"
+    GO_URL=https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz
+    cd ~/Downloads || exit ${LINENO}
+    if ! test -f $(basename ${GO_URL}); then
+	wget "${GO_URL}" || exit ${LINENO}
+    fi
+    FNAME=$(basename ${GO_URL})
+    INSTALL_DIR=/usr/local
+    DNAME=$(basename ${FNAME} .tar.gz)
+
+    test -d ${INSTALL_DIR}/go ||  \
+	sudo tar -C ${INSTALL_DIR} -xzf ${FNAME} || exit ${LINENO}
+
+    test -d ${INSTALL_DIR}/go || exit ${LINENO}
+
+    export PATH=${PATH}:${INSTALL_DIR}/go/bin
+    go version || exit ${LINENO}
 fi
 
 for REPO in\
