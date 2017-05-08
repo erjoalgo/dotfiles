3f681bcb4304dd2705eb9d4819f9a458d542f2b4
Ernesto Alfonso <erjoalgo@gmail.com>
2017-05-07 20:50:12 -0700

    add git-notify

---
+++ git-notify.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# send mail to the current user in case there exist no remote
+# for which the current branch of this repo is up to date
+
+# instead of sending mail, simply output those 'ahead' repos
+# let cron send the mail automatically
+# 30  7	* * *	root	~ealfonso/git/erjoalgo-gnu-scripts/git-notify.sh ~ealfonso/git/*
+
+
+while test $# -gt 0; do
+    REPO=${1}
+    shift
+
+    cd "${REPO}" || exit ${LINENO}
+
+    # if ! git status &> /dev/null; then
+    if ! test -d .git; then
+	echo "not at a git repo: ${REPO}" && exit ${LINENO}
+    fi
+
+    BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+
+    if test $? -ne 0 || test $(git branch -r --contains ${BRANCH} | wc -l) -eq 0; then
+	# echo mail -s "git-notify: ${REPO}/${BRANCH} might be ahead of all remotes" $(whoami)
+	echo ${REPO}
+    fi
+    # read
+done
+
