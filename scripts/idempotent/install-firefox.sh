48fa8e6298f47b1a520c1592c7d46f9cec66e9dc
Ernesto Alfonso <erjoalgo@gmail.com>
2017-05-16 13:32:54 -0700

    install-firefox: downgrade from ff 52
    which breaks ALSA

--- scripts/idempotent/install-firefox.sh
+++ scripts/idempotent/install-firefox.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -x
 
+# 'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US'
+# URL="https://download.mozilla.org/?product=firefox-52.1.1esr-SSL&os=linux64&lang=en-US"
+URL="https://download.mozilla.org/?product=firefox-45.9.0esr-SSL&os=linux64&lang=en-ZA"
 cd /tmp/
 FFTAR="firefox.tar.bz2"
 FF=$(basename "${FFTAR}" .tar.bz2)
 
-if ! test -f ${FFTAR}; then
-    sudo wget 'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US' -O "${FFTAR}" || exit ${LINENO}
+if test -f ${FFTAR}; then
+    rm -f ${FFTAR} || exit ${LINENO}
+    true
 fi
+
+wget "${URL}" -O "${FFTAR}" || exit ${LINENO}
 
 INSTALL_DIR=/usr/local/${FF}
 
-if ! test -d ${INSTALL_DIR}; then
-    sudo tar -C -axvf "${FFTAR}" || exit ${LINENO}
+if test -d ${INSTALL_DIR}; then
+    test -n "${FF}" || exit ${LINENO}
+    sudo rm -rf /usr/local/${FF}
 fi
 
+sudo tar -C /usr/local -axvf "${FFTAR}" || exit ${LINENO}
 cd  || exit ${LINENO}
 
 
