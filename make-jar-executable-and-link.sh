511d93be40be9aaf0022180d04d22115acb9fb3c
Ernesto Alfonso <erjoalgo@gmail.com>
2017-06-10 01:26:25 -0700

    new file:   make-jar-executable-and-link.sh easily put a jar into the PATH

---
+++ make-jar-executable-and-link.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+JAR=$(realpath "${1}")
+grep "[.]jar$" <<< "${JAR}" || exit ${LINENO}
+EXE="${JAR}.sh"
+cat /dev/stdin "${JAR}" <<'EOF' > "${EXE}"
+#!/bin/sh
+#https://coderwall.com/p/ssuaxa/how-to-make-a-jar-file-linux-executable
+MYSELF=$(which "$0" 2>/dev/null)
+[ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
+test -n "$JAVA_HOME" && JAVA="$JAVA_HOME/bin/java"
+JAVA=${JAVA:-java}
+exec "$JAVA" $java_args -jar $MYSELF "$@"
+exit 1 
+EOF
+
+chmod +x "${EXE}"
+BINDIR=${2:-/usr/local/bin}
+sudo ln -sf "${EXE}" "${BINDIR}"
+echo "${BINDIR}/$(basename ${EXE})"
