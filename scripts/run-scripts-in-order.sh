2f4eb7edd9aaa8ae1da11582e0be8e10e98ca088
Ernesto Alfonso <erjoalgo@gmail.com>
2017-07-11 00:13:56 -0700

    run-scripts-in-order more generic

--- scripts/run-scripts-in-order.sh
+++ scripts/run-scripts-in-order.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-DEFAULT_INSTALLS=$(dirname $(readlink -f $0))
-cd ${DEFAULT_INSTALLS}
+SELF=$(readlink -f $0)
+SELF_D=$(dirname "${SELF}")
+SCRIPTS_D=${1:-$SELF_D}
+SHORT_CIRCUIT=${SHORT_CIRCUIT:-true}
 
-pwd
-for SCRIPT in $(find . -type l | sort); do
-    echo "running ${SCRIPT}"
+cd ${SCRIPTS_D} || exit ${LINENO}
+echo "running scripts in ${SCRIPTS_D}"
+
+for SCRIPT in $(find -L . -maxdepth 1 -type f -not -samefile "${0}"| sort); do
+    echo "running ${SCRIPT}..."
     ${SCRIPT}
+    continue
     if test 0 -ne $?; then
-	 echo "script ${SCRIPT} failed" && exit ${LINENO}
+	echo "script ${SCRIPT} failed!"
+	test $SHORT_CIRCUIT != true || exit ${LINENO}
     fi
 done
+
 echo "all scripts ran successfully"
