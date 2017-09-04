2639e151f2c74aa5e1c93f0a4b3b53748de9ca25
Ernesto Alfonso <erjoalgo@gmail.com>
2017-09-03 19:38:33 -0700

    ls-recursive-last-modified: add attribution

--- ls-recursive-last-modified.sh
+++ ls-recursive-last-modified.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# https://superuser.com/questions/416308/how-to-list-files-recursively-and-sort-them-by-modification-time
 find . -type f -exec stat --printf "%Y\t%y %N\n" '{}' \;  \
     | sort -rn  | cut -f2-
