014d5aa0da3dce137672749125ca0c6bc638921c
Ernesto Alfonso <erjoalgo@gmail.com>
2017-09-26 15:45:04 -0700

    defcommands: add ocr-scrot-clipboard
    take a scrot, ocr it, place resulting text in clipboard

--- lisp/defcommands.lisp
+++ lisp/defcommands.lisp
@@ -152,6 +152,26 @@ be used to override the default window formatting."
 (defcommand scrot-cmd-full-screen (name)
     ((:string "enter name for scrot: "))
   (take-scrot name :fullscreen-p t))
+
+(defun image-fn-to-text (image-fn)
+  (let ((cmd (format nil "tesseract ~A -" image-fn)))
+    (run-shell-command cmd t)))
+
+
+(defcommand ocr-scrot-clipboard () ()
+  "interactively prompt for a region of the screen, take a screenshot,
+perform ocr on it, place ocr'd text into clipboard"
+  (let* ((ocr-name "last-ocr")
+	 (ocr-png-filename (take-scrot ocr-name
+				       :fullscreen-p nil
+				       :verbose nil
+				       :eog-scrot nil))
+	 (ocr-text (image-fn-to-text ocr-png-filename)))
+    (set-x-selection ocr-text :clipboard)
+    (message "copied ocr of length ~D to clipboard..."
+	     (length ocr-text))))
+
+
 (defun take-scrot (name &key
 			  (fullscreen-p nil)
 			  (scrot-top *scrots-top*)
