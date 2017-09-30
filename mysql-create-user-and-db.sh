809bf922ba95064ffbfd2c67e9126a02a5ae3e3c
Ernesto Alfonso <erjoalgo@gmail.com>
2017-09-29 17:33:03 -0700

    mysql-create-user-and-db: separate ADMINPASS from PASS

--- mysql-create-user-and-db.sh
+++ mysql-create-user-and-db.sh
@@ -5,8 +5,8 @@ USER=${1:-${DBNAME}} && shift
 HOST=${1:-localhost} && shift
 
 
-echo 'enter password for "${USER}"' -n
-read -s PASS
+echo 'enter mysql password for root' -n
+read -s ADMINPASS
 echo
 
 if ! test -n "${DBNAME}" -a -n "${USER}" -a -n "${PASS}" -a -n "${HOST}"; then
@@ -17,24 +17,37 @@ if grep '-' <<< "${DBNAME}"; then
     echo "dbname cannot have hyphens" && exit ${LINENO}
 fi
 
+DROP=${DROP:-}
+
 if test -n "${DROP}"; then
-    mysql -uroot -pcontra <<EOF
+    mysql -uroot -p${ADMINPASS} <<EOF
+GRANT USAGE ON *.* TO '${USER}'@'localhost';
 drop USER '${USER}'@'localhost';
-drop database ${DBNAME};
+drop database if exists ${DBNAME};
 FLUSH PRIVILEGES;
 EOF
-fi
+    # make sure we can't log in
+    # mysql -u${USER} -p${PASS} < /dev/null && test 0 -ne $?
+    echo successful drop
+else
+    # cat <<EOF
+    echo 'enter mysql password for "${USER}"' -n
+    read -s PASS
+    echo
 
-
-
-# cat <<EOF
-mysql -uroot -pcontra <<EOF
+    mysql -uroot -p${ADMINPASS} <<EOF
 CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
 CREATE DATABASE ${DBNAME};
 GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${USER}'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
+    mysql -u${USER} -p${PASS} < /dev/null
+    cat <<EOF
+dbname: ${DBNAME}
+username: ${USER}
+EOF
+fi
 # Local Variables:
 # compile-command: "./mysql-create-user-and-db.sh piwigokorea"
 # End:
