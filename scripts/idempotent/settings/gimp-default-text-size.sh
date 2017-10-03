c5e0e1688c0394240f11f8b8825eb5db4719dd86
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-02 19:47:13 -0700

    scripts/gimp-default-text-size: shorten to a single command

--- scripts/idempotent/settings/gimp-default-text-size.sh
+++ scripts/idempotent/settings/gimp-default-text-size.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
-FN="${HOME}/.gimp-2.8/tool-options/gimp-text-tool"
-test -f "${FN}" || exit ${LINENO}
-grep "(font-size " "${FN}" && exit 0
+set -euo pipefail
 
-cat <<EOF >> "${FN}"
+insert-text-block '# 2b024c6e-f750-4cb0-97a8-6bab3d9b05ba-gimp-set-default-text-size' \
+		  "${HOME}/.gimp-2.8/tool-options/gimp-text-tool" <<EOF
 # GIMP gimp-text-tool options
-
 (palette "Basic")
 (font "Gilgongo")
 (font-size 60.000000)
-
 # end of gimp-text-tool options
 EOF
