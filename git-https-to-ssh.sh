de8e5fe4985f8ee3dd08d78d121c58267d893aab
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-03 16:48:00 -0700

    git-https-to-ssh: set upstream to the new ssh remote

--- git-https-to-ssh.sh
+++ git-https-to-ssh.sh
@@ -4,3 +4,5 @@ REMOTE="${1:-origin}"
 SSH_URL=$(git remote -vv | grep "${REMOTE}" | head -1 | awk '{print $2}' | sed 's,https*://,ssh://git@,')
 git remote rename "${REMOTE}" "${REMOTE}-https"
 git remote add "${REMOTE}" "${SSH_URL}"
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git branch --set-upstream-to=${REMOTE}/${BRANCH}
