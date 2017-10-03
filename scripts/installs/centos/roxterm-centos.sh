361edf67d68577f8f58c49afd006b41493604a25
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-03 15:08:13 -0700

    add scripts/installs/roxterm-centos
    delete old attempt to build from source

---
+++ scripts/installs/centos/roxterm-centos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+if ! test -f /etc/yum.repos.d/gf.repo; then
+    wget http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7.noarch.rpm
+    sudo rpm -Uvh gf-release*rpm
+fi
+
+sudo yum --enablerepo=gf-plus install -y roxterm
+
--- scripts/installs/centos/roxterm-install.sh
+++
@@ -1,6 +0,0 @@
-
-
-
-sudo pip install lockfile
-sudo yum install -y python-lockfile
-sudo yum install -y po4a itstool vte291{,-devel}
