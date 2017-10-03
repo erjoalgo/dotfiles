ca5c9d1ce88959631b6c4a1998e322126f3fdec9
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-02 20:05:22 -0700

    scripts: consolidate too many scripts into boot.sh

---
+++ scripts/boot.sh
@@ -0,0 +1,83 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+test 0 -ne "${EUID}"
+
+# get sudo, git and curl
+APT_GET=$(which apt-get yum 2>/dev/null) || true
+SUDOCMD="su -c"
+if which sudo && SUDO_ASKPASS=$(which false) sudo true; then
+    SUDOCMD="sudo"
+fi
+
+${SUDOCMD} ${APT_GET} install -y git sudo curl
+
+# set up passwordless sudo
+LINE="${USER} ALL=(ALL:ALL) NOPASSWD:ALL"
+grep -F "${LINE}" /etc/sudoers || ${SUDOCMD} tee -a /etc/sudoers <<< "${LINE}"
+
+# fetch my git repos
+GIT_HOME=${HOME}/git
+mkdir -p ${GIT_HOME}
+cd ${GIT_HOME}
+for REPO in erjoalgo-stumpwmrc \
+		erjoalgo-gnu-scripts \
+		dotemacs \
+		erjoalgo-firefox-addons \
+		erjoalgo-vimfx-config \
+	    ; do
+    test -d ${REPO} || git clone "https://github.com/erjoalgo/${REPO}"
+done
+
+export PATH=$PATH:${GIT_HOME}/erjoalgo-gnu-scripts
+
+ADDBLOCK=${GIT_HOME}/erjoalgo-gnu-scripts/insert-text-block
+# sudo ln -sf  /usr/local/bin
+# sudo which insert-text-block
+
+# set default cmd line editor to vi
+sudo update-alternatives --set editor /usr/bin/vim.tiny --verbose || true
+# set firefox-new-tab.sh as browse command
+FIREFOX_NEW_TAB=$(which firefox-new-tab)
+sudo update-alternatives --install $(which x-www-browser) \
+     x-www-browser ${FIREFOX_NEW_TAB} 200 || true
+sudo update-alternatives --set x-www-browser ${FIREFOX_NEW_TAB} || true
+
+git config --global user.email "erjoalgo@gmail.com"
+git config --global user.name "Ernesto Alfonso"
+
+
+insert-text-block '# bbdede6e-87c5-4ba9-927e-78865afb3dcb-source-my-bashrc'  \
+		  ${HOME}/.bashrc <<<"source ${HOME}/.my-bashrc"
+sed -i '/^HIST\(FILE\)\?SIZE=[0-9]*/d' "${HOME}/.bashrc"
+
+# autologin to stumpwm on tty1
+AUTOLOGIN_CONF="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+sudo mkdir -p $(dirname "${AUTOLOGIN_CONF}")
+sudo ${ADDBLOCK} '# e8a6c230-997f-4dd5-9b57-7e3b31ab67bc'  \
+     "${AUTOLOGIN_CONF}" <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin "${USER}" %I
+EOF
+
+# set GRUB timeout to zero
+GRUB_FILE=/etc/default/grub
+if test -e ${GRUB_FILE} &&  \
+	grep -P '^GRUB_TIMEOUT=[0-9]+$' ${GRUB_FILE} | grep -v '=0'; then
+    sudo sed -i 's/^GRUB_TIMEOUT=[0-9]*$/GRUB_TIMEOUT=0/' ${GRUB_FILE}
+    sudo update-grub
+fi
+
+# link inits, install stumpwm
+cd ~/git/erjoalgo-stumpwmrc/scripts/idempotent
+for SCRIPT in link-inits.sh\
+	      ;do
+    # install-stumpwm.sh
+    ./${SCRIPT}
+done
+
+# some essential scripts
+sudo ${APT_GET} install -y htop auditd fail2ban bootlogd
+sudo apt-get install -y apt-file && sudo apt-file update
