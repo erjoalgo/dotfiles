bb2dffac6a60734df198c08028f72f9744ece471
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-03 01:15:03 -0700

    fix install-stumpwm.sh
    use bash strict mode
    fix indentation
    remove "|| exit ${LINENO}"
    use https quicklisp
    verify signature (redundant)?
    don't rely on (ql:add-to-init-file),
     which cannot be non-interactive (report bug)
    add new alexandria dep
    change build dir to ~/.local
    don't prompt before building
    system-wide sudo make install
    fix yum install command
    move tty1 auto-login code here from boot

--- scripts/installs/install-stumpwm.sh
+++ scripts/installs/install-stumpwm.sh
@@ -1,32 +1,43 @@
-#!/bin/bash
+#!/bin/bash -x
+
+set -euo pipefail
 
 
 APT_GET="apt-get"
 if command -v yum; then
-	APT_GET="yum"
+    APT_GET="yum"
 fi
 
-sudo ${APT_GET} install -y sbcl curl autoconf make texinfo rlwrap || exit ${LINENO}
+sudo ${APT_GET} install -y sbcl curl autoconf make texinfo rlwrap
 
 sudo ${APT_GET} install -y xinit x11-xserver-utils \
-     xbacklight xcalib xsel upower xscreensaver || exit ${LINENO}
+     xbacklight xcalib xsel upower xscreensaver
 
-if command -v stumpwm; then
-    echo "stumpwm already installed"
-    exit 0
+QL_SETUP="${HOME}/quicklisp/setup.lisp"
+if ! test -f "${QL_SETUP}"; then
+    curl -O https://beta.quicklisp.org/quicklisp.lisp
+    curl -O https://beta.quicklisp.org/quicklisp.lisp.asc
+    gpg --verify quicklisp.lisp.asc quicklisp.lisp
+    sbcl --load quicklisp.lisp --non-interactive --eval  \
+	'(quicklisp-quickstart:install)'
+    rm quicklisp.lisp{,.asc}
 fi
 
+ADDBLOCK=${HOME}/git/erjoalgo-gnu-scripts/insert-text-block
 
 SBCLRC="${HOME}/.sbclrc"
-test -f "${SBCLRC}" && \
-    sbcl --load "${SBCLRC}" --script /dev/stdin <<< '(ql:quickload "cl-ppcre")'
-
-if ! test $? -eq 0; then
-    curl -O http://beta.quicklisp.org/quicklisp.lisp || exit ${LINENO}
-    sbcl --load quicklisp.lisp --script  \
-	 /dev/stdin <<< '(quicklisp-quickstart:install) (ql:add-to-init-file)' \
-	|| exit ${LINENO}
-    rm quicklisp.lisp
+if test ! -e "${SBCLRC}"; then
+    # sbcl --load "${QL_SETUP}" --non-interactive --eval '(ql:add-to-init-file)' \
+    # 	< /dev/null
+    ${ADDBLOCK} ";;; 2a00bf58-9854-4512-8e13-a85409493a54-ql:add-to-init-file-manual" \
+	"${SBCLRC}"<<EOF
+  ;;; The following lines added by ql:add-to-init-file:
+  #-quicklisp
+  (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                         (user-homedir-pathname))))
+    (when (probe-file quicklisp-init)
+      (load quicklisp-init)))
+EOF
 fi
 
 sbcl --load "${SBCLRC}" --script /dev/stdin <<EOF
@@ -37,31 +48,41 @@ sbcl --load "${SBCLRC}" --script /dev/stdin <<EOF
 "swank"
 "quicklisp-slime-helper"
 "usocket"
+"alexandria"
 ))
 EOF
 
-PROGRAMS="${HOME}/programs"
-test -d "${PROGRAMS}" || mkdir "${PROGRAMS}"
-cd "${PROGRAMS}"
+mkdir -p "${HOME}/.local"
+cd "${HOME}/.local"
 
 if ! test -d stumpwm; then
-    git clone https://github.com/stumpwm/stumpwm.git || exit ${LINENO}
+    git clone https://github.com/stumpwm/stumpwm.git
 fi
 
-STUMPWM="${PROGRAMS}/stumpwm"
+STUMPWM="$(pwd)/stumpwm"
 cd stumpwm
 
 if ! command -v stumpwm; then #the executable
-    read -p "confirm make in ${PWD}: "
-    ./autogen.sh || exit ${LINENO}
-    ./configure || exit ${LINENO} 
-    make || exit ${LINENO}
+    ./autogen.sh
+    ./configure
+    make
     test -d ~/bin || mkdir ~/bin
-    sudo ln -s "${STUMPWM}/stumpwm" /usr/local/bin
+    sudo make install
+    # sudo ln -sf "${STUMPWM}/stumpwm" /usr/local/bin
 fi
 
 if command -v yum; then
-   yum groupinstall "X Window Server";
+   sudo yum groupinstall "X Window System";
 fi
 
 
+# autologin to stumpwm on tty1
+ADDBLOCK=${HOME}/git/erjoalgo-gnu-scripts/insert-text-block
+AUTOLOGIN_CONF="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+sudo mkdir -p $(dirname "${AUTOLOGIN_CONF}")
+sudo ${ADDBLOCK} '# e8a6c230-997f-4dd5-9b57-7e3b31ab67bc'  \
+     "${AUTOLOGIN_CONF}" <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin "${USER}" %I
+EOF
