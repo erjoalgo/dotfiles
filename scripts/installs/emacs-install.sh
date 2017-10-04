46f3707bf044c7bdde44b331c857477b1d782eca
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-03 22:44:50 -0700

    emacs-install: use bash strict mode
    delete || exit $LINENO spam

--- scripts/installs/emacs-install.sh
+++ scripts/installs/emacs-install.sh
@@ -1,17 +1,14 @@
 #!/bin/bash -x
 
+set -euo pipefail
+
 URL=http://gnu.mirrors.pair.com/gnu/emacs/emacs-25.2.tar.gz
 
 FNAME=$(basename ${URL})
 cd ~/Downloads
 
-if ! test -f ${FNAME}; then
-    wget ${URL} || exit ${LINENO}
-fi
-
-if ! test -f ${FNAME}.sig; then
-    wget ${URL}.sig || exit ${LINENO}
-fi
+test -f ${FNAME} || wget ${URL}
+test -f ${FNAME}.sig || wget ${URL}.sig
 
 # gpg --recv-keys BE216115
 
@@ -19,26 +16,25 @@ INSTALL_DIR=/usr/local
 DNAME=$(basename ${FNAME} .tar.gz)
 
 if ! test -d ${INSTALL_DIR}/${DNAME}; then
-    gpg --recv-keys 7C207910 || exit ${LINENO}
-    gpg --verify ${FNAME}{.sig,} || exit ${LINENO}
-    sudo tar -C ${INSTALL_DIR} -xvf ${FNAME} || exit ${LINENO}
+    gpg --recv-keys 7C207910
+    gpg --verify ${FNAME}{.sig,}
+    sudo tar -C ${INSTALL_DIR} -xvf ${FNAME}
 fi
-cd ${INSTALL_DIR}/${DNAME} || exit ${LINENO}
-
+cd ${INSTALL_DIR}/${DNAME}
 
 if command -v yum; then
     sudo yum install -y yum-utils rpm-build
     yumdownloader --source emacs
     # yum install -y libX11-devel
-    # sudo yum -y groupinstall "Additional Development" || exit ${LINENO}
+    # sudo yum -y groupinstall "Additional Development"
     # rpmbuild --rebuild ftp://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/source/tree/Packages/e/emacs-25.2-2.fc27.src.rpm
     sudo yum install -y giflib-devel libXdmcp-devel m17n-lib-devel  \
     libotf-devel ImageMagick-devel GConf2-devel gpm-devel  \
     liblockfile-devel webkitgtk4-devel python3-devel
 elif command -v apt-get; then
-    sudo apt-get build-dep -y emacs24 || exit ${LINENO}
+    sudo apt-get build-dep -y emacs24
 fi
 
 ./configure --with-xwidgets && read -p 'continue: ' &&  \
     make &&  \
-    sudo make install || exit ${LINENO}
+    sudo make install
