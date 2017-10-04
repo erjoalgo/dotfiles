cf76064af7f4dc3379d89682bc858ed140ee99a9
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-04 13:05:38 -0700

    scripts/go-installs: need root to edit /etc files

--- scripts/installs/go-installs.sh
+++ scripts/installs/go-installs.sh
@@ -16,6 +16,7 @@ if ! command -v go; then
     test -d ${INSTALL_DIR}/go
 
     PROFILE_FILE=/etc/profile.d/go-env.sh
+    which insert-text-block
     sudo $(which insert-text-block) \
 	 '# c0b15b6c-e5fc-495a-b1be-f02308cee38d-add-go-tools-to-path'  \
 		      ${PROFILE_FILE} <<EOF
@@ -28,13 +29,13 @@ fi
 # add fallback-gopath if user's GOPATH not set
 
 GOPATH=/usr/share/gopath
-insert-text-block '# 387b6046-a715-11e7-b87e-2be468b96d0a-set-default-gopath'  \
+sudo $(which insert-text-block) '# 387b6046-a715-11e7-b87e-2be468b96d0a-set-default-gopath'  \
 		  /etc/bash.bashrc <<EOF
 export GOPATH=$GOPATH
 export PATH=\$PATH:\$GOPATH/bin
 EOF
 
-insert-text-block '# fba1e4c6-a726-11e7-b4e2-23bbc233d273-set-default-gopath'  \
+sudo $(which insert-text-block) '# fba1e4c6-a726-11e7-b4e2-23bbc233d273-set-default-gopath'  \
 		  /etc/environment <<< "GOPATH=$GOPATH"
 
 for REPO in\
