dba3c232946dc86baba342b391d1c8df77453465
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-03 23:41:54 -0700

    go-installs: add fallback GOPATH in /etc/environment
    and /etc/bash.bashrc

--- scripts/installs/go-installs.sh
+++ scripts/installs/go-installs.sh
@@ -25,6 +25,18 @@ EOF
     go version
 fi
 
+# add fallback-gopath if user's GOPATH not set
+
+GOPATH=/usr/share/gopath
+insert-text-block '# 387b6046-a715-11e7-b87e-2be468b96d0a-set-default-gopath'  \
+		  /etc/bash.bashrc <<EOF
+export GOPATH=$GOPATH
+export PATH=\$PATH:\$GOPATH/bin
+EOF
+
+insert-text-block '# fba1e4c6-a726-11e7-b4e2-23bbc233d273-set-default-gopath'  \
+		  /etc/environment <<< "GOPATH=$GOPATH"
+
 for REPO in\
     github.com/github/hub \
     golang.org/x/tools/cmd/goimports \
