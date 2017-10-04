8d7b65f3f471aa0c06e7522d480da83585d4f583
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-03 23:10:43 -0700

    git-https-to-ssh: run git fetch before setting upstream
    should not be required

--- git-https-to-ssh.sh
+++ git-https-to-ssh.sh
@@ -5,4 +5,5 @@ SSH_URL=$(git remote -vv | grep "${REMOTE}" | head -1 | awk '{print $2}' | sed '
 git remote rename "${REMOTE}" "${REMOTE}-https"
 git remote add "${REMOTE}" "${SSH_URL}"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git fetch ${REMOTE}
 git branch --set-upstream-to=${REMOTE}/${BRANCH}
