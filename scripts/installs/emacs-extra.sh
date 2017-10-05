7ba723193b21a66af73f9ac4317701ebe1eb0f34
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-05 03:40:15 -0700

    emacs-install: add aspell or hunspell. merge emacs-extra.sh

--- scripts/installs/emacs-extra.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-
-sudo apt-get install -y aspell-es emacs-goodies-el
--- scripts/installs/emacs-install.sh
+++ scripts/installs/emacs-install.sh
@@ -36,8 +36,10 @@ if command -v yum; then
 
     sudo yum install -y libXpm libXpm-devel.x86_64
     sudo yum install -y gnutls gnutls-utils
+    sagiy hunspell{,-es}
 elif command -v apt-get; then
     sudo apt-get build-dep -y emacs24
+    sudo apt-get install -y aspell-es emacs-goodies-el
 fi
 
 ./configure && \
