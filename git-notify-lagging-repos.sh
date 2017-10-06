77c98905e83ce7567a7aeba35f3dbdbc41d4a9aa
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-05 22:23:00 -0700

    lagging-repos: print errs to stderr in red color
    1. factor out check_lagging to a function
    2. distinguish between errs and lagging repo
    3. print errs in red
    4. fix proceeding with errs

--- git-notify-lagging-repos.sh
+++ git-notify-lagging-repos.sh
@@ -10,22 +10,30 @@
 # echo "git-notify called with args: $0 $*, PATH: $PATH, USER: $(whoami)"  \
 #     | sudo tee /var/log/git-notify
 
-while test $# -gt 0; do
-    REPO=${1} && shift
+function check_lagging	{
+    test -d ${REPO} && cd "${REPO}" || return 2
 
-    cd "${REPO}" || exit ${LINENO}
-
-    # if ! git status &> /dev/null; then
-    # if ! test -d .git; then
-    if ! git status &> /dev/null; then
-	# echo "not at a git repo: ${REPO}" && continue
-	continue
-    fi
-
-    BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    git status &> /dev/null || return 2
+    BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null) || return 2
 
     if test $? -ne 0 -o $(git branch -r --contains ${BRANCH} | wc -l) -eq 0; then
-	echo "${REPO}/${BRANCH} might be ahead of all remotes"
+	return 1
+    else
+	return 0
     fi
-    # read
+}
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+# https://stackoverflow.com/questions/5947742/
+
+while test $# -gt 0; do
+    REPO=${1} && shift
+    check_lagging "${REPO}"
+    LAST=$?
+    if test 1 -eq ${LAST}; then
+	echo "${REPO}/${BRANCH} might be ahead of all remotes"
+    elif test ${LAST} -gt 1; then
+	printf "${RED}problem processing ${REPO} ...${NC}\n" 1>&2
+    fi
 done
