eced19ddabb769a3397eeda8a0c92d6fe67d82df
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-07 10:44:45 -0700

    boot.sh: lagging-repos notification once a day

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -76,5 +76,5 @@ which apt-get && sudo apt-get install -y apt-file && sudo apt-file update || tru
 
 insert-text-block '# 91352955-c448-4c16-a4d4-54470089c900-notify-lagging-repos-user-crontab' \
     <(crontab -l 2>/dev/null) -o >(crontab) <<EOF
-0 * * * * bash -c '~/git/erjoalgo-gnu-scripts/git-notify-lagging-repos.sh ~/git/*'
+30 10 * * * bash -c '~/git/erjoalgo-gnu-scripts/git-notify-lagging-repos.sh ~/git/*'
 EOF
