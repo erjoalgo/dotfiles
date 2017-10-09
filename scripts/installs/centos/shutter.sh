6b7bf65df5b95c9db82816d489f28be30c9bd55a
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-09 12:20:34 -0700

    install shutter and eog on centos

---
+++ scripts/installs/centos/shutter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+# rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm
+# rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el6.nux.noarch.rpm 
+sudo yum -y install epel-release
+sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+
+sudo yum install -y shutter eog
