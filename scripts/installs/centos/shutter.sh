cd2674df87aeb98162fae96609b93d8ac93f0e09
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-09 12:42:54 -0700

    shutter install: add dbus-x11 still not working

--- scripts/installs/centos/shutter.sh
+++ scripts/installs/centos/shutter.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 sudo yum -y install epel-release
 sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 
-sudo yum install -y shutter eog librsvg2
+sudo yum install -y shutter eog librsvg2 dbus-x11
