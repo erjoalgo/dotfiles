c1c20cf3ae5c4991343430d8665461699861c839
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-12 19:27:30 -0700

    show last-modified last

--- ls-recursive-last-modified.sh
+++ ls-recursive-last-modified.sh
@@ -2,4 +2,4 @@
 
 # https://superuser.com/questions/416308/how-to-list-files-recursively-and-sort-them-by-modification-time
 find . -type f -exec stat --printf "%Y\t%y %N\n" '{}' \;  \
-    | sort -rn  | cut -f2-
+    | sort -n  | cut -f2-
