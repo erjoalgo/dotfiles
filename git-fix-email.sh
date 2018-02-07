a8a4a95507e2bf0cbad20287fa9fd60323fc9b2e
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2018-02-06 19:12:57 -0800

    new file:   git-fix-email.sh

---
+++ git-fix-email.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+export WRONG_EMAIL="$1" && shift
+export NEW_EMAIL="$1" && shift
+
+git filter-branch -f --env-filter '
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
