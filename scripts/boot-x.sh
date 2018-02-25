931567e9a64adf8b4799c65f916d6c0dc788e60b
Ernesto Alfonso <erjoalgo@gmail.com>
2018-02-25 15:59:32 -0800

    add boot-x.sh for x-related tools

---
+++ scripts/boot-x.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+cd $( dirname "${BASH_SOURCE[0]}" )
+
+./installs/install-stumpwm.sh
+./installs/emacs-install.sh
+./installs/roxterm-install.sh
+sudo apt-get install -y chromium
+
+# TODO wifi-boot
+# TODO automate debian installer seed
+# TODO automate vimium installation
