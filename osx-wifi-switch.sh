1deefc6c977579a42bc30db2849df13b89d8fee5
Ernesto Alfonso <erjoalgo@gmail.com>
2018-02-27 19:48:50 -0800

    use speech synthesis to indicate wifi state, ESSID and IP address

--- osx-wifi-switch.sh
+++ osx-wifi-switch.sh
@@ -9,4 +9,19 @@ STATE=$(/usr/sbin/networksetup -getairportpower ${IFACE} | grep -o 'On\|Off')
 
 NEW_STATE=$(test $STATE = "On"  && echo "Off" || echo "On")
 
+say "turning wifi ${NEW_STATE}"
+
 /usr/sbin/networksetup -setairportpower ${IFACE} ${NEW_STATE}
+
+
+if test "On" = "${NEW_STATE}"; then
+    while ! /usr/sbin/networksetup -getairportnetwork ${IFACE}; do
+        sleep 1
+        say "waiting for connection"
+    done
+
+    ESSID=$(/usr/sbin/networksetup -getairportnetwork ${IFACE} | cut -d: -f2 | xargs echo)
+    say "connected to ${ESSID}"
+    IP_ADDR=$(ifconfig ${IFACE} | grep -o "inet [0-9.]*")
+    say "my ip address is ${IP_ADDR}"
+fi
