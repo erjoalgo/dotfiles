346a067a85ded0ba306b6bee5195efef6c814453
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-01 20:25:35 -0800

    support custom nmap args

--- nmap-discover-hosts.sh
+++ nmap-discover-hosts.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
 
-SUBNET=$1
-
-if test -z ${SUBNET}; then
+if true; then
     LOCALHOST='127.0.0.1'
     SUBNETS=$(ifconfig -a |\
 		     grep -oP 'inet addr:.*? '|\
@@ -20,5 +18,10 @@ if test -z ${SUBNET}; then
    SUBNET=$(sed 's|[0-9]*$|1/24|' <<< ${SUBNET})
 fi
 
-# subnet = re.sub("[0-9]+$", "1/24", subnet)
-nmap -sP ${SUBNET}
+ARGS=${*}
+
+if test $# -lt 1; then
+    ARGS=-sn
+fi
+
+nmap ${SUBNET} ${ARGS}
