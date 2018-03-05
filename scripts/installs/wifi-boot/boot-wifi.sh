f2f3d860bd6cff65e8cbbef75de519b7427d363e
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-04 23:58:52 -0800

    add wip wifi-boot

---
+++ scripts/installs/wifi-boot/boot-wifi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+sudo apt-get install -y wireless-tools expect
+
+# apt-get install -y firmware-realtek
+# apt-get install -y firmware-iwlwifi
+
+# modprobe -r realtek
+# modprobe realtek
+
+# 108  modprobe -r iwlwifi; modproblem iwlwifi
+# 109  modprobe -r realtek ; modprobe realtek
