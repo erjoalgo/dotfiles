9e263a5b9038b5fc9f7418274b77f3142bd673e5
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-06 15:17:29 -0800

    switch to ip from ifconfig

--- nmap-discover-hosts.sh
+++ nmap-discover-hosts.sh
@@ -2,10 +2,10 @@
 
 if true; then
     LOCALHOST='127.0.0.1'
-    SUBNETS=$(ifconfig -a |\
-		     grep -oP 'inet addr:.*? '|\
-		     cut -d: -f2|\
-		     grep -v ${LOCALHOST})
+    SUBNETS=$(ip address |\
+		  grep -oP 'inet [0-9./]*'|\
+		  cut -d' ' -f2|\
+		  grep -v ${LOCALHOST})
 
    if test $(wc -l <<< "${SUBNETS}") -gt 1; then
        select SUBNET in ${SUBNETS}; do
@@ -15,7 +15,6 @@ if true; then
    else
        SUBNET=$(tr -d ' ' <<< ${SUBNETS})
    fi
-   SUBNET=$(sed 's|[0-9]*$|1/24|' <<< ${SUBNET})
 fi
 
 ARGS=${*}
