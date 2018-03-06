244f1bf5167630ba734462cda4271808f59a4980
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-05 20:21:35 -0800

    fix gpg --recv-keys error in stretch

--- scripts/boot-x.sh
+++ scripts/boot-x.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 cd $( dirname "${BASH_SOURCE[0]}" )
 
+sudo apt-get install -y dirmngr || true
+
 ./installs/install-stumpwm.sh
 ./installs/emacs-install.sh
 ./installs/roxterm-install.sh
