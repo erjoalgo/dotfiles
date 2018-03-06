b58756dbf931c2ce4c31748210c30ba3f7530ebd
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-05 20:22:37 -0800

    fix /dev/tty* permission denied in stretch

--- scripts/boot-x.sh
+++ scripts/boot-x.sh
@@ -12,6 +12,10 @@ sudo apt-get install -y dirmngr || true
 sudo apt-get install -y chromium zathura
 sudo apt-get install -y eog
 
+sudo $(which insert-text-block) '# 37561c4f-5b87-4252-9724-6eed90ee3943-fix-stretch-X-issue'  \
+                  /etc/X11/Xwrapper.config<<EOF
+needs_root_rights=yes
+EOF
 
 # TODO wifi-boot
 # TODO automate debian installer seed
