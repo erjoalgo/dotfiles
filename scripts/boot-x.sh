8dd1f384e83bd3b7f372ac35e990e7d39f61228e
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-05 20:22:06 -0800

    continue despite missing roxterm in stretch

--- scripts/boot-x.sh
+++ scripts/boot-x.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y dirmngr || true
 
 ./installs/install-stumpwm.sh
 ./installs/emacs-install.sh
-./installs/roxterm-install.sh
+./installs/roxterm-install.sh || true
 sudo apt-get install -y chromium zathura
 sudo apt-get install -y eog
 
