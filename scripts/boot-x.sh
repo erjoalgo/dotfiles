37bdcdc0c766c41fc20f7c5f12e78ed28e2ce0cd
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-05 21:30:15 -0800

    add workaround to debian bug affecting xcalib invert screen

--- scripts/boot-x.sh
+++ scripts/boot-x.sh
@@ -17,6 +17,17 @@ sudo $(which insert-text-block) '# 37561c4f-5b87-4252-9724-6eed90ee3943-fix-stre
 needs_root_rights=yes
 EOF
 
+
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=851810
+sudo $(which insert-text-block) '# ed74222e-d8c2-4920-a450-fa17d93e7650-fix-xcalib-broken'  \
+                  --parents /etc/X11/xorg.conf.d/20-intel.conf<<EOF
+Section "Device"
+  Identifier "Intel Graphics"
+  Driver "intel"
+EndSection
+EOF
+
+
 # TODO wifi-boot
 # TODO automate debian installer seed
 # TODO automate vimium installation
