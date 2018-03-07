dfff1080ebfcf2cd9e8452732db74f35cb2576cb
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-06 23:54:53 -0800

    deprecate roxterm in favor of gnome-terminal
    removed from stretch, unmaintained
    hard to install from source
    gnome-terminal is adequate

--- scripts/boot-x.sh
+++ scripts/boot-x.sh
@@ -8,8 +8,7 @@ sudo apt-get install -y dirmngr || true
 
 ./installs/install-stumpwm.sh
 ./installs/emacs-install.sh
-./installs/roxterm-install.sh || true
-sudo apt-get install -y chromium zathura
+sudo apt-get install -y chromium zathura gnome-terminal
 sudo apt-get install -y eog
 
 
