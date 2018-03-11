12cf4ce51b6d62bb056887bcba309bea8166f941
Ernesto Alfonso <erjoalgo@gmail.com>
2018-03-10 17:45:02 -0800

    embed genpasswd fun

--- htpasswd-gen.sh
+++ htpasswd-gen.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -x
 
 set -euo pipefail
+function genpasswd {
+    local l=$1;
+    [ "$l" == "" ] && l=16;
+    tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs;
+}
+
 HTPASSWD_FILE=${1} && shift
 LEN=8
 test -e ${HTPASSWD_FILE} || touch ${HTPASSWD_FILE}
