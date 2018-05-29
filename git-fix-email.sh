c5d1164ebf0bd465fb1f80395f900cb808a3b366
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2018-05-29 12:55:05 -0700

    add example on how to filter a range of commits

--- git-fix-email.sh
+++ git-fix-email.sh
@@ -15,3 +15,5 @@ then
     export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
+
+# git filter-branch -f --env-filter 'export GIT_AUTHOR_EMAIL="victor@ge.com" GIT_AUTHOR_NAME="Victor"' origin/master..tmp
