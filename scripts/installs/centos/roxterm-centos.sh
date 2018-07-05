bb0bc3f24c6e24e9fed10e6d7793787ab9170dc6
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2018-07-04 23:04:22 -0700

    extract roxterm repo files in /tmp

--- scripts/installs/centos/roxterm-centos.sh
+++ scripts/installs/centos/roxterm-centos.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 if ! test -f /etc/yum.repos.d/gf.repo; then
+    cd /tmp
     wget http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7.noarch.rpm
     sudo rpm -Uvh gf-release*rpm
 fi
