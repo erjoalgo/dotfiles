eb4d345bf939e3df021d5d7892293b55169663ed
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2018-07-04 18:33:36 -0700

    Revert "echo-ip.sh: split long line"
    This reverts commit 0e5d893345a67a97629d9a82c08adae8fa3ad0e9.
    
    Conflicts:
    	echo-ip.sh

--- echo-ip.sh
+++ echo-ip.sh
@@ -1,2 +1,5 @@
 #!/bin/bash -x
-curl -s checkip.dyndns.org | grep -oi "current ip address: [^<]*" | grep -o "[0-9.]*"
+
+curl -s checkip.dyndns.org \
+    | grep -oi "current ip address: [^<]*" \
+    | grep -o "[0-9.]*"
