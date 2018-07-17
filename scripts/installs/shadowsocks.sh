b75ffe4c2bf0610892d7d836c5d5eff97702202f
Ernesto Alfonso <erjoalgo@gmail.com>
2018-07-16 18:44:00 -0700

    add sample config and command to run on it

--- scripts/installs/shadowsocks.sh
+++ scripts/installs/shadowsocks.sh
@@ -5,3 +5,19 @@ set -euo pipefail
 which go
 
 go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
+
+SAMPLE_CONFIG=${HOME}/.shadow-sample-config.json
+
+cat <<EOF > ${SAMPLE_CONFIG}
+{
+    "server": "***REMOVED***",
+    "server_port":***REMOVED***,
+    "local_address":"127.0.0.1",
+    "local_port":1080,
+    "password":"***REMOVED***",
+    "timeout":120,
+    "method": "aes-256-cfb"
+}
+EOF
+
+echo "run: ss-local -c ${SAMPLE_CONFIG}"
