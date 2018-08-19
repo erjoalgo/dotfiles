0df8aaa15324d97dd90d8fbeef16a75218546914
Ernesto Alfonso <erjoalgo@gmail.com>
2018-08-19 10:42:32 -0700

    fix not updating mute/unmute flag
    fix formatting

--- lisp/commands/vol.lisp
+++ lisp/commands/vol.lisp
@@ -5,45 +5,30 @@
     ((car (:y-or-n "raise volume?"))
      (:number "percentage?"))
   "change volume by a percentage, via amixer"
-    (let* (
-	   (sign (if up "+" "-"))
-           (cmd (format nil
-			"amixer set Master ~D%~A"
-			percentage
-			sign))
-	   )
-      ;;(echo (format nil "running: ~A" cmd))
-      (run-shell-command cmd)
-      (if up (setq *vol-muted* nil ))
-      ))
-;;(run-shell-command "amixer set Master 0%")
+  (let* ((sign (if up "+" "-"))
+         (cmd (format nil
+		      "amixer set Master ~D%~A"
+		      percentage
+		      sign)))
+    ;;(echo (format nil "running: ~A" cmd))
+    (run-shell-command cmd)
+    (if up (setq *vol-muted* nil))))
+
 (defun get-curr-volume ()
-  (let* (
-         (out (run-shell-command "amixer get Master" t))
-         ;;(vol (extract-match "'Master',([0-9]+)" out 1))
-         (vol (extract-match "Playback.*?[[]([0-9]+)%]" out 1))
-         )
-    vol
-    )
-  )
+  (let* ((out (run-shell-command "amixer get Master" t))
+         (vol (extract-match "Playback.*?[[]([0-9]+)%]" out 1)))
+    vol))
 
 (defcommand volmute-toggle ()()
   "mute/unmute"
-  (if (eq *vol-muted* nil )
+  (if (null *vol-muted*)
       (progn
         (setq *vol-muted* (get-curr-volume))
         (run-shell-command "amixer set Master 0%"))
-      (run-shell-command (format nil
-                                 "amixer set Master ~A%"
-                                 *vol-muted*)))
-
-  )
-
-
+      (progn (run-shell-command (format nil
+                                        "amixer set Master ~A%"
+                                        *vol-muted*))
+             (setf *vol-muted* nil))))
 
 (defcommand volup ()() "volume up" (volchange t *VOLSTEP* ))
-(defcommand voldown ()() "volume down" (volchange nil *VOLSTEP* ))
-
-
-
-
+(defcommand voldown ()() "volume down" (volchange nil *VOLSTEP*))
