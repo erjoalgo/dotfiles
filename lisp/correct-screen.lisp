73b0a5921206b35d630201865c8514daf41ee477
Ernesto Alfonso <erjoalgo@gmail.com>
2018-08-27 18:54:57 -0700

    formatting

--- lisp/correct-screen.lisp
+++ lisp/correct-screen.lisp
@@ -28,18 +28,18 @@
                                                        (ppcre:split " +" (pop lines))))
     while lines
     collect (destructuring-bind (id state . etc) (funcall pop-line)
-	      (declare (ignore etc))
-	      (let ((modes (loop
+              (declare (ignore etc))
+              (let ((modes (loop
                              as line = (car lines)
-			     while (ppcre:scan "^ +[0-9]+x[0-9]+" line)
-			     collect (xrandr-parse-mode (funcall pop-line t))))
-		    (extra (loop
-			     while (ppcre:scan "^ +" (car lines))
-			     collect (funcall pop-line))))
-		(make-xrandr-display :id id :state state
-				     :modes modes
-				     :connected-p (equal state "connected")
-				     :extra extra
+                             while (ppcre:scan "^ +[0-9]+x[0-9]+" line)
+                             collect (xrandr-parse-mode (funcall pop-line t))))
+                    (extra (loop
+                             while (ppcre:scan "^ +" (car lines))
+                             collect (funcall pop-line))))
+                (make-xrandr-display :id id :state state
+                                     :modes modes
+                                     :connected-p (equal state "connected")
+                                     :extra extra
                                      :mode (loop for mode in modes thereis
                                                                    (and (xrandr-mode-active-p mode) mode)))))))
 
@@ -50,26 +50,26 @@
       (loop as line = (read-line fh nil nil)
             while line
             unless (equal "" line)
-            collect (destructuring-bind (display mode)
-                        (cl-ppcre:split #\Tab line)
-                      (cons display (xrandr-parse-mode mode)))))))
+              collect (destructuring-bind (display mode)
+                          (cl-ppcre:split #\Tab line)
+                        (cons display (xrandr-parse-mode mode)))))))
 
 (defun correct-screen (&optional order)
   (let* ((displays (xrandr-displays))
-	 (connected (remove-if-not 'xrandr-display-connected-p
-				   displays))
-	 (to-connect-ordered (if order (loop for ith in order
-					     collect (nth ith connected))
-				 connected))
-	 (to-disconnect (remove-if (lambda (display)
-				     (member display to-connect-ordered))
-				   displays)))
+         (connected (remove-if-not 'xrandr-display-connected-p
+                                   displays))
+         (to-connect-ordered (if order (loop for ith in order
+                                             collect (nth ith connected))
+                                 connected))
+         (to-disconnect (remove-if (lambda (display)
+                                     (member display to-connect-ordered))
+                                   displays)))
     ;; disconnect
     (mapc (lambda (off-display)
-	    (run-shell-command-print
-	     (format nil "xrandr --output ~A --off"
-		     (xrandr-display-id off-display))))
-	  to-disconnect)
+            (run-shell-command-print
+             (format nil "xrandr --output ~A --off"
+                     (xrandr-display-id off-display))))
+          to-disconnect)
     ;; connect displays in order
     (loop for display in to-connect-ordered
           with pos-x = 0
@@ -77,14 +77,14 @@
           as mode = (or (cdr (assoc (xrandr-display-id display) display-mode-prefs :test #'equal))
                         (car (xrandr-display-modes display)))
           do (destructuring-bind (mode-width mode-height) mode
-	       (declare (ignore mode-height))
-	       (let* ((id (xrandr-display-id display))
-		      (mode-string (format nil "~{~a~^x~}" mode))
-		      (pos-string (format nil "~Dx0" pos-x))
-		      (cmd (format nil "xrandr --output ~A --mode ~A --pos ~A"
-				   id mode-string pos-string )))
-	         (run-shell-command-print cmd))
-	       (incf pos-x mode-width)))))
+               (declare (ignore mode-height))
+               (let* ((id (xrandr-display-id display))
+                      (mode-string (format nil "~{~a~^x~}" mode))
+                      (pos-string (format nil "~Dx0" pos-x))
+                      (cmd (format nil "xrandr --output ~A --mode ~A --pos ~A"
+                                   id mode-string pos-string )))
+                 (run-shell-command-print cmd))
+               (incf pos-x mode-width)))))
 
 (defcommand correct-screen-fix-display-prefs () ()
   (loop with fixes = nil
@@ -106,36 +106,34 @@
 (defcommand correct-screen-prompt-display-order () ()
   "correct screen, prompting for display order"
   (let* ((displays (remove-if-not
-		    'xrandr-display-connected-p
-		    (xrandr-displays)))
-	  (prompt (format nil "~{~{~a: \"~a\"~}~^, ~}: "
-			  (loop for display in (mapcar 'xrandr-display-id displays)
-			     for i from 0
-			     collect (list i display))))
-	   ;(line (read-one-line (current-screen) prompt))
-	   ;(line (progn (echo (mapcar 'caar info))
-			;(read-one-line (current-screen) ": ")))
-	   (line (if (null (cdr displays))
-		     ;; `((0 . ,(car displays)))
-		     "0"
-		     (read-one-line (current-screen)
-				prompt)))
-	   (order (loop for c across line
-		     collect (- (char-code c) (char-code #\0))))
-	   )
+                    'xrandr-display-connected-p
+                    (xrandr-displays)))
+         (prompt (format nil "~{~{~a: \"~a\"~}~^, ~}: "
+                         (loop for display in (mapcar 'xrandr-display-id displays)
+                               for i from 0
+                               collect (list i display))))
+                                        ;(line (read-one-line (current-screen) prompt))
+                                        ;(line (progn (echo (mapcar 'caar info))
+                                        ;(read-one-line (current-screen) ": ")))
+         (line (if (null (cdr displays))
+                   ;; `((0 . ,(car displays)))
+                   "0"
+                   (read-one-line (current-screen)
+                                  prompt)))
+         (order (loop for c across line
+                      collect (- (char-code c) (char-code #\0))))
+         )
     (when (and order
-	       (>= (apply 'max order) (length displays)))
-	(error "index out of bounds"))
-      (correct-screen order)))
+               (>= (apply 'max order) (length displays)))
+      (error "index out of bounds"))
+    (correct-screen order)))
 
 '(when display-names
   (if (null (cdr display-names))
       (car display-names)
       (let ((prompt (format nil "select display order: ~A~%"
-			    (apply 'join #\Newline display-names)))
-	    (response (completing-read (current-screen)
-				       prompt
-				       display-names :require-match t)))
-	(cons (correct-screen-prompt-display-order)))))
-
-
+                            (apply 'join #\Newline display-names)))
+            (response (completing-read (current-screen)
+                                       prompt
+                                       display-names :require-match t)))
+        (cons (correct-screen-prompt-display-order)))))
