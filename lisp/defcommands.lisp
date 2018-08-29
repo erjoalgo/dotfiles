79460c79e5c7499f798756788295e079be9e52cd
Ernesto Alfonso <erjoalgo@gmail.com>
2018-08-28 23:38:08 -0700

    move speech-related commands to speech.lisp

--- lisp/defcommands.lisp
+++ lisp/defcommands.lisp
@@ -94,34 +94,6 @@
   (echo "connecting to internet...")
   (run-shell-command "wifi-connect"))
 
-(defcommand speak-key ()
-    ;;((:key "enter key to speak: " ));;this requires pressing enter
-    ()
-  ;;(let ((text (key-keysym key)))
-  "speak the name of the next typed key"
-  (let ((text (read-one-char (current-screen))))
-    (SB-EXT:RUN-PROGRAM "espeak"
-			(list (format nil "~A" text))
-			:search t
-			:output t
-			:error t
-			:wait t)))
-
-(defcommand speak-string (text)
-    ((:string "enter string to speak: " ))
-  "speak some text"
-  (SB-EXT:RUN-PROGRAM "espeak"
-		      (list text)
-		      :search t
-		      :output t
-		      :error t
-		      :wait t))
-
-(defcommand spell-clipboard () ()
-            "spell clipboard contents, character by character"
-            (speak-string
-             (cl-ppcre:regex-replace-all "(.)" (GET-X-SELECTION) " \\1")))
-
 (defcommand echo-window-class () ()
   "echo window class"
   (message "window class: ~A" (window-class (current-window))))
