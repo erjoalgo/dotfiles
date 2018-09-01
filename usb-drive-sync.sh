95b960d6c49989a2de847311cedde42ef5123dbc
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-01 16:53:06 -0700

    new file:   usb-drive-sync.sh

---
+++ usb-drive-sync.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+MNT=${HOME}/.usb-drive-symlink
+
+HOME_TWO_WAY=${HOME}/private-data
+mkdir -p ${HOME_TWO_WAY}
+unison -dontchmod -perms 0  \
+           -batch \
+           ${MNT}/sync-two-ways ${HOME_TWO_WAY}
+
+# -merge = Name *.org -> emacsclient --eval  \
+    # '(ediff-merge-files-with-ancestor "CURRENT1" "CURRENT2" "CURRENTARCH" nil "NEW")'
+
+
+ROOT_DEVICE_PATH=$(sed "s/ 	/ /g" /etc/fstab | grep ' / ' | cut -f1 -d' ')
+MACHINE_UUID=$(lsblk ${ROOT_DEVICE_PATH} -o uuid | tail -1)
+
+MNT_ONE_WAY=${MNT}/machines/${MACHINE_UUID}
+
+mkdir -p ${MNT_ONE_WAY}
+mkdir -p ${HOME}/private-data-one-way
+
+for ONE_WAY_REL in .bash_history  \
+                       private-data-one-way/ \
+                   ; do
+    SRC=${HOME}/${ONE_WAY_REL}
+    DEST=${MNT_ONE_WAY}/${ONE_WAY_REL}
+    if test -e ${SRC}; then
+        rsync -rv ${SRC} ${DEST}
+    fi
+done
+
+# TODO rsnapshot
