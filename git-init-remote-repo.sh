fe76c839b79a1194135983f7230a29ddb12cd3b2
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-26 01:59:48 -0700

    add usage, getopt
    assume user is ussing ssh-config
    to encapsulate ssh user+host+port

--- git-init-remote-repo.sh
+++ git-init-remote-repo.sh
@@ -2,24 +2,46 @@
 
 set -euo pipefail
 
+# ensure we're on a git directory
 git status
-USERHOST=${1} && shift
-PORT=${1:-22} && shift || true
 
-REPO=$(basename $(pwd))
-HOST=$(sed 's/.*@//g' <<< ${USERHOST})
-SRV_PREFIX=/opt/git
+function usage {
+    echo "$(basename ${0}) -t SSH_USERHOST [ -o SSH_opts ] [ -r local_repo ]
+[ -d remote_git_bare_prefix ]"
+}
 
-# git server setup. ensure git user has been set up with right shell
-RSA=$(cat ${HOME}/.ssh/id_rsa.pub)
+SSH_OPTS=""
+while getopts "ht:o:r:d:" OPT; do
+    case ${OPT} in
+    t)
+        SSH_USERHOST=${OPTARG}
+        ;;
+    o)
+        SSH_OPTS=${OPTARG}
+        ;;
+    r)
+        REPO=${OPTARG}
+        ;;
+    d)
+        SRV_PREFIX=${OPTARG}
+        ;;
+    h)
+        less $0
+        usage
+        exit 0
+        ;;
+    esac
+done
 
-ssh ${USERHOST} -p${PORT}  "sudo bash -s" <<EOF
-which git-shell
 
-grep git-shell /etc/shells || which git-shell >> /etc/shells
+REPO=${REPO:-$(basename $(pwd))}
+SRV_PREFIX=${SRV_PREFIX:-/opt/git}
+if test -z "${SSH_USERHOST}"; then
+    usage
+    exit 1
+fi
 
-
-
+ssh ${SSH_USERHOST} ${SSH_OPTS}  "sudo bash -s" <<EOF
 set -e
 
 REPOPATH=${SRV_PREFIX}/${REPO}
@@ -28,10 +50,13 @@ git init --bare
 chown -R git:git .
 EOF
 
-REMOTE_URL="ssh://git@${HOST}:${PORT}${SRV_PREFIX}/${REPO}"
+# TODO remove possible user from SSH_USERHOST
+# TODO add possible non-standard port
+
+REMOTE_URL="ssh://git@${SSH_USERHOST}${SRV_PREFIX}/${REPO}"
 echo ${REMOTE_URL}
 ORIGIN_NAME=origin
-for NAME in origin origin-${USERHOST} origin-${USERHOST}-${RANDOM}; do
+for NAME in origin origin-${SSH_USERHOST} origin-${SSH_USERHOST}-${RANDOM}; do
     ORIGIN_NAME=${NAME}
     if test "$(git config --get remote.${NAME}.url)" = "${REMOTE_URL}"; then
 	break
@@ -39,7 +64,6 @@ for NAME in origin origin-${USERHOST} origin-${USERHOST}-${RANDOM}; do
 	break
     fi
 done
-
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 git fetch ${ORIGIN_NAME}
