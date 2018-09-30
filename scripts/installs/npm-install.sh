50f50ee9cefdcaa422badba062b27cc3bc2a6b45
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-30 12:11:07 -0700

    also add nvm to system profile

--- scripts/installs/npm-install.sh
+++ scripts/installs/npm-install.sh
@@ -4,8 +4,14 @@ set -euo pipefail
 cd
 test -d .nvm || git clone https://github.com/creationix/nvm.git .nvm
 
-insert-text-block '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' ${HOME}/.profile <<"EOF"
+for PROFILE_FILE in \
+    /etc/profile.d/node-env.sh \
+        ${HOME}/.profile \
+    ; do
+    PROFILE_FILE=${HOME}/.profile
+    insert-text-block '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' ${PROFILE_FILE} <<"EOF"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 EOF
+done
