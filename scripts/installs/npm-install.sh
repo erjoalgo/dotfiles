631fb9a1075b727213ca330ea7803ececf019d60
Ernesto Alfonso <erjoalgo@gmail.com>
2018-12-22 22:07:18 +0000

    fix no nvm on ssh login

--- scripts/installs/npm-install.sh
+++ scripts/installs/npm-install.sh
@@ -7,8 +7,8 @@ test -d .nvm || git clone https://github.com/creationix/nvm.git .nvm
 for PROFILE_FILE in \
     /etc/profile.d/node-env.sh \
         ${HOME}/.profile \
+        ${HOME}/.bash_profile \
     ; do
-    PROFILE_FILE=${HOME}/.profile
     insert-text-block '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' ${PROFILE_FILE} <<"EOF"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
