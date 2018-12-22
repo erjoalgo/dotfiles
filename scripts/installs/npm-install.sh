220113f16d58cd9fc22705d58b5ae6a2a1694768
Ernesto Alfonso <erjoalgo@gmail.com>
2018-12-22 22:08:07 +0000

    maybe use sudo

--- scripts/installs/npm-install.sh
+++ scripts/installs/npm-install.sh
@@ -9,7 +9,10 @@ for PROFILE_FILE in \
         ${HOME}/.profile \
         ${HOME}/.bash_profile \
     ; do
-    insert-text-block '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' ${PROFILE_FILE} <<"EOF"
+    test -w ${PROFILE_FILE} && SUDOOPT="" || SUDOOPT="sudo"
+    ${SUDOOPT} $(which insert-text-block) \
+               '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' \
+               ${PROFILE_FILE} <<"EOF"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
