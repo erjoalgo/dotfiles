c89a0b77c08e4eba3d85ed1d91e2eb2a9244a87f
Ernesto Alfonso <erjoalgo@gmail.com>
2019-01-18 23:07:09 -0800

    mkdir -p ~/bin

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -29,6 +29,8 @@ GIT_EMAIL=erjoalgo@gmail.com
 GIT_NAME="Ernesto Alfonso"
 GIT_HOME=${HOME}/git
 mkdir -p ${GIT_HOME}
+mkdir -p ${HOME}/bin
+
 for REPO in erjoalgo-stumpwmrc \
 		dotemacs \
 	    ; do
