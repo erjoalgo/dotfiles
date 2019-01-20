7c40f8745376d93dfb1cc56c0290afcebe2bd8d4
Ernesto Alfonso <erjoalgo@gmail.com>
2019-01-20 12:56:07 -0800

    new file:   scripts/installs/sbcl.sh

---
+++ scripts/installs/sbcl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+URL=http://prdownloads.sourceforge.net/sbcl/sbcl-1.4.15-x86-64-linux-binary.tar.bz2
+
+BASE=$(basename "${URL}")
+DIR_PREFIX=$(grep -o "sbcl[0-9.-]*"  <<< "${BASE}")
+
+TOP=${HOME}/src
+mkdir -p ${TOP}
+cd ${TOP}
+
+function find-dir  {
+    find . -name "${DIR_PREFIX}*" -type d |  \
+        tr -d '\n'
+}
+
+DIR=$(find-dir)
+if ! test -d ${DIR}; then
+    test -e ${BASE} || wget "${URL}"
+    test -e ${BASE}
+    tar -C ~/src/ -axvf ${BASE}
+    DIR=$(find-dir)
+    test -d ${DIR}
+fi
+
+cd ${DIR}
+
+./install.sh
