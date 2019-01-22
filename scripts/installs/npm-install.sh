023eff357517146ee6275369a4420eaf0d539791
Ernesto Alfonso <erjoalgo@gmail.com>
2019-01-21 21:01:04 -0800

    avoid re-sourcing nvm.sh

--- scripts/installs/npm-install.sh
+++ scripts/installs/npm-install.sh
@@ -1,22 +1,27 @@
 #!/bin/bash -x
 
 set -euo pipefail
-cd
-test -d .nvm || git clone https://github.com/creationix/nvm.git .nvm
+NVM_SH="${HOME}/.nvm/nvm.sh"
 
-for PROFILE_FILE in \
-    /etc/profile.d/node-env.sh \
-        ${HOME}/.my-profile \
-    ; do
-    test -w ${PROFILE_FILE} && SUDOOPT="" || SUDOOPT="sudo"
-    ${SUDOOPT} $(which insert-text-block) \
-               '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' \
-               ${PROFILE_FILE} <<"EOF"
+if ! test -e "${NVM_SH}"; then
+    cd
+    test -d .nvm || git clone https://github.com/creationix/nvm.git .nvm
+
+    for PROFILE_FILE in \
+        /etc/profile.d/node-env.sh \
+            ${HOME}/.my-profile \
+        ; do
+        test -w ${PROFILE_FILE} && SUDOOPT="" || SUDOOPT="sudo"
+        ${SUDOOPT} $(which insert-text-block) \
+                   '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' \
+                   ${PROFILE_FILE} <<"EOF"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 EOF
-done
+    done
+    source "${NVM_SH}"
+fi
 
 cat<<EOF
 NVM installed. Now run:
