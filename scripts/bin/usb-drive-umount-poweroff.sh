9b8da0d094d476bf50a9f96a89158633699044be
Ernesto Alfonso <erjoalgo@gmail.com>
2019-02-06 21:42:11 -0800

    allow specifying partition via arg. fall back to prompting user
    - skip umount if needed

--- scripts/bin/usb-drive-umount-poweroff.sh
+++ scripts/bin/usb-drive-umount-poweroff.sh
@@ -8,15 +8,27 @@ MNT=${HOME}/.usb-drive-symlink
 # MOUNT_LINE=$(sudo mount | grep -F "${REALPATH}")
 # PARTITION=$(grep -o "^[^ 	]*" <<< "${MOUNT_LINE}")
 
-for PART in $(findmnt -n -o SOURCE --target ${MNT}); do
-    if test -e ${PART}; then
-        PARTITION=${PART}
-        break
-    fi
-done
+PARTITION=${1} && shift
 
+if test -z "${PARTITION}"; then
+    for PART in $(findmnt -n -o SOURCE --target ${MNT}); do
+        if test -e ${PART}; then
+            PARTITION=${PART}
+            break
+        fi
+    done
+fi
+
+if test -z "${PARTITION}"; then
+    sudo blkid
+    read -p"could not locate partition, enter it manually: " \
+         PARTITION
+fi
 
 BLOCK_DEVICE="/dev/$(lsblk -no pkname ${PARTITION})"
 
-sudo umount ${MNT}
+if test -e "${MNT}"; then
+    sudo umount ${MNT}
+fi
+
 sudo udisksctl power-off --block-device  ${BLOCK_DEVICE}
