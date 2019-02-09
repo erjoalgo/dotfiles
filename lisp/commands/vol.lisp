edf0d2498d018e5f023a95618aea3d65cd709a25
Ernesto Alfonso <erjoalgo@gmail.com>
2019-02-09 03:43:12 -0800

    support pactl

--- lisp/commands/vol.lisp
+++ lisp/commands/vol.lisp
@@ -1,42 +1,77 @@
 (defvar *vol-muted* nil)
-(defvar *VOLSTEP* 3)
+(defvar *vol-backend* :amixer)
 
-(defcommand volchange (up percentage)
-    ((car (:y-or-n "raise volume?"))
-     (:number "percentage?"))
-  "change volume by a percentage, via amixer"
-  (let* ((sign (if up "+" "-"))
-         (cmd (format nil
-		      "amixer set Master ~D%~A"
-		      percentage
-		      sign)))
-    ;;(echo (format nil "running: ~A" cmd))
-    (run-shell-command cmd)
-    (if up (setq *vol-muted* nil))))
+(setf *vol-backend*
+      (cond
+        ((which "amixer") :amixer)
+        ((which "pactl") :pactl)
+        (t (warn "no volume cli found"))))
 
-(defun get-curr-volume ()
-  (let* ((out (run-shell-command "amixer get Master" t))
-         (vol (ppcre:register-groups-bind (vol) ("Playback.*?[[]([0-9]+)%]" out)
-                vol)))
-    vol))
+(defun vol (action
+            &key
+              (percent 3)
+              sink
+              (backend *vol-backend*))
+  (assert (member action '(:set :up :down :mute-toggle :get)))
+  (case backend
+    (:amixer
+     (setf sink (or sink "Master"))
+     (case action
+       ((:set :up :down)
+        (sb-ext:run-program
+         "amixer"
+         (list "set"
+               sink
+               (format nil "~D%~A" percent
+                       (case action
+                         (:set "")
+                         (:up "+")
+                         (:down "-"))))
+         :wait nil
+         :search t))
+       (:mute-toggle
+        (if *vol-muted*
+            (progn (vol :set :percent *vol-muted*)
+                   (setf *vol-muted* nil)
+                   t)
+            (progn (setf *vol-muted* (vol :set))
+                   (vol :set :percent 0)
+                   nil)))
+       (:get
+        (let* ((out (run-shell-command (format nil "amixer get ~A" sink) t))
+               (vol (ppcre:register-groups-bind (vol) ("Playback.*?[[]([0-9]+)%]" out)
+                      vol)))
+          (assert vol)
+          vol))))
+    (:pactl
+     (setf sink (or sink 0))
+     (case action
+       ((:set :up :down)
+        (sb-ext:run-program
+         "pactl"
+         (list "set-sink-volume"
+               (write-to-string sink)
+               (format nil "~A~D%"
+                       (case action
+                         (:set "")
+                         (:up "+")
+                         (:down "-"))
+                       percent))
+         :wait nil
+         :search t))
+       (:get (error "not implemented"))
+       (:mute-toggle (error "not implemented"))))
+    (t (error "unknown backend ~A" backend))))
 
-(defcommand volmute-toggle ()()
-  "mute/unmute"
-  (if (null *vol-muted*)
-      (progn
-        (setq *vol-muted* (get-curr-volume))
-        (run-shell-command "amixer set Master 0%")
-        (message "muted"))
-      (progn (run-shell-command (format nil
-                                        "amixer set Master ~A%"
-                                        *vol-muted*)
-                                (message "unmuted: ~A" *vol-muted*))
-             (setf *vol-muted* nil))))
-
-(defcommand volup ()() "volume up"
-            (volchange t *VOLSTEP*)
+(defcommand vol-up ()() "volume up"
+            (vol :up)
             (message "volup"))
 
-(defcommand voldown ()() "volume down"
-            (volchange nil *VOLSTEP*)
+(defcommand vol-down ()() "volume down"
+            (vol :down)
             (message "voldown"))
+
+(defcommand vol-mute-toggle ()() "volume mute"
+            (let ((muted (vol :mute-toggle)))
+              (message "vol ~Amute"
+                       (if muted "" "un"))))
