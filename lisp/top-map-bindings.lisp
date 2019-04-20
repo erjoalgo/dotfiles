20d48a0c4810c911966e5ebab1f43acc49b392a1
Ernesto Alfonso <erjoalgo@gmail.com>
2019-04-19 18:43:22 -0700

    bind "n" to take-scrot-snip

--- lisp/top-map-bindings.lisp
+++ lisp/top-map-bindings.lisp
@@ -172,7 +172,7 @@
    ("h" *help-map*)
    ("s" "scrot-cmd-anon" )
    ("S" "scrot-cmd-full-screen-anon" )
-   ("n" "run-shell-command snipit")
+   ("n" "take-scrot-snipit")
    ("o" "ocr-scrot-clipboard" )
    ("k" "speak-string" )
    ("l" "spell-clipboard" )
