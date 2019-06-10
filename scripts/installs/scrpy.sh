9268a435edb7cf34bc8893383a20383e70202252
Ernesto Alfonso <erjoalgo@gmail.com>
2019-06-09 22:58:38 -0700

    new file:   scripts/installs/scrpy.sh

---
+++ scripts/installs/scrpy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+# runtime dependencies
+sudo apt install -y ffmpeg libsdl2-2.0-0
+
+# client build dependencies
+sudo apt install -y make gcc git pkg-config meson ninja-build \
+     libavcodec-dev libavformat-dev libavutil-dev \
+     libsdl2-dev
+
+which meson ninja
+
+# server build dependencies
+sudo apt install -y openjdk-8-jdk
+
+cd ${HOME}/git
+URL=https://github.com/Genymobile/scrcpy
+BASE=$(basename "${URL}")
+test -d "${BASE}" || git clone "${URL}"
+cd "${BASE}"
+
+meson x --buildtype release --strip -Db_lto=true || true
+cd x
+ninja
+
+command -v scrcpy
