1c431fb5d09c87e59230935082f8c9eb88d6be37
Ernesto Alfonso <erjoalgo@gmail.com>
2019-06-09 17:29:18 -0700

    make BLOCK, MOUNT_POINT explicit opts

--- scripts/bin/mount-lvm.sh
+++ scripts/bin/mount-lvm.sh
@@ -8,12 +8,22 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
+function usage  {
+    echo "mount-lvm.sh -b <BLOCK> -m <MOUNT_POINT> [-u]"
+}
+
 DOMOUNT=true
-while getopts "hu" OPT; do
+while getopts "hub:m:" OPT; do
     case ${OPT} in
 	u)
 	    DOMOUNT=false
 	    ;;
+        b)
+            BLOCK=${OPTARG}
+            ;;
+        m)
+            MOUNT_POINT=${OPTARG}
+            ;;
 	h)
 	    less $0
 	    exit 0
@@ -22,24 +32,25 @@ while getopts "hu" OPT; do
 done
 
 
+if test -z "${MOUNT_POINT:-}" -o -z "${BLOCK:-}"; then
+    usage
+    exit 1
+fi
+
 if test ${DOMOUNT} = true; then
-    BLOCK=${1} && shift
-    MNT=${1} && shift
-
-    mkdir -p ${MNT}
-
+    mkdir -p ${MOUNT_POINT}
     udisksctl unlock -b ${BLOCK}
     vgchange -ay
 
     select MAPPER_ID in $(dmsetup ls | cut -f1); do
 	MAPPER=/dev/mapper/${MAPPER_ID}
-        sudo mount ${MAPPER} ${MNT}
+        sudo mount ${MAPPER} ${MOUNT_POINT}
         break
     done
 else
-    MNT=${@:$OPTIND:1}
-    BLOCK=$(mount | grep " on ${MNT}" | cut -f1 -d' ') || true
-    umount ${MNT} || true
+    MOUNT_POINT=${@:$OPTIND:1}
+    BLOCK=$(mount | grep " on ${MOUNT_POINT}" | cut -f1 -d' ') || true
+    umount ${MOUNT_POINT} || true
     select MAPPER in $(dmsetup ls | cut -f1); do
         BASE=$(basename ${MAPPER})
         vgchange -an ${BASE} || true
