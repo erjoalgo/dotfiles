6b69e3aac0bf835b08ad3494c77cc810a254e71f
Ernesto Alfonso <erjoalgo@gmail.com>
2019-07-14 17:12:21 -0700

    new file:   scripts/installs/openproject-debian-9.sh

---
+++ scripts/installs/openproject-debian-9.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+wget -qO- https://dl.packager.io/srv/opf/openproject/key | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+
+wget -O /etc/apt/sources.list.d/openproject.list \
+     https://dl.packager.io/srv/opf/openproject/stable/9/installer/debian/9.repo
+
+sudo apt-get update
+sudo apt-get install -y openproject
