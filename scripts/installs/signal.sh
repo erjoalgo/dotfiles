fbc1ec7b0453ff253f6ab08ee107285728b45297
Ernesto Alfonso <erjoalgo@gmail.com>
2019-07-21 21:59:42 -0700

    new file:   scripts/installs/signal.sh

---
+++ scripts/installs/signal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+curl -s https://updates.signal.org/desktop/apt/keys.asc \
+    | sudo apt-key add -
+
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" \
+    | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+sudo apt-get install -y apt-transport-https
+
+sudo apt update && sudo apt install -y signal-desktop
