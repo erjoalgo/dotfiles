c3314bb92743755359b2ace2781482a5237c6485
Ernesto Alfonso <erjoalgo@gmail.com>
2019-07-28 01:14:54 +0000

    modernize script to setup new mysql user and db.
    - fix using ${USER} built-in env variable
    - fix quoting
    - use -p to automatically prompt for mysql admin password
    - doc nit
    - use getopt

--- scripts/bin/mysql-create-user-and-db.sh
+++ scripts/bin/mysql-create-user-and-db.sh
@@ -1,29 +1,46 @@
 #!/bin/bash -x
 
-
-DBNAME=${1} && shift
-USER=${1:-${DBNAME}} && shift
-HOST=${1:-localhost} && shift
-
 set -euo pipefail
 
-echo 'enter mysql password for root' -n
-read -s ADMINPASS
-echo
+while getopts "d:u:h" OPT; do
+    case ${OPT} in
+    d)
+        DBNAME=${OPTARG}
+        ;;
+    u)
+	DBUSER=${OPTARG}
+	;;
+    t)
+	HOST=${OPTARG}
+	;;
+    p)
+	DROP=true
+	;;
+    h)
+        less $0
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND -1))
 
-if ! test -n "${DBNAME}" -a -n "${USER}" -a -n "${HOST}"; then
-    echo "missing arguments" && exit ${LINENO}
+DBUSER=${DBUSER:-${DBNAME:-}}
+HOST=${HOST:-localhost}
+DROP=${DROP:-}
+
+
+if ! test -n "${DBNAME:-}" -a -n "${DBUSER:-}" -a -n "${HOST:}"; then
+    echo "missing arguments"
+    exit ${LINENO}
 fi
 
 if grep '-' <<< "${DBNAME}"; then
-    echo "dbname cannot have hyphens" && exit ${LINENO}
+    echo "dbname cannot include hyphens" && exit ${LINENO}
 fi
 
-DROP=${DROP:-}
-
 if test -n "${DROP}"; then
-    mysql -uroot -p${ADMINPASS} <<EOF
-GRANT USAGE ON *.* TO '${USER}'@'localhost';
+    sudo mysql -uroot -p <<EOF
+GRANT USAGE ON *.* TO '${DBUSER}'@'localhost';
 drop USER '${USER}'@'localhost';
 drop database if exists ${DBNAME};
 FLUSH PRIVILEGES;
@@ -32,22 +49,21 @@ EOF
     # mysql -u${USER} -p${PASS} < /dev/null && test 0 -ne $?
     echo successful drop
 else
-    # cat <<EOF
-    echo 'enter mysql password for "${USER}"' -n
-    read -s PASS
-    echo
+    if test -z "${PASS:-}"; then
+	read -sp "enter mysql password for ${DBUSER}: " PASS
+    fi
 
-    mysql -uroot -p${ADMINPASS} <<EOF
-CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
-CREATE DATABASE ${DBNAME};
-GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${USER}'@'localhost';
+    sudo mysql <<EOF
+-- CREATE DATABASE ${DBNAME};
+CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY '${PASS}';
+GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${DBUSER}'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
-    mysql -u${USER} -p${PASS} < /dev/null
+    sudo mysql -u${DBUSER} -p < /dev/null
     cat <<EOF
 dbname: ${DBNAME}
-username: ${USER}
+username: ${DBUSER}
 EOF
 fi
 # Local Variables:
