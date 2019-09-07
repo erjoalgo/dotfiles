4076923c9672122d115cdb015d91db8e6e4bf996
Ernesto Alfonso <erjoalgo@gmail.com>
2019-09-07 12:16:03 -0700

    improve spelling support

--- lisp/commands/speech.lisp
+++ lisp/commands/speech.lisp
@@ -11,17 +11,72 @@
 			:error t
 			:wait t)))
 
-(defcommand speak-string (text)
-    ((:string "enter string to speak: " ))
-  "speak some text"
+(defun speak-string (text &key (speed-wpm 160) (word-gap-ms 10))
   (SB-EXT:RUN-PROGRAM "espeak"
-		      (list text)
+		      (list (format nil "-s~D" speed-wpm)
+                            (format nil "-g~D" word-gap-ms)
+                            text)
 		      :search t
 		      :output t
 		      :error t
 		      :wait t))
 
+(defcommand speak-string-cmd (text)
+    ((:string "enter string to speak: " ))
+  "speak some text"
+  (speak-string text))
+
 (defcommand spell-clipboard () ()
   "spell clipboard contents, character by character"
-  (speak-string
-   (cl-ppcre:regex-replace-all "(.)" (GET-X-SELECTION) " \\1")))
+  (let* ((word (GET-X-SELECTION)))
+    (spell-word word)))
+
+(defun spell-char-line (c)
+  (cond
+    ((ALPHA-CHAR-P c)
+     (let* ((c-lower (char-downcase c))
+            (word (cdr (assoc c-lower char-sample-word)))
+            (is-capital (not (eq c-lower c))))
+           (format nil "~A ~A as in ~A"
+                   (if is-capital "capital" "")
+                   (cond ((eq c-lower #\a) "'A'")
+                         (t (format nil "'~C'" (char-upcase c))))
+                   (or word
+                       (progn
+                         (warn "missing sample word for char ~C" c-lower)
+                         (format nil "~C" c-lower))))))
+    ((DIGIT-CHAR-P c)
+     (format nil "the number ~C" c))
+    ((eq c #\Space) "Space")
+    (t (format nil "~C" c))))
+
+(defun spell-word (word)
+  (let* ((spelling-lines
+          (loop for c across word collect (spell-char-line c)))
+         (preamble (format nil "spelling ~D characters" (length word))))
+    (speak-string preamble)
+    (sleep 1)
+    (speak-string (format nil "~{~A~^, ~}" spelling-lines))
+    spelling-lines))
+
+(defparameter spell-words
+  '("Yokohama" "Guadalajara" "Atlanta"
+    "Houston"
+    "Jerusalem" "Paris" "India"
+    "Beijing" "Chicago" "Xochimilco" "Florence"
+    "Richmond" "Detroit" "Queretaro"
+    "Washington" "Oakland" "Korea" "Zimbabwe"
+    "Egypt" "Uruguay" "Vietnam"
+    "London" "Shanghai" "Mexico"
+    "New York" "Tokyo"))
+
+(defparameter char-sample-word
+  (loop
+     with alist = nil
+     for word in spell-words
+     as c = (aref word 0)
+     as key = (CHAR-DOWNCASE c)
+     as val = (assoc key alist)
+     unless (or val (not (ALPHA-CHAR-P c)))
+     do (push (cons key word) alist)
+     finally (return alist)))
