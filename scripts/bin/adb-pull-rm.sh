4a6f04ac2eb013f6e8ef1c4cab86d69350268ed7
Ernesto Alfonso <erjoalgo@gmail.com>
2019-11-22 19:27:32 -0800

    support path prefix and multiple files in a single invocation

--- scripts/bin/adb-pull-rm.sh
+++ scripts/bin/adb-pull-rm.sh
@@ -6,31 +6,49 @@ function usage  {
     echo "usage: $(basename ${0}) <ANDROID_FILE_PATH>"
 }
 
-if test $# -ne 1; then
-    usage
-    exit 1
-fi
+PATH_PREFIX=""
+while getopts "hp:" OPT; do
+    case ${OPT} in
+    p)
+        PATH_PREFIX=${OPTARG}
+        ;;
+    h)
+        less $0
+        usage
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND -1))
 
 ADB_CMD=${ADB_CMD:-"sudo adb"}
-ANDROID_PATH="${1}"
-NAME=$(basename "${ANDROID_PATH}")
 
-# TODO make sure 'adb shell ls' succeeds
-OUT=$(${ADB_CMD} shell ls "${ANDROID_PATH}")
+for FILE in ${*}; do
+    echo ${FILE}
+    ANDROID_PATH="${PATH_PREFIX}/$(tr -d '\r' <<< ${FILE})"
+    NAME=$(basename "${ANDROID_PATH}")
 
-echo "${ANDROID_PATH}"
+    # TODO make sure 'adb shell ls' succeeds
+    OUT=$(${ADB_CMD} shell ls "${ANDROID_PATH}")
 
-! grep -i "no such file" <<< "${OUT}" || false
+    echo "${ANDROID_PATH}"
 
-test -e "${NAME}" || ${ADB_CMD} pull "${ANDROID_PATH}"
+    if grep -i "no such file" <<< "${OUT}"; then
+        echo "${OUT}"
+        exit ${LINENO}
+    fi
+    if ! test -e "${NAME}"; then
+        ${ADB_CMD} pull "${ANDROID_PATH}"
+    fi
 
-MD5_HDD=$(md5sum "${NAME}" | tr ' ' '\t' | cut -f1)
-MD5_ANDROID=$(${ADB_CMD} shell md5sum "${ANDROID_PATH}" | tr ' ' '\t' | cut -f1)
+    MD5_HDD=$(md5sum "${NAME}" | tr ' ' '\t' | cut -f1)
+    MD5_ANDROID=$(${ADB_CMD} shell md5sum "${ANDROID_PATH}" | tr ' ' '\t' | cut -f1)
 
-if test "${MD5_ANDROID}" = "${MD5_HDD}"; then
-    ${ADB_CMD} shell rm "${ANDROID_PATH}"
-else
-    echo "unexpected checksum for ${ANDROID_PATH}: ${MD5_ANDROID} vs ${MD5_HDD}"
-    echo "${ANDROID_PATH}\t$(${ADB_CMD} shell du -b ${ANDROID_PATH})"
-    echo "${NAME}\t$(du -b ${NAME})"
-fi
+    if test "${MD5_ANDROID}" = "${MD5_HDD}"; then
+        ${ADB_CMD} shell rm "${ANDROID_PATH}"
+    else
+        echo "unexpected checksum for ${ANDROID_PATH}: ${MD5_ANDROID} vs ${MD5_HDD}"
+        echo "${ANDROID_PATH}\t$(${ADB_CMD} shell du -b ${ANDROID_PATH})"
+        echo "${NAME}\t$(du -b ${NAME})"
+    fi
+done
