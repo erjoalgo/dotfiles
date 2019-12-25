8109edc488452fe94075741f76e076ec258bd530
Ernesto Alfonso <erjoalgo@gmail.com>
2019-12-25 01:37:20 +0000

    don't rebuild sbcl if installed

--- scripts/installs/sbcl.sh
+++ scripts/installs/sbcl.sh
@@ -2,33 +2,37 @@
 
 set -euo pipefail
 
-URL=http://prdownloads.sourceforge.net/sbcl/sbcl-1.5.9-source.tar.bz2
+if ! command -v sbcl; then
+    URL=http://prdownloads.sourceforge.net/sbcl/sbcl-1.5.9-source.tar.bz2
 
-BASE=$(basename "${URL}")
-DIR_PREFIX=$(grep -o "sbcl-[0-9.]*"  <<< "${BASE}")
+    BASE=$(basename "${URL}")
+    DIR_PREFIX=$(grep -o "sbcl-[0-9.]*"  <<< "${BASE}")
 
-TOP=${HOME}/src
-mkdir -p ${TOP}
-cd ${TOP}
+    TOP=${HOME}/src
+    mkdir -p ${TOP}
+    cd ${TOP}
 
-function find-dir  {
-    find . -name "${DIR_PREFIX}*" -type d |  \
-        tr -d '\n'
-}
+    function find-dir  {
+        find . -name "${DIR_PREFIX}*" -type d |  \
+            tr -d '\n'
+    }
 
-DIR=$(find-dir)
-
-if ! test -d "${DIR}"; then
-    test -e ${BASE} || wget "${URL}"
-    test -e ${BASE}
-    tar -C ~/src/ -axvf ${BASE}
     DIR=$(find-dir)
+
+    if ! test -d "${DIR}"; then
+        test -e ${BASE} || wget "${URL}"
+        test -e ${BASE}
+        tar -C ~/src/ -axvf ${BASE}
+        DIR=$(find-dir)
+    fi
+
+    test -d "${DIR}"
+
+    cd "${DIR}"
+
+    sudo apt-get install -y zlib1g-dev
+    sh make.sh --fancy
+    sudo ./install.sh
 fi
 
-test -d "${DIR}"
-
-cd "${DIR}"
-
-sh make.sh --fancy
-sudo ./install.sh
 sudo apt-get install -y make rlwrap
