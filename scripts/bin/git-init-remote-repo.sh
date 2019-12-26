3050d306793c8d57e9ee7d21f4754c014c29ec9b
Ernesto Alfonso <erjoalgo@gmail.com>
2019-12-26 03:18:48 -0800

    force the user to specify new git remote name

--- scripts/bin/git-init-remote-repo.sh
+++ scripts/bin/git-init-remote-repo.sh
@@ -6,12 +6,12 @@ set -euo pipefail
 git status
 
 function usage {
-    echo "$(basename ${0}) -t SSH_USERHOST [ -o SSH_opts ] [ -r local_repo ]
+    echo "$(basename ${0}) -t SSH_USERHOST [ -o SSH_opts ] -r REMOTE_NAME [ -n repo_name ]
 [ -d remote_git_bare_prefix ]"
 }
 
 SSH_OPTS=""
-while getopts "ht:o:r:d:" OPT; do
+while getopts "ht:o:r:n:d:" OPT; do
     case ${OPT} in
     t)
         SSH_USERHOST=${OPTARG}
@@ -19,11 +19,14 @@ while getopts "ht:o:r:d:" OPT; do
     o)
         SSH_OPTS=${OPTARG}
         ;;
-    r)
+    n)
         REPO=${OPTARG}
         ;;
     d)
         SRV_PREFIX=${OPTARG}
+        ;;
+    r)
+        REMOTE_NAME=${OPTARG}
         ;;
     h)
         less $0
@@ -36,7 +39,7 @@ done
 
 REPO=${REPO:-$(basename $(pwd))}
 SRV_PREFIX=${SRV_PREFIX:-/opt/git}
-if test -z "${SSH_USERHOST}"; then
+if test -z "${SSH_USERHOST}" -o -z "${REMOTE_NAME}"; then
     usage
     exit 1
 fi
@@ -56,14 +59,6 @@ EOF
 REMOTE_URL="ssh://git@${SSH_USERHOST}${SRV_PREFIX}/${REPO}"
 echo ${REMOTE_URL}
 ORIGIN_NAME=origin
-for NAME in origin ${SSH_USERHOST} ${SSH_USERHOST}-${RANDOM}; do
-    REMOTE_NAME=${NAME}
-    if test "$(git config --get remote.${NAME}.url)" = "${REMOTE_URL}"; then
-	break
-    elif git remote add ${REMOTE_NAME} ${REMOTE_URL}; then
-	break
-    fi
-done
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 git fetch ${REMOTE_NAME}
