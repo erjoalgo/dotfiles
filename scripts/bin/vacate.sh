41a26310461b171e4213d4393bc90d0e30b31081
Ernesto Alfonso <erjoalgo@gmail.com>
2019-12-26 01:52:19 -0800

    add script to safely vacate a machine without data loss

---
+++ scripts/bin/vacate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+for DIR in ${HOME}/git/*; do
+    cd "${DIR}"
+    pwd
+    if git status &> /dev/null; then
+        if ! git-repo-is-safe-to-delete.sh; then
+            echo "git repo at ${DIR} is not safe to delete"
+            exit ${LINENO}
+        fi
+    fi
+done
