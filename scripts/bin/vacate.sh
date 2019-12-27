fd9450221c0cc237646165342d1a73f83ca71d62
Ernesto Alfonso <erjoalgo@gmail.com>
2019-12-26 22:51:56 -0800

    logging

--- scripts/bin/vacate.sh
+++ scripts/bin/vacate.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 # consider directories in reverse mtime order, so that newly-edited/fixed repos
 # are considered last
 for DIR in $(find ${HOME}/git/ /opt/git -maxdepth 1 -type d | xargs ls -1trd); do
+    echo "considering ${DIR}"
     cd "${DIR}"
-    pwd
     if git status &> /dev/null; then
         if ! git-repo-is-safe-to-delete.sh; then
             echo "git repo at ${DIR} is not safe to delete"
