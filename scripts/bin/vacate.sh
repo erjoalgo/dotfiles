04318d453040c6470d1afaca8c169265a74cce7f
Ernesto Alfonso <erjoalgo@gmail.com>
2019-12-26 22:51:14 -0800

    consider repos in reverse mtime order

--- scripts/bin/vacate.sh
+++ scripts/bin/vacate.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-for DIR in $(find ${HOME}/git/ /opt/git -maxdepth 1 -type d); do
+# consider directories in reverse mtime order, so that newly-edited/fixed repos
+# are considered last
+for DIR in $(find ${HOME}/git/ /opt/git -maxdepth 1 -type d | xargs ls -1trd); do
     cd "${DIR}"
     pwd
     if git status &> /dev/null; then
