8d15240989eddfa5c04c0e84a69658f3ec12db72
Ernesto Alfonso <erjoalgo@gmail.com>
2019-12-29 23:29:08 -0800

    fail on non-git repos under ~/git/

--- scripts/bin/vacate.sh
+++ scripts/bin/vacate.sh
@@ -7,10 +7,12 @@ set -euo pipefail
 for DIR in $(find ${HOME}/git/ /opt/git -maxdepth 1 -type d | xargs ls -1trd); do
     echo "considering ${DIR}"
     cd "${DIR}"
-    if git status &> /dev/null; then
-        if ! git-repo-is-safe-to-delete.sh; then
-            echo "git repo at ${DIR} is not safe to delete"
-            exit ${LINENO}
-        fi
+    if ! git status &> /dev/null; then
+        echo "non git repository: ${DIR}"
+        exit ${LINENO}
+    fi
+    if ! git-repo-is-safe-to-delete.sh; then
+        echo "git repo at ${DIR} is not safe to delete"
+        exit ${LINENO}
     fi
 done
