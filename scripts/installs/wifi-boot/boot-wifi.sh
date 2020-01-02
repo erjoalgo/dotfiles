25182cfa86399883c7de14bdc73ef931416029a4
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-01 23:02:44 -0800

    fix wifi-boot.sh for firmare-iwlwifi

--- scripts/installs/wifi-boot/boot-wifi.sh
+++ scripts/installs/wifi-boot/boot-wifi.sh
@@ -4,20 +4,30 @@ set -euo pipefail
 
 LINE=$(lspci | grep -i network)
 
+function add_non-free_apt_source {
+    sudo tee /etc/apt/sources.list.d/non-free.list <<EOF
+deb http://deb.debian.org/debian buster non-free
+deb-src http://deb.debian.org/debian buster non-free
+EOF
+    sudo apt-get update
+}
+
 if grep -i centrino <<< "$LINE"; then
-    sudo apt-get install firmware-iwlwifi
-    sudo modprobe -r iwlwifi
+    add_non-free_apt_source
+    sudo apt-get install -u firmware-iwlwifi
+    sudo modprobe -r iwlwifi || true
     sudo modprobe iwlwifi
 elif false; then
     echo false
+else
+    echo "unknown wifi card: ${LINE}"
+    exit $LINENO
 fi
-sudo apt-get install -y expect
+sudo apt-get install -y wireless-tools iw wpasupplicant
+
+sudo iwconfig
 
 # apt-get install -y firmware-realtek
-# apt-get install -y firmware-iwlwifi
-
 # modprobe -r realtek
 # modprobe realtek
-
-# 108  modprobe -r iwlwifi; modproblem iwlwifi
 # 109  modprobe -r realtek ; modprobe realtek
