85dc1f52aecf87294dd39593df4e22e394c9b120
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-02 23:38:48 -0800

    add -h flag. remove -x verbosity

--- scripts/bin/android-find-pull-rm.sh
+++ scripts/bin/android-find-pull-rm.sh
@@ -1,7 +1,17 @@
-#!/bin/bash -x
+#!/bin/bash
 
-# example to pull videos and images:
-# android-find-pull-rm.sh sdcard/DCIM -name "'*mp4'" -o -name "'*jpg'"
+while getopts "h" OPT; do
+    case ${OPT} in
+    h)
+        # example to pull videos and images:
+        # android-find-pull-rm.sh sdcard/DCIM -name "'*mp4'" -o -name "'*jpg'"
+        less $0
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
 
 FIND_ARGS=${*}
 echo ${FIND_ARGS}
