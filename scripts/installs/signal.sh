b020ec5272954377a901d503240edf9f8f8b0316
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-02 23:39:10 -0800

    update signal.sh script
    - formatting
    - dont keep appending to sources file
    - fix executable permissions

--- scripts/installs/signal.sh
+++ scripts/installs/signal.sh
@@ -2,12 +2,13 @@
 
 set -euo pipefail
 
-curl -s https://updates.signal.org/desktop/apt/keys.asc \
-    | sudo apt-key add -
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" \
-    | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+    | sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
 sudo apt-get install -y apt-transport-https
 
 sudo apt update && sudo apt install -y signal-desktop
+
+sudo chmod 4755 /opt/Signal/chrome-sandbox
