e02e9ef78a2569ca9ff2d9d160f5dba02e849d82
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-03 19:02:11 -0800

    update alsa packge names

--- scripts/installs/alsa-install.sh
+++ scripts/installs/alsa-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 if ! command -v alsamixer; then
-    sudo apt-get install -y alsa-base alsa-utils
+    sudo apt-get install -y alsa-tools alsa-utils
 fi
 
 if ! command -v mpg321; then
