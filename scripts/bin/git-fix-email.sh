3a7ecd7790467677aaf3cce07c697e8882c98a90
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-06 20:39:31 -0800

    Revert "remove one level of interpolation"
    This reverts commit 2a6815bca10513071ccd4365f78c0fe6215fc057.

--- scripts/bin/git-fix-email.sh
+++ scripts/bin/git-fix-email.sh
@@ -2,18 +2,18 @@
 
 set -euo pipefail
 
-WRONG_EMAIL=$1 && shift
-NEW_EMAIL=$1 && shift
+export WRONG_EMAIL="$1" && shift
+export NEW_EMAIL="$1" && shift
 
-git filter-branch -f --env-filter "
-if [ \$GIT_COMMITTER_EMAIL = $WRONG_EMAIL ]
+git filter-branch -f --env-filter '
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
 then
-    export GIT_COMMITTER_EMAIL=$NEW_EMAIL
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
 fi
-if [ \$GIT_AUTHOR_EMAIL = $WRONG_EMAIL ]
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
 then
-    export GIT_AUTHOR_EMAIL=$NEW_EMAIL
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
 fi
-" --tag-name-filter cat -- --branches --tags
+' --tag-name-filter cat -- --branches --tags
 
 # git filter-branch -f --env-filter 'export GIT_AUTHOR_EMAIL="victor@ge.com" GIT_AUTHOR_NAME="Victor"' origin/master..tmp
