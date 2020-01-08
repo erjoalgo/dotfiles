09a3709e69499ae040cbf89560df84847ff9cfe1
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-08 08:24:26 +0000

    modernize tor-install.sh
    - use set -e instead of "|| exit ${LINENO}"
    - use /etc/apt/sources.list.d dedicated file
    - add deb-src in addition to deb line
    - call service tor start

--- scripts/installs/tor-install.sh
+++ scripts/installs/tor-install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash -x
 
-if command -v tor; then
-    exit 0
-fi
+set -euo pipefail
 
 CODENAME=$(lsb_release -c | cut -d: -f2 | tr -d '[:space:]')
-LINE="deb     http://deb.torproject.org/torproject.org ${CODENAME} main"
+sudo tee /etc/apt/sources.list.d/torproject.list <<EOF
+deb     http://deb.torproject.org/torproject.org ${CODENAME} main
+deb-src     http://deb.torproject.org/torproject.org ${CODENAME} main
+EOF
 
-APT_SOURCES=/etc/apt/sources.list
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc \
+    | gpg --import
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
-if ! grep -F "${LINE}" ${APT_SOURCES}; then
-    echo "${LINE}" | sudo tee -a ${APT_SOURCES} || exit ${LINENO}
-fi
-
-
-gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 || exit ${LINENO}
-gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add - || exit ${LINENO}
-
-sudo apt-get update || exit ${LINENO}
+sudo apt-get update
 sudo apt-get install -y deb.torproject.org-keyring tor
 
 cat <<EOF >> ~/.torrc
 TestSocks 1
 SafeSocks 1
 EOF
+sudo service tor start
