5d280c4d8721f9e1c132f6e8aaba258ce08830f6
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-08 23:50:15 -0800

    refactor usb-drive-umount-poweroff.sh
    - fall-back to interactive partition selection

--- scripts/bin/usb-drive-umount-poweroff.sh
+++ scripts/bin/usb-drive-umount-poweroff.sh
@@ -4,31 +4,44 @@ set -euo pipefail
 
 MNT=${HOME}/.usb-drive-symlink
 
-# REALPATH=$(python -c "import os; print(os.path.realpath('${MNT}'))")
-# MOUNT_LINE=$(sudo mount | grep -F "${REALPATH}")
-# PARTITION=$(grep -o "^[^ 	]*" <<< "${MOUNT_LINE}")
+while getopts "m:h" OPT; do
+    case ${OPT} in
+    m)
+        MOUNT_POINT=${OPTARG}
+        ;;
+    h)
+        less $0
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND -1))
 
-PARTITION=${1:-} && shift || true
+MOUNT_POINT=${MOUNT_POINT:-}
+PARTITION=${PARTITION:-}
 
-if test -z "${PARTITION}"; then
-    for PART in $(findmnt -n -o SOURCE --target ${MNT}); do
+if test -n "${MOUNT_POINT}"; then
+    for PART in $(findmnt -n -o SOURCE --target ${MOUNT_POINT}); do
         if test -e ${PART}; then
             PARTITION=${PART}
             break
         fi
     done
+else
+    select PARTITION in $(sudo blkid | cut -f1 -d:); do
+        break
+    done
 fi
 
-if test -z "${PARTITION}"; then
-    sudo blkid
-    read -p"could not locate partition, enter it manually: " \
-         PARTITION
-fi
-
+test -e "${PARTITION}"
 BLOCK_DEVICE="/dev/$(lsblk -no pkname ${PARTITION})"
 
-if test -e "${MNT}"; then
-    sudo umount ${MNT}
+if test -z "${MOUNT_POINT}"; then
+    MOUNT_POINT=$(sudo mount | grep -Po "(?<=${PARTITION} on )[^ ]+") || true
 fi
 
-sudo udisksctl power-off --block-device  ${BLOCK_DEVICE}
+if test -e "${MOUNT_POINT}"; then
+    sudo umount ${MOUNT_POINT}
+fi
+
+sudo udisksctl power-off --block-device ${BLOCK_DEVICE}
