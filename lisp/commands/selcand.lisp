4366b3a2266e6190f5a6f0d12a50cc3a41d876ae
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-20 00:45:19 -0800

    adapt selcand util to stumpwm

---
+++ lisp/commands/selcand.lisp
@@ -0,0 +1,45 @@
+(defvar *selcand-default-hints*
+  "1234acdefqrstvwxz"
+  "Default hint chars.")
+
+(defun selcand-hints (candidates &optional chars)
+  "Return an alist (HINT . CAND) for each candidate in CANDIDATES.
+
+  Each hint consists of characters in the string CHARS."
+  (setf chars (or chars *selcand-default-hints*))
+  (assert candidates)
+  (loop
+     with hint-width = (ceiling (log (length candidates) (length chars)))
+     with hints = '("")
+     for wi below hint-width do
+       (setf hints
+             (loop for c across chars
+                append (mapcar (lambda (rest)
+                                 (format nil "~C~A" c rest))
+                               hints)))
+     finally (return (loop for hint in hints
+                        for cand in candidates
+                        collect (cons hint cand)))))
+
+(defun selcand-select (candidates &optional prompt stringify-fn
+                                    autoselect-if-single
+                                    no-hints)
+  "Use PROMPT to prompt for a selection from CANDIDATES."
+  (let* ((sep ") ")
+         (stringify-fn (or stringify-fn #'prin1-to-string))
+         (hints-cands (if no-hints
+                          (mapcar (lambda (cand)
+                                    (cons (funcall stringify-fn cand) cand))
+                                  candidates)
+                          (loop for (hint . cand) in (selcand-hints candidates)
+                             collect (cons (concat hint sep (funcall stringify-fn cand))
+                                           cand))))
+         (choices (mapcar #'car hints-cands))
+         (prompt (or prompt "select candidate: "))
+         (hint-selected (if (and autoselect-if-single (null (cdr choices)))
+                     (car choices)
+                     (completing-read (current-screen) prompt
+                                      choices :require-match t)))
+         (cand (cdr (assoc hint-selected hints-cands :test #'equal))))
+    (assert (or (null hint-selected) cand))
+    cand))
