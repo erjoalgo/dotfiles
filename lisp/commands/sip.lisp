5f471e9d0f81ba496bf6f3220a5eb9f2225877f1
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-19 23:06:13 -0800

    don't prefix 10-digit numbers with 1

--- lisp/commands/sip.lisp
+++ lisp/commands/sip.lisp
@@ -7,7 +7,7 @@
 (defun sip-call (number &key (sip-host "sanjose2.voip.ms"))
   (assert number)
   (let* ((number-clean (ppcre:regex-replace-all "[^0-9]" number ""))
-         (intl-prefix-opt (if (eq (length number-clean) 10) "1" ""))
+         (intl-prefix-opt "")
          (call-arg (format nil "sip:~A~A@~A"
                            intl-prefix-opt number-clean sip-host)))
     (echo (format nil "calling ~A" call-arg))
