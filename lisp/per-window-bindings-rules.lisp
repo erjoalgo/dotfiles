92d378b0aa10a47a17f0162a2e747c3d0613b0ae
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-24 16:10:40 -0800

    add dedicated names for per-window-binding commands

--- lisp/per-window-bindings-rules.lisp
+++ lisp/per-window-bindings-rules.lisp
@@ -1,20 +1,26 @@
 (setf *per-window-bindings-rules*
       `(
   (,stumpwm::*browser-classes*
-   ("F2" (send-fake-key (current-window) (kbd "C-TAB")))
-   ("F1" (send-fake-key (current-window) (kbd "C-ISO_Left_Tab")))
-   ("F4" (send-fake-key (current-window) (kbd "C-F4")))
-   ("F9" (send-fake-key (current-window) (kbd "C--")))
-   ("F10" (send-fake-key (current-window) (kbd "C-=")))
+   ("F2" "send-control-tab"
+         (send-fake-key (current-window) (kbd "C-TAB")))
+   ("F1" "send-control-shift-tab"
+         (send-fake-key (current-window) (kbd "C-ISO_Left_Tab")))
+   ("F4" "send-control-f4"
+         (send-fake-key (current-window) (kbd "C-F4")))
+   ("F9" "send-control--"
+         (send-fake-key (current-window) (kbd "C--")))
+   ("F10" "send-control-=" (send-fake-key (current-window) (kbd "C-=")))
    ;;("F12" (echo "hola f12" ))
-   ("S-F4" (send-fake-key (current-window) (kbd "C-T")))
-   ("F3" (send-fake-key (current-window) (kbd "C-g")) )
+   ("S-F4" "send-control-t" (send-fake-key (current-window) (kbd "C-T")))
+   ("F3" "send-control-g" (send-fake-key (current-window) (kbd "C-g")) )
    ;;("S-F4" (send-fake-key (current-window) (kbd "C-S-F4")))
-   ("M-F1" (send-fake-key (current-window) (kbd "M-Left")))
-   ("M-F2" (send-fake-key (current-window) (kbd "M-Right")))
+   ("M-F1" "send-alt-lefta" (send-fake-key (current-window) (kbd "M-Left")))
+   ("M-F2" "send-alt-right" (send-fake-key (current-window) (kbd "M-Right")))
 
-   ("S-F1" (send-fake-key (current-window) (kbd "C-S-SunPageUp")))
-   ("S-F2" (send-fake-key (current-window) (kbd "C-S-SunPageDown")))
+   ("S-F1" "send-control-shift-pgup"
+           (send-fake-key (current-window) (kbd "C-S-SunPageUp")))
+   ("S-F2" "send-control-shift-pagedown"
+           (send-fake-key (current-window) (kbd "C-S-SunPageDown")))
 
    ;;("d" (send-fake-key (current-window) (kbd "SunPageDown")))
 
@@ -25,10 +31,11 @@
    )
 
   (("x-terminal-emulator" "X-terminal-emulator" "roxterm" "Roxterm" "Gnome-terminal")
-   ("SunPageUp" (send-fake-key (current-window) (kbd "S-SunPageUp")) (echo "sunpageup"))
-   ("SunPageDown" (send-fake-key (current-window) (kbd "S-SunPageDown")) (echo "sunpagedown"))
-   ("F1" (send-fake-key (current-window) (kbd "C-SunPageUp")))
-   ("F2" (send-fake-key (current-window) (kbd "C-SunPageDown")))
+   ("SunPageUp" "send-pgup" (send-fake-key (current-window) (kbd "S-SunPageUp")) (echo "sunpageup"))
+   ("SunPageDown" "send-pgdown" (send-fake-key (current-window) (kbd "S-SunPageDown")) (echo "sunpagedown"))
+   ("F1" "send-control-pageup" (send-fake-key (current-window) (kbd "C-SunPageUp")))
+   ("F2" "send-control-pagedown"
+         (send-fake-key (current-window) (kbd "C-SunPageDown")))
 					;("F4" (send-fake-key (current-window) (kbd "C-F4")))
 					;("M-F1" (send-fake-key (current-window) (kbd "M-Left")))
    )
