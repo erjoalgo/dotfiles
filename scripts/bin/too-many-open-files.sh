a7b7ea7532f0ae34de20235820de914f1f30e704
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-24 16:14:59 -0800

    show (OPEN-FILE-COUNT, PID, COMMAND) last.
    - support changing the PID for which to list all open files

--- scripts/bin/too-many-open-files.sh
+++ scripts/bin/too-many-open-files.sh
@@ -3,11 +3,15 @@
 # debug "too many open files" error
 set -euo pipefail
 
-while getopts "n:h" OPT; do
+while getopts "n:t:h" OPT; do
     case ${OPT} in
     n)
         TOP_N=${OPTARG}
         ;;
+    t)
+      # pid for which to list all open files
+      TOP_PID=${OPTARG}
+      ;;
     h)
         less $0
         exit 0
@@ -15,7 +19,6 @@ while getopts "n:h" OPT; do
     esac
 done
 shift $((OPTIND -1))
-
 TOP_N=${TOP_N:-10}
 
 TOP=$(
@@ -23,18 +26,18 @@ TOP=$(
     echo -e "$(sudo ls -1 $PROC | wc -l)\t$(basename $(dirname $PROC))\n"
     done | sort -n | tail -n ${TOP_N})
 
-echo "${TOP}"
 
+OUT=""
 OLDIFS=$IFS
 IFS=$'\n'
 for LINE in ${TOP}; do
-    echo ${LINE}
     COUNT=$(cut -f1  <<< "${LINE}")
     PID=$(cut -f2  <<< "${LINE}")
-    echo "${COUNT} $(ps -fp ${PID} -o command=)"
+    OUT+="${COUNT}\t$(ps -fp ${PID} -o pid=,command=)\n"
 done
 IFS=$OLDIFS
 
-TOP_PID=${PID}
+TOP_PID=${TOP_PID:-${PID}}
 sudo ls -l /proc/${TOP_PID}/fd
 
+echo -e ${OUT}
