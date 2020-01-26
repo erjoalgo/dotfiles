6c2dc3678ab6605d4b6300d5eda560fc3dc0e073
Ernesto Alfonso <erjoalgo@gmail.com>
2020-01-25 19:43:21 -0800

    remember useful go "ws" command

--- scripts/installs/go-installs.sh
+++ scripts/installs/go-installs.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 for REPO in\
     golang.org/x/tools/cmd/goimports \
-    github.com/golang/lint \
+        github.com/golang/lint \
+        github.com/hashrocket/ws
     ; do
     ${GOEXE} get ${REPO}
 done
