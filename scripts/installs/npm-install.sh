24cd52c900e9d3e1961df6b538db59e91f3858bd
Ernesto Alfonso <erjoalgo@gmail.com>
2020-02-16 23:52:27 -0800

    mitigate nvm slowing down bash promt

--- scripts/installs/npm-install.sh
+++ scripts/installs/npm-install.sh
@@ -3,29 +3,28 @@
 set -euo pipefail
 NVM_SH="${HOME}/.nvm/nvm.sh"
 
-if ! test -e "${NVM_SH}"; then
-    cd
-    test -d .nvm || git clone https://github.com/creationix/nvm.git .nvm
+cd
+test -d .nvm || git clone https://github.com/creationix/nvm.git .nvm
 
-    for PROFILE_FILE in \
-        /etc/profile.d/node-env.sh \
-            ${HOME}/.profile-env \
-        ; do
-        test -w ${PROFILE_FILE} && SUDOOPT="" || SUDOOPT="sudo"
-        ${SUDOOPT} insert-text-block \
-                   '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' \
-                   ${PROFILE_FILE} <<"EOF"
+for PROFILE_FILE in \
+    /etc/profile.d/node-env.sh \
+        ${HOME}/.profile-env \
+    ; do
+    test -w ${PROFILE_FILE} && SUDOOPT="" || SUDOOPT="sudo"
+    ${SUDOOPT} insert-text-block \
+               '# 69596022-9179-4a5c-be28-a6d12bcdc132-install-nvm' \
+               ${PROFILE_FILE} <<"EOF"
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 EOF
-    done
-    source "${NVM_SH}"
-fi
+done
+source "${NVM_SH}"
+
+nvm install stable
+nvm alias default node
 
 if ! command -v npm; then
-    nvm install stable
-    nvm alias default node
     npm --version
 fi
 
