843eec07d4759b98417cb8c860aa0b556a6e7d3d
Ernesto Alfonso <erjoalgo@gmail.com>
2020-02-17 12:26:10 -0800

    support either call or sms

--- scripts/bin/linphone-dial.sh
+++ scripts/bin/linphone-dial.sh
@@ -21,13 +21,32 @@ shift $((OPTIND -1))
 SIP_HOST=${SIP_HOST:-$(grep -Po '(?<=^domain=).*$' ${HOME}/.linphonerc | head -1)}
 TEL=$(sed 's/^tel://'  <<< "${TEL}")
 
-set +e
-ERROR=$(linphonecsh dial ${TEL}@${SIP_HOST} 2>&1)
-RET=$?
-set -e
+ADDRESS=${TEL}@${SIP_HOST}
 
-if test ${RET} -ne 0; then
-    xmessage ${ERROR} -timeout 2
+LINPHONE_COMMAND="dial"
+LINPHONE_COMMAND_ARGS="${ADDRESS}"
+
+if command -v x-service-curl; then
+    RESP=$(x-service-curl  \
+               /read-char  \
+               -H"STUMPWM-PROMPT: call (c) or (t) text ${TEL}? ")
+
+    if test "${RESP}" = "t"; then
+        CHAT=$(x-service-curl  \
+               /read-line  \
+               -H "STUMPWM-PROMPT: enter SMS to send to ${TEL}: ")
+        LINPHONE_COMMAND="generic"
+        LINPHONE_COMMAND_ARGS="chat ${ADDRESS} ${CHAT}"
+    elif test "${RESP}" = "c"; then
+        LINPHONE_COMMAND="dial"
+        LINPHONE_COMMAND_ARGS="${ADDRESS}"
+    else
+        echo "unknown option: ${RESP}"
+        exit ${LINENO}
+    fi
 fi
 
 
+if ! linphonecsh ${LINPHONE_COMMAND} "${LINPHONE_COMMAND_ARGS}" 2>&1; then
+    xmessage ${ERROR} -timeout 2 || true
+fi
