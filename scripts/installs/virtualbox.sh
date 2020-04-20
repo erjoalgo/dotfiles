30170f1fdaab861c0180a515a4760e066b679717
Ernesto Alfonso <erjoalgo@gmail.com>
2020-04-20 00:50:21 -0700

    new file:   scripts/installs/virtualbox.sh

---
+++ scripts/installs/virtualbox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/virtualbox.list
+deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian buster contrib
+EOF
+
+for KEY in  \
+    \
+    https://www.virtualbox.org/download/oracle_vbox.asc \
+        https://www.virtualbox.org/download/oracle_vbox_2016.asc \
+    ; do
+    wget -q  -O- ${KEY_URL} | sudo apt-key add -
+done
+
+sudo apt-get -y update
+sudo apt-get install -y virtualbox-6.1
