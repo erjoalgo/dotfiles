4e05216b57e9c7029b95bde9e852d3137a452000
Ernesto Alfonso <erjoalgo@gmail.com>
2020-07-06 02:18:13 -0700

    clean up .pythonrc.py

--- inits/.pythonrc.py
+++ inits/.pythonrc.py
@@ -1,13 +1,2 @@
-#import rlcompleter, readline
-#readline.parse_and_bind('tab:complete')
+from pydoc import help
 import os, re, subprocess, sys
-from itertools import *
-from functools import partial
-from pydoc import help
-from lxml import html as lhtml
-from pydoc import help
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
-# sys.path.append("/home/ernesto/bin")
-# sys.path.append("/home/ernesto/Projects")
-# from ernestoutils import *
