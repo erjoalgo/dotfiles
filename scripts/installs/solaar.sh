cadc444a4fd94cc69f720c275edb13c578a0415a
Ernesto Alfonso <erjoalgo@gmail.com>
2020-07-09 15:26:08 -0700

    solaar

--- scripts/installs/solaar.sh
+++ scripts/installs/solaar.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 
 sudo apt-get install -y python3-pyudev python3-gi gir1.2-gtk-3.0
 
-sudo pip3 install solar
+GIT="${HOME}/git"
+mkdir -p "${GIT}"
+cd "${GIT}"
+test -e Solaar || git clone https://github.com/pwr-Solaar/Solaar
+cd Solaar
+sudo pip3 install .
