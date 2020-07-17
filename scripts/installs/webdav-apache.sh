857e88d4f6bf090f7f163d8677ffb3e74d1ddeeb
Ernesto Alfonso <erjoalgo@gmail.com>
2020-07-16 18:26:11 -0700

    fix webdav installation root directory

--- scripts/installs/webdav-apache.sh
+++ scripts/installs/webdav-apache.sh
@@ -47,16 +47,7 @@ sudo insert-text-block '# 4b56218f-2a94-4089-b556-177861092b1c-webdav-host' \
      /etc/apache2/sites-enabled/webdav.conf <<EOF
 DavLockDB ${WEBDAV_LOCK}
 <VirtualHost *:${WEBDAV_PORT}>
-# The ServerName directive sets the request scheme, hostname and port that
-# the server uses to identify itself. This is used when creating
-# redirection URLs. In the context of virtual hosts, the ServerName
-# specifies what hostname must appear in the request's Host: header to
-# match this virtual host. For the default virtual host (this file) this
-# value is not decisive as it is used as a last resort host regardless.
-# However, you must set it for any further virtual host explicitly.
 
-ServerName webdav.erjoalgo.com
-ServerAdmin admin+webdav@erjoalgo.com
 DocumentRoot ${WEBDAV_ROOT}
 
 # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
@@ -67,15 +58,6 @@ DocumentRoot ${WEBDAV_ROOT}
 
 ErrorLog \${APACHE_LOG_DIR}/error.log
 CustomLog \${APACHE_LOG_DIR}/access.log combined
-
-# For most configuration files from conf-available/, which are
-# enabled or disabled at a global level, it is possible to
-# include a line for only one particular virtual host. For example the
-# following line enables the CGI configuration for this host only
-# after it has been globally disabled with "a2disconf".
-#Include conf-available/serve-cgi-bin.conf
-
-# Alias /webdav ${WEBDAV_ROOT}
 
 <Directory ${WEBDAV_ROOT}>
 DAV On
