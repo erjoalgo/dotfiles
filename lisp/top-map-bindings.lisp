d1ec8a99e7ffccb74b88030245e5b2efe8fd1630
Ernesto Alfonso <erjoalgo@gmail.com>
2020-08-16 12:57:04 -0700

    Revert "workaround to avoid call-to-click in chrome"
    This reverts commit dfa3dc43ec75d8dd4220b438bcf21438c027bb3a.

--- lisp/top-map-bindings.lisp
+++ lisp/top-map-bindings.lisp
@@ -1,10 +1,7 @@
 (in-package :STUMPWM)
 
 (define-run-or-pull-program "BROWSER"
-    :cmd
-  (if (ppcre:scan "chrom" *browser-name*)
-      (format nil "~A --click-to-call-ui=false" *browser-name*)
-      *browser-name*)
+    :cmd *browser-name*
     :raise-key "H-f"
     :pull-key "H-F"
     :classes *browser-classes*)
