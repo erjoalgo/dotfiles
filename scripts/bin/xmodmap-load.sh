602d024c6ceed61d088b7a2694000f5ba82a74e9
Ernesto Alfonso <erjoalgo@gmail.com>
2020-08-31 02:28:38 -0400

    add wip script to set up wireless keyboard

---
+++ scripts/bin/xmodmap-load.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+# exec >> /tmp/xmodmap-load-${USER}.log
+# exec 2>&1
+
+{
+    echo $0 args
+    echo ${*}
+    date
+    env
+    echo
+    echo
+}
+
+if ! test "${USER}" = ealfonso && ! su ealfonso < /dev/null; then
+    echo "warn: unable to switch user"
+    exit 0
+fi
+
+export DISPLAY=:0.0
+export XAUTHORITY=~/.Xauthority
+
+for CAND in ~/.xmodmap/{$(hostname),default}.xmodmap; do
+    if test -e "${CAND}"; then
+        xmodmap "${CAND}"
+        break
+    fi
+done
+
+xset r rate 170 50 # kbd delay, repeat rate
+xset m 10 1 # mouse accel, thresh
+
+# run this last as it may fail
+sudo solaar config 1 fn-swap off
