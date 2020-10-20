a8cd8e337529c36d5956c0c86e245b454785c594
Ernesto Alfonso <erjoalgo@gmail.com>
2020-10-20 14:29:28 -0400

    simplify sed

--- scripts/bin/linphone-dial.sh
+++ scripts/bin/linphone-dial.sh
@@ -24,7 +24,7 @@ function urldecode() {: "${*//+/ }"; echo -e "${_//%/\\x}";}
 
 TEL=$(urldecode "${TEL}")
 SIP_HOST=$(linphone-default-host.sh)
-TEL=$(sed -e 's/^tel://' -e 's/^[+]1\|[.]//g' -e 's/[^0-9]//g' <<< "${TEL}")
+TEL=$(sed -e 's/^tel://' -e 's/^[+]1//g' -e 's/[^0-9]//g' <<< "${TEL}")
 
 ADDRESS=${TEL}@${SIP_HOST}
 
