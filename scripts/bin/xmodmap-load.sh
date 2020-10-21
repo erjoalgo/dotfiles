9a12014535fa3f7e31a24af8e260325b468f5f75
Ernesto Alfonso <erjoalgo@gmail.com>
2020-10-21 00:37:40 -0400

    Revert "wip automate xmodmap-load via udev"
    This reverts commit 832ebe8153816e80136f0550821441ed276373b6.

--- scripts/bin/xmodmap-load.sh
+++ scripts/bin/xmodmap-load.sh
@@ -1,48 +1,32 @@
 #!/bin/bash -x
 
+set -euo pipefail
 
-exec >> /tmp/udev.log
-exec 2>&1
+# exec >> /tmp/xmodmap-load-${USER}.log
+# exec 2>&1
 
-echo loading
 {
-    set -euo pipefail
-    EALFONSO_HOME=$(echo /home/e*alfonso)
-    EALFONSO_USER=$(basename ${EALFONSO_HOME})
-    export XAUTHORITY=${EALFONSO_HOME}/.Xauthority
-    export DISPLAY=:0
-
-    xmessage hola -timeout 1
-    UDEV_RULE=/etc/udev/rules.d/logitech-keyboard.rules
-    cat<<EOF | sudo tee "${UDEV_RULE}"
-ACTION="bind", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c52b", RUN+="/tmp/test.sh"
-EOF
-
-    if test $(whoami) != ${EALFONSO_USER}; then
-        su ${EALFONSO_USER}
-    fi
-
-    for CAND in ~/.xmodmap/{$(hostname),default}.xmodmap; do
-        if test -e "${CAND}"; then
-            xmodmap "${CAND}"
-            break
-        fi
-    done
-
-    xset r rate 170 50 # kbd delay, repeat rate
-    xset m 10 1 # mouse accel, thresh
-
+    echo $0 args
+    echo ${*}
+    date
     env
-    echo "pid: $$?" $(whoami)
-    # run this last as it may fail
-    for _ in $(seq 10); do
-        if sudo $(which solaar) config 1 fn-swap off; then
-            break
-        fi
-        # echo "failed, retrying..."
-        xmessage "failed, retrying..." -timeout 1
-        # sleep 1
-    done
-    echo "done"
-    xmessage done -timeout 1
+    echo
+    echo
 }
+
+
+export DISPLAY=:0.0
+export XAUTHORITY=~/.Xauthority
+
+for CAND in ~/.xmodmap/{$(hostname),default}.xmodmap; do
+    if test -e "${CAND}"; then
+        xmodmap "${CAND}"
+        break
+    fi
+done
+
+xset r rate 170 50 # kbd delay, repeat rate
+xset m 10 1 # mouse accel, thresh
+
+# run this last as it may fail
+sudo $(which solaar) config 1 fn-swap off
