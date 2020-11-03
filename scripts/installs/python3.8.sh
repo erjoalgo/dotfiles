98ad23e2d291b062f3b2f6e3e50a0c5e1d3bc1df
Ernesto Alfonso <erjoalgo@gmail.com>
2020-11-02 20:52:43 -0500

    update to python3.9

--- scripts/installs/python3.8.sh
+++ scripts/installs/python3.8.sh
@@ -2,10 +2,14 @@
 
 set -euo pipefail
 
-sudo apt-get install -y libffi-dev
+sudo apt-get install -y libffi-dev liblzma-dev \
+     build-essential zlib1g-dev libncurses5-dev \
+     libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl  \
+     libbz2-dev
 
-if ! command -v python3.8 || test -n "${FORCE:-}"; then
-    install-from-source \
-        -u https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tar.xz
+
+if ! command -v python3.9; then
+    install-from-source  \
+        -u https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz \
+        -c "--enable-optimizations"
 fi
-
