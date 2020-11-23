04b2b12379e135e1f2a472585f8fdbeed5b38131
Ernesto Alfonso <erjoalgo@gmail.com>
2020-11-23 09:21:52 -0800

    auto-rename erjoalgo-stumpwmrc

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -52,6 +52,13 @@ GIT_NAME="Ernesto Alfonso"
 GIT_HOME=${HOME}/git
 mkdir -p ${GIT_HOME}
 
+ERJOALGO_STUMPWMRC=${GIT_HOME}/erjoalgo-stumpwmrc
+DOTFILES=${GIT_HOME}/dotfifles
+if test -e "${ERJOALGO_STUMPWMRC}" -a ! -e "${DOTFILES}"; then
+  mv "${ERJOALGO_STUMPWMRC}" "${DOTFILES}"
+  ln -s "${DOTFILES}" "${ERJOALGO_STUMPWMRC}"
+fi
+
 for REPO in  \
     dotfiles \
 	dotemacs \
