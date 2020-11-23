bea8daf59d18012d9e3c26ae738a0614c1499c67
Ernesto Alfonso <erjoalgo@gmail.com>
2020-11-23 09:27:10 -0800

    don't create home directory

--- scripts/installs/nonet.sh
+++ scripts/installs/nonet.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 USERNAME=nonet
 
-id -u ${USERNAME} || sudo useradd ${USERNAME} -m
+id -u ${USERNAME} || sudo useradd ${USERNAME}
 sudo iptables -A OUTPUT -p all -m owner --uid-owner ${USERNAME} -j DROP
