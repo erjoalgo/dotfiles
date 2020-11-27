5ceed1ca592ff28b1f801e5131b6236bbc845e3a
Ernesto Alfonso <erjoalgo@gmail.com>
2020-11-27 17:53:34 -0500

    fmt

--- scripts/installs/go.sh
+++ scripts/installs/go.sh
@@ -21,7 +21,7 @@ if ! command -v go || test ${UPGRADE_GO} = true; then
     which insert-text-block
     sudo insert-text-block \
 	 '# c0b15b6c-e5fc-495a-b1be-f02308cee38d-add-go-tools-to-path'  \
-		      ${PROFILE_FILE} <<EOF
+	 ${PROFILE_FILE} <<EOF
 export PATH=\${PATH}:${GOROOT}/bin
 EOF
     source ${PROFILE_FILE}
@@ -31,19 +31,22 @@ fi
 # add fallback-gopath if user's GOPATH not set
 
 GOPATH=/usr/share/gopath
-sudo $(which insert-text-block) '# 387b6046-a715-11e7-b87e-2be468b96d0a-set-default-gopath'  \
-		  /etc/bash.bashrc <<EOF
+sudo $(which insert-text-block) \
+     '# 387b6046-a715-11e7-b87e-2be468b96d0a-set-default-gopath'  \
+     /etc/bash.bashrc <<EOF
 export GOPATH=$GOPATH
 export PATH=\$PATH:\$GOPATH/bin
 EOF
 
 GOPATH=${HOME}/src/go
 mkdir -p ${GOPATH}
-insert-text-block '# a7095f2e-b531-4f1e-9fd6-ea4e404f19f2-add-user-gopath'  \
-		  ~/.profile-env <<EOF
+insert-text-block \
+    '# a7095f2e-b531-4f1e-9fd6-ea4e404f19f2-add-user-gopath'  \
+    ~/.profile-env <<EOF
 export GOPATH=$GOPATH
 export PATH=\$PATH:\$GOPATH/bin:${GOROOT}/bin
 EOF
 
-sudo $(which insert-text-block) '# fba1e4c6-a726-11e7-b4e2-23bbc233d273-set-default-gopath'  \
-		  /etc/environment <<< "GOPATH=$GOPATH"
+sudo $(which insert-text-block)  \
+     '# fba1e4c6-a726-11e7-b4e2-23bbc233d273-set-default-gopath'  \
+     /etc/environment <<< "GOPATH=$GOPATH"
