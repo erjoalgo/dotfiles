4f50e342ed3a9fa0c1f336999fedb1993ca04e5d
Ernesto Alfonso <erjoalgo@gmail.com>
2020-12-04 08:05:46 -0500

    fix "import gi" on python3.9

--- scripts/installs/solaar.sh
+++ scripts/installs/solaar.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 
 sudo apt-get install -y python3-pyudev python3-gi gir1.2-gtk-3.0
 
+sudo apt install -y libgirepository1.0-dev
+
+python3 -m pip install PyGObject
+
 GIT="${HOME}/git"
 mkdir -p "${GIT}"
 cd "${GIT}"
