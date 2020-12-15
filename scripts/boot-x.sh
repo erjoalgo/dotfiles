91351b5e1650c94a19be6b0fb894a76f038e0fc9
Ernesto Alfonso <erjoalgo@gmail.com>
2020-12-14 20:06:54 -0500

    install python3 pip

--- scripts/boot-x.sh
+++ scripts/boot-x.sh
@@ -96,6 +96,7 @@ for XDG_OPEN_SCRIPT in ./installs/xdg-open-*; do
     ${XDG_OPEN_SCRIPT} || true
 done
 
+sudo apt-get install python3-pip
 python3 -m pip install pyudev
 # for the logitech wireless keyboard
 sudo python3 -m pip install solaar
