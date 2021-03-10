c5f8f9eecbdc6e61eea9909dbb3a2126ce2d1a33
Ernesto Alfonso <erjoalgo@gmail.com>
2021-03-10 00:20:49 -0500

    make local backup easier to access

--- scripts/bin/backup-setup.sh
+++ scripts/bin/backup-setup.sh
@@ -29,13 +29,13 @@ if test "${MODE}" = "client"; then
     read -p"Enter local backup server port: " SSHFS_PORT_LOCAL
     SSHFS_PATH=${SSHFS_PATH:-docs}
     USERNAME=${USERNAME:-ealfonso}
-    mkdir -p "${MOUNT_POINT}"{,.local}
+    mkdir -p "${MOUNT_POINT}"{,.remote}
 
     sudo insert-text-block  \
          '# a62a6d16-6dae-4fdd-b42c-94d72bd8ec60-sshfs-backup-automount' \
          /etc/fstab<<EOF
-${USERNAME}@${SSHFS_SERVER}:${SSHFS_PATH} ${MOUNT_POINT} fuse.sshfs _netdev,user,idmap=user,transform_symlinks,identityfile=/home/${USERNAME}/.ssh/id_rsa,allow_other,default_permissions,port=${SSHFS_PORT} 0
-${USERNAME}@${SSHFS_SERVER_LOCAL}:${SSHFS_PATH} ${MOUNT_POINT}.local fuse.sshfs _netdev,user,idmap=user,transform_symlinks,identityfile=/home/${USERNAME}/.ssh/id_rsa,allow_other,default_permissions,port=${SSHFS_PORT_LOCAL} 0
+${USERNAME}@${SSHFS_SERVER}:${SSHFS_PATH} ${MOUNT_POINT}.remote fuse.sshfs _netdev,user,idmap=user,transform_symlinks,identityfile=/home/${USERNAME}/.ssh/id_rsa,allow_other,default_permissions,port=${SSHFS_PORT} 0
+${USERNAME}@${SSHFS_SERVER_LOCAL}:${SSHFS_PATH} ${MOUNT_POINT} fuse.sshfs _netdev,user,idmap=user,transform_symlinks,identityfile=/home/${USERNAME}/.ssh/id_rsa,allow_other,default_permissions,port=${SSHFS_PORT_LOCAL} 0
 EOF
     sudo mount -av
 elif test "${MODE}" = "server"; then
