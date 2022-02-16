519a4a5a02a28e5a54d47bc65a4a8db769073b27
Ernesto Alfonso <erjoalgo@gmail.com>
2022-02-16 02:24:41 -0500

    use the new snipit cli client

--- lisp/commands/snipit.lisp
+++ lisp/commands/snipit.lisp
@@ -3,14 +3,11 @@
 (defvar *snipit-upload-url*)
 
 (defcommand take-scrot-snipit (&optional png) ()
-  (assert *snipit-upload-url*)
-  (let ((png (or png (pathname (get-x-selection nil :clipboard)))))
-    (assert (probe-file png))
-    (run-command-async
-     "curl"
-     (list "-sLF" (format nil "imagedata=@~A" png) *snipit-upload-url*)
-     (retcode output)
-     (let ((url output))
-       (set-x-selection url :clipboard)
-       (message-wrapped "^2created snipit: ~A^*" output)
-       (run-command-retcode-output "curl" (list url))))))
+  "take screenshot using the snipit cli"
+  (run-command-async
+   "snipit" ()
+   (retcode output)
+   (ppcre:register-groups-bind (url)
+       ("Uploaded to: (http.*)" output)
+     (set-x-selection url :clipboard)
+     (message-wrapped "^2created snipit at: ~A^*" url))))
