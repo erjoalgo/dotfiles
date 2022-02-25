e70a39d07c70acc2651ea99f5e65e71a5fc53ac3
Ernesto Alfonso <erjoalgo@gmail.com>
2022-02-25 03:56:25 -0500

    new file:   scripts/installs/beep.sh

---
+++ scripts/installs/beep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+sudo apt-get install -y beep
+sudo groupadd -f beep
+sudo usermod -a -G beep ${USER}
+
+sudo insert-text-block '# 618075ff-72c0-4113-a54c-3b81b180b5b4-pc-spkr'  \
+     /usr/lib/udev/rules.d/70-pcspkr-beep.rules<<EOF
+ACTION=="add", SUBSYSTEM=="input", ATTRS{name}=="PC Speaker", ENV{DEVNAME}!="", GROUP="beep", MODE="0620"
+EOF
+sudo rmmod pcspkr
+sudo modprobe pcspkr
+sudo udevadm control --reload
