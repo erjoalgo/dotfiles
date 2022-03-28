10009d3895caaca5442b2f34e057f766bd046b14
Ernesto Alfonso <erjoalgo@gmail.com>
2022-03-28 14:34:24 -0700

    merge with workstation version of openproject-docker.sh

--- scripts/installs/openproject-docker.sh
+++ scripts/installs/openproject-docker.sh
@@ -2,14 +2,19 @@
 
 set -euo pipefail
 
-cd ~/git
+TOP=${HOME}/git/openproject-deploy
+
 if ! test -d openproject; then
-    git clone https://github.com/opf/openproject-deploy --depth=1 --branch=stable/12 openproject
+  git clone https://github.com/opf/openproject-deploy \
+    --depth=1 --branch=stable/12 openproject  \
+    "${TOP}"
 fi
 
-cd openproject/compose
+cd "${TOP}/compose"
 
 sudo apt-get install -y docker{,-compose}
+
+command -v docker-compose
 
 sudo usermod -aG docker $USER
 
@@ -18,3 +23,17 @@ sudo usermod -aG docker $USER
 sudo docker-compose pull
 
 sudo docker-compose up -d
+
+# OPENPROJECT_DATA_ROOT=/var/lib/openproject
+# OPENPROJECT_VERSION=openproject/community:11
+# sudo mkdir -p "${OPENPROJECT_DATA_ROOT}/"{pgdata,assets}
+# SECRET_KEY_BASE=secret
+# export DOCKER_HOST=http+unix://run/containerd/containerd.sock
+# docker-compose pull ${OPENPROJECT_VERSION}
+# docker run -d -p 8080:80 --name openproject \
+#   -e SERVER_HOSTNAME=REDACTED \
+#   # The secret key base used for cookies \
+#   -e SECRET_KEY_BASE="${SECRET_KEY_BASE}" \
+#   -v ${OPENPROJECT_DATA_ROOT}/pgdata:/var/openproject/pgdata \
+#   -v ${OPENPROJECT_DATA_ROOT}/assets:/var/openproject/assets \
+#   ${OPENPROJECT_VERSION}
