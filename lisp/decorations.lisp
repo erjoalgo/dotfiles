c98d691172a15ddb00fc88c5f9caa4323cade5d8
Ernesto Alfonso <erjoalgo@gmail.com>
2022-03-31 13:21:53 -0400

    avoid using "fn" for filenames

--- lisp/decorations.lisp
+++ lisp/decorations.lisp
@@ -1,11 +1,11 @@
 (in-package :STUMPWM)
 
-(defvar *background-image-fn* #P"~/.background-image-symlink")
+(defvar *background-image-filename* #P"~/.background-image-symlink")
 
 (defun background-image-init ()
   (if (not *background-image-fn*)
-      (print (concat "no background image found at" *background-image-fn*))
+      (print "No background image found at ~A" *background-image-filename*)
       (run-shell-command
        (format nil
 	       "display -window root ~A"
-	       *background-image-fn*))))
+	       *background-image-filename*))))
