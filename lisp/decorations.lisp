1d84d3709e67b6752b2bb19fa8ec02d5635b6626
Ernesto Alfonso <erjoalgo@gmail.com>
2022-03-31 13:22:05 -0400

    use message instead of print

--- lisp/decorations.lisp
+++ lisp/decorations.lisp
@@ -4,7 +4,7 @@
 
 (defun background-image-init ()
   (if (not *background-image-fn*)
-      (print "No background image found at ~A" *background-image-filename*)
+      (message "No background image found at ~A" *background-image-filename*)
       (run-shell-command
        (format nil
 	       "display -window root ~A"
