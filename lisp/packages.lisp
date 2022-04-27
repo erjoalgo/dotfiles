eda0ac13646efe0d286c332ae6091d92d5350908
Ernesto Alfonso <erjoalgo@gmail.com>
2022-04-27 03:16:18 -0400

    remove unused references to postmodern, add in-package statement

--- lisp/packages.lisp
+++ lisp/packages.lisp
@@ -1,16 +1,5 @@
+(in-package :stumwpm)
+
 (defpackage erjoalgo-stumpmwrc/util
   (:use :cl)
-  (:import-from #:postmodern
-                connect-toplevel
-                query
-                dao-class
-                dao-table-definition
-                db-null
-                execute
-                insert-dao
-                update-dao
-                save-dao
-                sql
-                defprepared
-                with-connection)
   (:export #:trim-spaces))
