09e165ae8886a8b09865a110cd1dfd2f6e549b7f
Ernesto Alfonso <erjoalgo@gmail.com>
2022-04-29 04:09:50 -0400

    fmt

--- scripts/installs/autologin-on-tty.sh
+++ scripts/installs/autologin-on-tty.sh
@@ -17,13 +17,15 @@ sudo insert-text-block '# e8a6c230-997f-4dd5-9b57-7e3b31ab67bc'  \
 ExecStart=-/sbin/agetty --autologin "${USER}" %I
 EOF
 
-sudo insert-text-block '# bZkpjy56EU9dBbdNDTk0zmMVGWJLzK9e-ssh' \
+sudo insert-text-block \
+     '# bZkpjy56EU9dBbdNDTk0zmMVGWJLzK9e-ssh' \
      "${HOME}/.ssh/config" <<EOF
 XAuthLocation /opt/X11/bin/xauth
 EOF
 
-sudo insert-text-block '# yTyIZrilAW59XgITTlNwLp3VdhMn9k7R-enable-sysrq' \
-                  /etc/sysctl.d/99-sysctl.conf<<EOF
+sudo insert-text-block \
+     '# yTyIZrilAW59XgITTlNwLp3VdhMn9k7R-enable-sysrq' \
+     /etc/sysctl.d/99-sysctl.conf<<EOF
 kernel.sysrq = 1
 EOF
 
