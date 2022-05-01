ebec450781eed1d9b32f9a17ab53fdecd2f0de67
Ernesto Alfonso <erjoalgo@gmail.com>
2022-05-01 18:37:29 -0400

    update the bfg repo url

--- scripts/installs/bfg-install.sh
+++ scripts/installs/bfg-install.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if ! which bfg; then
     mkdir -p ~/src
     cd ~/src
-    URL=http://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar
+    URL=https://repo1.maven.org/maven2/com/madgag/bfg/1.14.0/bfg-1.14.0.jar
     BASE=$(basename ${URL})
     test -e ${BASE} || wget ${URL}
 
