1e6fa51202f5bf809f4001ed003476e968a896f6
Ernesto Alfonso <erjoalgo@gmail.com>
2022-05-06 11:23:01 -0400

    disable autologin service on tty1

--- scripts/installs/autologin-on-tty.sh
+++ scripts/installs/autologin-on-tty.sh
@@ -7,16 +7,6 @@ sudo insert-text-block '# Zss7UaEgcFtP1T8JPS7h77vOaQlYDR3H-enable-autologin' \
 NAutoVTs=6
 EOF
 
-AUTOLOGIN_CONF=/etc/systemd/system/getty@tty1.service.d/autologin.conf
-# autologin on tty1
-sudo mkdir -p $(dirname "${AUTOLOGIN_CONF}")
-
-sudo insert-text-block '# e8a6c230-997f-4dd5-9b57-7e3b31ab67bc'  \
-     "${AUTOLOGIN_CONF}" <<EOF
-[Service]
-ExecStart=-/sbin/agetty --autologin "${USER}" %I
-EOF
-
 sudo insert-text-block \
      '# bZkpjy56EU9dBbdNDTk0zmMVGWJLzK9e-ssh' \
      "${HOME}/.ssh/config" <<EOF
@@ -29,4 +19,3 @@ sudo insert-text-block \
 kernel.sysrq = 1
 EOF
 
-sudo systemctl enable getty@tty1.service
