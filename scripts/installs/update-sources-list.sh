a5564ce48b27b65ba609fb7235e6fd8ecee5e348
Ernesto Alfonso <erjoalgo@gmail.com>
2022-12-17 21:28:14 -0500

    fix duplicates in sources.list

--- scripts/installs/update-sources-list.sh
+++ scripts/installs/update-sources-list.sh
@@ -17,13 +17,21 @@ else
     CODENAME=$(printf '%s\n' "$VERSION" | grep -o '[a-z]*')
 fi
 
-cat << EOF >> $SOURCES
+INDICATOR_LINE="# c1346404-34f5-4223-9142-90f9468c98a2-update-sources"
+
+if ! grep -F "${INDICATOR_LINE}" "${SOURCES}"; then
+    cat << EOF >> ${SOURCES}
+${INDICATOR_LINE}
 deb http://ftp.debian.org/debian $CODENAME main
 deb-src http://ftp.debian.org/debian $CODENAME main
 
 deb http://ftp.debian.org/debian ${CODENAME}-updates main
 deb-src http://ftp.debian.org/debian ${CODENAME}-updates main
 EOF
+else
+    echo "skipping updating ${SOURCES} since indicator line was found: "
+    echo "${INDICATOR_LINE}"
+fi
 
 sed -i 's/^deb cdrom/# &/g' $SOURCES
 
