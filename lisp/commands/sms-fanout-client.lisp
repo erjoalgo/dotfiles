c196780f1f11ca957e0cd127965f2c283b3ff58c
Ernesto Alfonso <erjoalgo@gmail.com>
2023-05-19 01:05:06 -0400

    fmt

--- lisp/commands/sms-fanout-client.lisp
+++ lisp/commands/sms-fanout-client.lisp
@@ -119,8 +119,7 @@
 (defun syslog-log (priority message)
   ;; '((:EMERG . 0) (:ALERT . 1) (:CRIT . 2) (:ERR . 3) (:WARNING . 4)
   ;;   (:NOTICE . 5) (:INFO . 6) (:DEBUG . 7))
-  (cl-syslog:log "stumpwm/sms-fanout" ':user priority
-                 message))
+  (cl-syslog:log "stumpwm/sms-fanout" ':user priority message))
 
 (defun reconnect-loop (&key
                          (address (authinfo-address))
