594d9d94abde99cc804ea5e85297b337af4f464d
Ernesto Alfonso <erjoalgo@gmail.com>
2023-05-20 00:11:31 -0400

    fix using the wrong chrome policies dir

--- scripts/installs/chrome-disable-xdg-open-prompt.sh
+++ scripts/installs/chrome-disable-xdg-open-prompt.sh
@@ -3,8 +3,19 @@
 set -euo pipefail
 
 # POLICIES_DIR=$(ls /etc/*chrom* /etc/opt/*chrom*)/managed
-POLICIES_DIR=/etc/chromium/
 
-sudo mkdir -p ${POLICIES_DIR}/{managed,recommended}
-echo '{"ExternalProtocolDialogShowAlwaysOpenCheckbox": true}' |  \
-    sudo tee ${POLICIES_DIR}/managed/protocol_open.json > /dev/null
+
+for POLICIES_DIR in /etc/opt/chrome/policies/ /etc/chromium/policies; do
+    sudo mkdir -p ${POLICIES_DIR}/{managed,recommended}
+    echo '{"ExternalProtocolDialogShowAlwaysOpenCheckbox": true}' |  \
+        sudo tee ${POLICIES_DIR}/managed/protocol_open.json > /dev/null
+
+    cat <<EOF | sudo tee ${POLICIES_DIR}/managed/allow_tel_protocol.json > /dev/null
+{
+  "URLAllowlist": [
+    "tel:*"
+  ]
+}
+EOF
+
+done
