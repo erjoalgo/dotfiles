4cc307ace8074cfd91e51a12c4813704858e3d19
Ernesto Alfonso <erjoalgo@gmail.com>
2023-06-05 02:41:24 -0400

    Revert "add failed attempts at webpage-title command"
    This reverts commit 0324b7efccf0b57115bffec4a0945defc45225a5.

--- scripts/bin/webpage-title.sh
+++ scripts/bin/webpage-title.sh
@@ -9,30 +9,3 @@ url = sys.argv[1]
 page = urllib.urlopen(url)
 t = lxml.html.parse(page)
 print(t.find(".//title").text)
-
-
-"""from mechanize import Browser
-br = Browser()
-br.open("http://www.google.com/")
-print(br.title())
-"""
-
-"""
-from BeautifulSoup import BeautifulSoup
-soup = BeautifulSoup(urllib2.urlopen())
-print(soup.title.string)
-"""
-
-
-"""#!/bin/bash
-
-set -euo pipefail
-
-URL=${1} && shift
-
-if ! command -v hxselect; then
-   sudo apt-get install -y html-xml-utils libxml2-utils
-fi
-
-wget -qO- "${URL}" | hxselect -s '\n' -c  'title' 2>/dev/null
-"""
