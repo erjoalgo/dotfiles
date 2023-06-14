ae7485bad9fef96dc5f2fa4c9ee49648afe24ef5
Ernesto Alfonso <erjoalgo@gmail.com>
2023-06-14 17:27:21 -0400

    support biblatex

--- scripts/installs/tex.sh
+++ scripts/installs/tex.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install -y texlive-fonts-recommended \
-     texlive-latex-recommended texlive-latex-extra
+     texlive-latex-recommended texlive-latex-extra \
+     texlive-bibtex-extra biber
