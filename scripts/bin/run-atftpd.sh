4decfcd18ae641a79baafcc730994a7141a17753
Ernesto Alfonso <erjoalgo@gmail.com>
2023-06-14 17:25:39 -0400

    add getopt. kill existing process. parameterize directory

--- scripts/bin/run-atftpd.sh
+++ scripts/bin/run-atftpd.sh
@@ -2,6 +2,25 @@
 
 set -euo pipefail
 
-cd /home/ealfonso/Downloads/netboot/
-sudo chmod -R a+rwx .
-sudo atftpd --no-fork --verbose=7 --logfile - --daemon .
+while getopts "hd:" OPT; do
+    case ${OPT} in
+    d)
+        DIRECTORY=${OPTARG}
+        ;;
+    h)
+        less $0
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+test -d "${DIRECTORY}"
+cd "${DIRECTORY}"
+sudo chmod -R a+rwx "${DIRECTORY}"
+
+while pgrep -f /sbin/atftpd | xargs kill -9 2> /dev/null; do
+    sleep 1
+done
+
+sudo atftpd --no-fork --verbose=7 --logfile - --daemon "${DIRECTORY}"
