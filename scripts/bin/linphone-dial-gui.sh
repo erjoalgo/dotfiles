9bf91d09e4191df7e6d821c152fa2cc5ee951ff3
Ernesto Alfonso <erjoalgo@gmail.com>
2023-06-22 00:30:38 -0400

    I need more time to read

--- scripts/bin/linphone-dial-gui.sh
+++ scripts/bin/linphone-dial-gui.sh
@@ -6,5 +6,5 @@ OUTPUT=$(linphone-dial.sh ${*} 2>&1)
 
 if test $? != 0 ; then
     echo ${OUTPUT}
-    xmessage "error: ${OUTPUT}" -timeout 2 || true
+    xmessage "error: ${OUTPUT}" -timeout 9999 || true
 fi
