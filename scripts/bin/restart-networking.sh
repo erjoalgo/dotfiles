5a65378fc8aba7fab58a9cd6b93f0fa6e8a335bd
Ernesto Alfonso <erjoalgo@gmail.com>
2023-07-12 17:34:09 -0400

    new file:   scripts/bin/restart-networking.sh

---
+++ scripts/bin/restart-networking.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+sudo service networking stop
+sudo rm /var/lib/dhcp/*lease*
+while sudo pkill dhclient; do
+    sleep 1
+done
+
+IFACES=$(ip link | grep -Po '^[0-9]+: [^:]+' | cut -f2 -d' ')
+for IFACE in ${IFACES}; do
+    sudo ip addr flush ${IFACE}
+done
+
+service networking restart
