300dd5705d91e43589b400172b06e5ffb089e9cb
Ernesto Alfonso <erjoalgo@gmail.com>
2023-07-21 12:16:24 -0400

    install texlive-latex-extra-utils for pdfjam

--- scripts/installs/tex.sh
+++ scripts/installs/tex.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get install -y texlive-fonts-recommended \
      texlive-latex-recommended texlive-latex-extra \
-     texlive-bibtex-extra biber texlive-font-utils
+     texlive-bibtex-extra biber texlive-font-utils \
+     texlive-latex-extra-utils
