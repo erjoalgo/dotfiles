0453af12000ea8c09cd2d7db36e98d1e21167e92
Ernesto Alfonso <erjoalgo@gmail.com>
2023-07-27 18:04:21 -0400

    fmt

--- lisp/commands/battery-notification.lisp
+++ lisp/commands/battery-notification.lisp
@@ -54,45 +54,45 @@
 (defun battery-notification-maybe-notify (&key (thresholds *battery-notification-thresholds-defaults*))
   "Maybe notify that battery is low. Thresholds has the form (see source): "
   (when-let* ((info (battery-info)))
-  (with-slots
-      (min-percent-message
-       min-percent-flashing-message
-       min-percent-window-popup
-       min-percent-beep
-       min-percent-suspend)
-      thresholds
-    (erjoalgo-webutil:with-json-paths info
-        (state (percentage-string "percentage"))
-      (statusor:if-let-ok
-       (err (error "battery notification error: ~A" err))
-       ((_ (when (or (null percentage-string) (null state))
-             (statusor:make-error "unable to parse percentage or state")))
-        (percentage (prog1
-                        ;; ignore multiple values,
-                        ;; which are interpreted as a status error
-                        (parse-integer percentage-string :junk-allowed t))))
-       (when (equal state "discharging")
-         (format t "trace: battery-notification dft0~%")
-         (when (<= percentage min-percent-message)
-           (format t "trace: battery-notification 9zuv~%")
-           (let ((*message-window-gravity* :top-right))
-             (message "^1 warning: battery discharging (~D%)^*" percentage)))
-         (when (<= percentage min-percent-flashing-message)
-           (format t "trace: battery-notification cx3p~%")
-           (loop for i below (- min-percent-message percentage)
-                 do
-                    (let ((*message-window-gravity* :top-right))
-                      (message
-                       "^1 warning: battery discharging (~D%)^*" percentage)
-                      (sleep .5)
-                      (unmap-all-message-windows)
-                      (sleep .5))))
-         (when (<= percentage min-percent-window-popup)
-           (run-shell-command "xmessage battery low!"))
-         (when (<= percentage min-percent-beep)
-           (beep))
-         (when (<= percentage min-percent-suspend)
-           (run-shell-command "systemctl suspend"))))))))
+    (with-slots
+          (min-percent-message
+           min-percent-flashing-message
+           min-percent-window-popup
+           min-percent-beep
+           min-percent-suspend)
+        thresholds
+      (erjoalgo-webutil:with-json-paths info
+          (state (percentage-string "percentage"))
+        (statusor:if-let-ok
+         (err (error "battery notification error: ~A" err))
+         ((_ (when (or (null percentage-string) (null state))
+               (statusor:make-error "unable to parse percentage or state")))
+          (percentage (prog1
+                          ;; ignore multiple values,
+                          ;; which are interpreted as a status error
+                          (parse-integer percentage-string :junk-allowed t))))
+         (when (equal state "discharging")
+           (format t "trace: battery-notification dft0~%")
+           (when (<= percentage min-percent-message)
+             (format t "trace: battery-notification 9zuv~%")
+             (let ((*message-window-gravity* :top-right))
+               (message "^1 warning: battery discharging (~D%)^*" percentage)))
+           (when (<= percentage min-percent-flashing-message)
+             (format t "trace: battery-notification cx3p~%")
+             (loop for i below (- min-percent-message percentage)
+                   do
+                      (let ((*message-window-gravity* :top-right))
+                        (message
+                         "^1 warning: battery discharging (~D%)^*" percentage)
+                        (sleep .5)
+                        (unmap-all-message-windows)
+                        (sleep .5))))
+           (when (<= percentage min-percent-window-popup)
+             (run-shell-command "xmessage battery low!"))
+           (when (<= percentage min-percent-beep)
+             (beep))
+           (when (<= percentage min-percent-suspend)
+             (run-shell-command "systemctl suspend"))))))))
 
 (defun battery-info-check-notify-loop (&key (interval-secs 30))
   (loop
