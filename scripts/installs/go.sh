3d72122661e4ff300daed9592a965031c46880fc
Ernesto Alfonso <erjoalgo@gmail.com>
2023-08-08 17:11:09 -0400

    fmt

--- scripts/installs/go.sh
+++ scripts/installs/go.sh
@@ -6,7 +6,6 @@ GOROOT=/usr/local/go
 GO_URL=https://go.dev/dl/go1.20.7.src.tar.gz
 
 if ! command -v go || test ${UPGRADE_GO} = true; then
-    # GO_URL=https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz
     mkdir -p ~/src && cd ~/src
     test -f $(basename ${GO_URL}) || wget "${GO_URL}"
     FNAME=$(basename ${GO_URL})
