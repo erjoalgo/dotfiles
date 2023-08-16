f90129153dc282366b8f017655a1eaf5c31936c3
Ernesto Alfonso <erjoalgo@gmail.com>
2023-08-16 08:54:55 -0400

    support resetting wifi mac and dhclient hostname

--- scripts/bin/wifi-cycle-mac.sh
+++ scripts/bin/wifi-cycle-mac.sh
@@ -2,15 +2,40 @@
 
 set -euo pipefail
 
+while getopts "hi:r" OPT; do
+    case ${OPT} in
+    i)
+        WIFI_IFACE=${OPTARG}
+        ;;
+    r)
+        RESET=true
+        ;;
+    h)
+        less $0
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
 WIFI_IFACE=${WIFI_IFACE:-$(ip l | grep -P '^[0-9]' |  \
                                cut -f2 -d: | tr -d ' ' | grep '^w')}
 
 sudo nmcli radio wifi off
-sudo macchanger -r ${WIFI_IFACE}
 
-FAKE_HOSTANME=$(hostname)-${RANDOM}
 
-sudo sed -i "s/\(send host-name = \).*/\1\"${FAKE_HOSTANME}\"/" \
+if test -n "${RESET:-}"; then
+    MACCHANGER_OPT=-p
+    NEW_HOSTNAME="$(hostname)"
+else
+    MACCHANGER_OPT=-r
+    NEW_HOSTNAME="$(hostname)-${RANDOM}"
+fi
+
+
+sudo macchanger ${MACCHANGER_OPT} "${WIFI_IFACE}"
+
+sudo sed -i "s/\(send host-name = \).*/\1\"${NEW_HOSTNAME}\"/" \
      /etc/dhcp/dhclient.conf
 
 sudo nmcli radio wifi on
