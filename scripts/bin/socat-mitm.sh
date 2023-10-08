7a1cd4f3f2d191780974057a51a2599beaee250d
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-08 18:35:43 -0400

    new file: scripts/bin/socat-mitm.sh

---
+++ scripts/bin/socat-mitm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+while getopts "h" OPT; do
+    case ${OPT} in
+    h)
+        less $0
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+LISTEN_PORT=${1} && shift
+REMOTE_ADDRESS=${1} && shift
+
+sudo socat -v TCP-LISTEN:${LISTEN_PORT},fork,reuseaddr TCP:${REMOTE_ADDRESS}
