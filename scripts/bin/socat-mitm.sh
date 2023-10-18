38536806aac0f3dd9ae679f1fa222c1fbcd5e903
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-18 18:00:32 -0400

    support turning off verbose for encrypted connections

--- scripts/bin/socat-mitm.sh
+++ scripts/bin/socat-mitm.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
+VERBOSE_OPT=-v
 while getopts "h" OPT; do
     case ${OPT} in
+        q)
+            # quiet
+            VERBOSE_OPT=
+            ;;
         h)
             less $0
             exit 0
@@ -15,4 +20,6 @@ shift $((OPTIND -1))
 LISTEN_PORT=${1} && shift
 REMOTE_ADDRESS=${1} && shift
 
-sudo socat -v TCP-LISTEN:${LISTEN_PORT},fork,reuseaddr TCP:${REMOTE_ADDRESS}
+sudo socat ${VERBOSE_OPT} \
+     TCP-LISTEN:${LISTEN_PORT},fork,reuseaddr  \
+     TCP:${REMOTE_ADDRESS}
