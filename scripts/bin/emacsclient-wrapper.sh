4a79e3ed2a941d1c056f1e30e5dff231f2230fac
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-24 06:43:44 -0400

    use emacs client wrapper to broadcast to all available emacs servers

--- scripts/bin/emacsclient-wrapper.sh
+++ scripts/bin/emacsclient-wrapper.sh
@@ -8,16 +8,18 @@ if test -n "${DESKTOP_GROUP_NUMBER:-}"; then
   SERVER_DIRECTORY=$(emacs -q --batch  \
     --eval "(require 'server)"  \
     --eval "(print server-socket-dir)" \
-    2>/dev/null \
-    | tail -1 | tr -d '"')
-  EMACS_SOCKET_NAME="${SERVER_DIRECTORY}/${DESKTOP_GROUP_NUMBER}"
-  if test -e "${EMACS_SOCKET_NAME}"; then
-    echo "attempting to connect to ${EMACS_SOCKET_NAME}"
-    EMACS_SOCKET_NAME=${EMACS_SOCKET_NAME}  \
-                     "${REAL_EMACSCLIENT}"  \
-                     "--socket-name=${EMACS_SOCKET_NAME}" "${@}"
-    exit $?
-  fi
+    2>/dev/null  \
+                         | tail -1 | tr -d '"')
+  RET=0
+  for EMACS_SOCKET_NAME in ${SERVER_DIRECTORY}/${DESKTOP_GROUP_NUMBER}.*; do
+      echo "attempting to connect to ${EMACS_SOCKET_NAME}"
+      EMACS_SOCKET_NAME=${EMACS_SOCKET_NAME} "${REAL_EMACSCLIENT}" \
+                       "--socket-name=${EMACS_SOCKET_NAME}" "${@}"
+      if test $? -ne 0; then
+          RET=$?
+      fi
+  done
+  exit $RET
 fi
 
 "${REAL_EMACSCLIENT}" "${@}"
