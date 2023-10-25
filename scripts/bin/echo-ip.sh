672018a50770e3744d0d76daff568619f5d5959c
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-25 13:19:26 -0400

    fix missing -s flag

--- scripts/bin/echo-ip.sh
+++ scripts/bin/echo-ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -s ipecho.erjoalgo.com --fail-with-body ||  \
-    curl checkip.dyndns.org \
+    curl -s checkip.dyndns.org \
         | grep -oi "current ip address: [^<]*" \
         | grep -o "[0-9.]*"
