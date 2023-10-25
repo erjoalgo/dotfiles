ba83e4583bd3c5b27e254ae73f741edcbb0c484c
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-25 12:57:15 -0400

    use faster ip echo service

--- scripts/bin/echo-ip.sh
+++ scripts/bin/echo-ip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -s checkip.dyndns.org \
-    | grep -oi "current ip address: [^<]*" \
-    | grep -o "[0-9.]*"
+curl -s ipecho.erjoalgo.com --fail-with-body ||  \
+    curl checkip.dyndns.org \
+        | grep -oi "current ip address: [^<]*" \
+        | grep -o "[0-9.]*"
