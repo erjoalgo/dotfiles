1717602a6c293925491007562baa5e1beee24692
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-27 16:35:54 -0400

    follow redirects

--- scripts/bin/echo-ip.sh
+++ scripts/bin/echo-ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s ipecho.erjoalgo.com --fail-with-body ||  \
-    curl -s checkip.dyndns.org \
+curl -Ls ipecho.erjoalgo.com --fail-with-body ||  \
+    curl -Ls checkip.dyndns.org \
         | grep -oi "current ip address: [^<]*" \
         | grep -o "[0-9.]*"
