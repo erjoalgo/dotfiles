9091409ce7be53965433b079d47589d94deb97ed
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-28 17:30:05 +0000

    add octoprint docker setup

---
+++ scripts/installs/octoprint/octoprint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+cd "$(realpath $(dirname "${BASH_SOURCE[0]}"))"
+
+# sudo apt-get install python3
+# pip3 install pyserial --break-system-packages
+# python3 -m serial.tools.miniterm
+
+PRINTER_DEVICE=$(echo /dev/ttyUSB*)
+
+if ! test -e ${PRINTER_DEVICE}; then
+  echo "3d printer not found!"; exit ${LINENO}
+fi
+
+sudo chown $(whoami):$(whoami) ${PRINTER_DEVICE}
+
+insert-text-block '# e1b3e892-395b-4db2-9061-dc8559f2e009-set-up-device-path' .env <<EOF
+PRINTER_DEVICE=${PRINTER_DEVICE}
+EOF
+
+docker compose up -d
+
+docker compose logs -f
+
