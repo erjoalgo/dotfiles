71dbe56cea2ff36e9977779218383b5cf2d7fb7d
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-29 00:13:50 +0000

    check if DEVICE is set

--- scripts/bin/usbip-server.sh
+++ scripts/bin/usbip-server.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 while getopts "b:h" OPT; do
     case ${OPT} in
     b)
-        BIND_DEVICE=${OPTARG}
+        # bind device
+        DEVICE=${OPTARG}
         ;;
     f)
         FILTER_OPT="grep -B1 '${OPTARG}' | "
@@ -21,16 +22,17 @@ shift $((OPTIND -1))
 sudo apt-get install -y usbip
 sudo modprobe usbip_host
 
-sudo usbip list -l
-CHOICES=$(sudo usbip list -l | ${FILTER_OPT} grep -Po '(?<=^ - busid )[^ ]+')
-
-if test -eq 1 $(wc -l <<< "${CHOICES}"); then
-  DEVICE=${CHOICES}
-else
-  echo "select device to bind: " 1>&2
-  select DEVICE in $CHOICES; do
-    break
-  done
+if test -z "${DEVICE:-}"; then
+	sudo usbip list -l
+	CHOICES=$(sudo usbip list -l | ${FILTER_OPT} | grep -Po '(?<=^ - busid )[^ ]+')
+	if test 1 -eq $(wc -l <<< "${CHOICES}"); then
+	  DEVICE=${CHOICES}
+	else
+	  echo "select device to bind: " 1>&2
+	  select DEVICE in $CHOICES; do
+	    break
+	  done
+	fi
 fi
 
 sudo usbip bind -b "${DEVICE}"
