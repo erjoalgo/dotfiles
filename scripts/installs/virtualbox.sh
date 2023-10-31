ac88f52e30ff1ec357bb7ca8fe49cd2913479e6c
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-31 12:33:09 -0400

    update virtualbox installer

--- scripts/installs/virtualbox.sh
+++ scripts/installs/virtualbox.sh
@@ -2,19 +2,9 @@
 
 set -euo pipefail
 
-cat <<EOF | sudo tee /etc/apt/sources.list.d/virtualbox.list
-deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian buster contrib
-EOF
-
-for KEY in  \
-    \
-    https://www.virtualbox.org/download/oracle_vbox.asc \
-        https://www.virtualbox.org/download/oracle_vbox_2016.asc \
-    ; do
-    wget -q  -O- ${KEY_URL} | sudo apt-key add -
-done
-
-sudo apt-get -y update
-sudo apt-get install -y virtualbox-6.1
+DEB_URL=https://download.virtualbox.org/virtualbox/7.0.12/virtualbox-7.0_7.0.12-159484~Debian~bookworm_amd64.deb
+cd $(mktemp -d)
+wget "${DEB_URL}"
+sudo dpkg -i *deb
 
 vboxmanage setextradata global GUI/SuppressMessages "all"
