d9e54b08646964977a15ed990b602df87680d436
Ernesto Alfonso <erjoalgo@gmail.com>
2023-11-17 15:26:43 -0500

    new file: installs/3d-printing.sh

---
+++ installs/3d-printing.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+sudo apt-get install -y blender cura
+
+pip install git+https://github.com/erjoalgo/octoprint-cli
+
+read -p"enter octorpint server URL, including scheme and basic auth: " SERVER_ADDRESS
+x-www-browser "${SERVER_ADDRESS}"
+read -p"enter octorpint API key: " API_KEY
+
+insert-text-block '# 0754c16b-b5aa-447d-81d6-b4fa758a56bc-octoprint-cli-config' \
+                  ${HOME}/.config/octoprint-cli.ini <<EOF
+[server]
+;Set OctoPrint server address and x-api-key
+ServerAddress = ${SERVER_ADDRESS}
+ApiKey = ${API_KEY}
+
+[preferences]
+;Set if the program uses colored or formatted text, this setting is turned off on windows due to cmd and powershell limitations
+FormattedText = true
+;Set if the program should check for updates
+UpdateCheck = true
+
+[printer]
+;Set maximum temperature that printer can be set to
+MaxExtruderTemp = 250
+MaxBedTemp = 85
+EOF
+
+octoprint-cli files list
