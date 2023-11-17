9abfccfe4a4bddc4df8d03d6fd172182f425c2d3
Ernesto Alfonso <erjoalgo@gmail.com>
2023-11-17 15:36:48 -0500

    deindent

--- bin/emacsclient-wrapper.sh
+++ bin/emacsclient-wrapper.sh
@@ -4,22 +4,22 @@ set -euo pipefail
 
 REAL_EMACSCLIENT=/usr/bin/emacsclient.emacs
 
-if test -n "${DESKTOP_GROUP_NUMBER:-}"; then
-  SERVER_DIRECTORY=$(emacs -q --batch  \
-    --eval "(require 'server)"  \
-    --eval "(print server-socket-dir)" \
-    2>/dev/null  \
-                         | tail -1 | tr -d '"')
-  RET=0
-  for EMACS_SOCKET_NAME in ${SERVER_DIRECTORY}/${DESKTOP_GROUP_NUMBER}.*; do
-      echo "attempting to connect to ${EMACS_SOCKET_NAME}"
-      if ! EMACS_SOCKET_NAME=${EMACS_SOCKET_NAME} "${REAL_EMACSCLIENT}" \
-                          "--socket-name=${EMACS_SOCKET_NAME}" "${@}"; then
-          echo "warn: failed to talk to emacs server at ${EMACS_SOCKET_NAME}"
-          RET=$?
-      fi
-  done
-  exit $RET
+if test -z "${DESKTOP_GROUP_NUMBER:-}"; then
+    "${REAL_EMACSCLIENT}" "${@}"
 fi
 
-"${REAL_EMACSCLIENT}" "${@}"
+SERVER_DIRECTORY=$(emacs -q --batch  \
+                         --eval "(require 'server)"  \
+                         --eval "(print server-socket-dir)" \
+                         2>/dev/null  \
+                       | tail -1 | tr -d '"')
+RET=0
+for EMACS_SOCKET_NAME in ${SERVER_DIRECTORY}/${DESKTOP_GROUP_NUMBER}.*; do
+    echo "attempting to connect to ${EMACS_SOCKET_NAME}"
+    if ! EMACS_SOCKET_NAME=${EMACS_SOCKET_NAME} "${REAL_EMACSCLIENT}" \
+         "--socket-name=${EMACS_SOCKET_NAME}" "${@}"; then
+        echo "warn: failed to talk to emacs server at ${EMACS_SOCKET_NAME}"
+        RET=$?
+    fi
+done
+exit $RET
