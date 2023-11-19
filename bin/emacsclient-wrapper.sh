dde2a84e4e50d92f184ae565ff419351938b0d5d
Ernesto Alfonso <erjoalgo@gmail.com>
2023-11-19 17:37:22 -0500

    try to ensure emacsclient works with sudo<F5>

--- bin/emacsclient-wrapper.sh
+++ bin/emacsclient-wrapper.sh
@@ -15,10 +15,18 @@ SERVER_DIRECTORY=$(emacs -q --batch  \
                          2>/dev/null  \
                        | tail -1 | tr -d '"')
 RET=0
-for EMACS_SOCKET_NAME in ${SERVER_DIRECTORY}/${DESKTOP_GROUP_NUMBER}.*; do
+
+for EMACS_SOCKET_NAME in /tmp/emacs*/* ; do
     echo "attempting to connect to ${EMACS_SOCKET_NAME}"
-    if ! EMACS_SOCKET_NAME=${EMACS_SOCKET_NAME} "${REAL_EMACSCLIENT}" \
-         "--socket-name=${EMACS_SOCKET_NAME}" "${@}"; then
+    SERVER_USER_ID=$(grep -Po "(?<=/emacs)[0-9]+(?=/)" <<< "${EMACS_SOCKET_NAME}")
+    SERVER_USER=$(id -un "${SERVER_USER_ID}")
+    TRAMP_PREFIX=""
+    if ! sudo -u "${SERVER_USER}" bash test -r "${@}"; then
+        TRAMP_PREFIX="/sudo:root@$(hostname):"
+    fi
+
+    if ! sudo -u "${SERVER_USER}" env EMACS_SOCKET_NAME=${EMACS_SOCKET_NAME} "${REAL_EMACSCLIENT}" \
+         "--socket-name=${EMACS_SOCKET_NAME}" "${TRAMP_PREFIX}${@}"; then
         echo "warn: failed to talk to emacs server at ${EMACS_SOCKET_NAME}"
         RET=$?
     fi
