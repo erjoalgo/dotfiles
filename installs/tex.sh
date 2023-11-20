ffb6a42e971173fe1bfd73af9241a1aa79246123
Ernesto Alfonso <erjoalgo@gmail.com>
2023-11-19 19:34:06 -0500

    fix unable to find texlive-latex-extra-utils

--- installs/tex.sh
+++ installs/tex.sh
@@ -2,5 +2,4 @@
 
 sudo apt-get install -y texlive-fonts-recommended \
      texlive-latex-recommended texlive-latex-extra \
-     texlive-bibtex-extra biber texlive-font-utils \
-     texlive-latex-extra-utils
+     texlive-bibtex-extra biber texlive-font-utils
