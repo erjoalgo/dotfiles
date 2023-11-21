5a5dd6bb4b20b8e98d281517ec4fbd8a28b98b94
Ernesto Alfonso <erjoalgo@gmail.com>
2023-11-21 10:43:07 -0500

    switch to https sources

--- installs/tor.sh
+++ installs/tor.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 CODENAME=$(lsb_release -c | cut -d: -f2 | tr -d '[:space:]')
 sudo tee /etc/apt/sources.list.d/torproject.list <<EOF
-deb     http://deb.torproject.org/torproject.org ${CODENAME} main
-deb-src     http://deb.torproject.org/torproject.org ${CODENAME} main
+deb     https://deb.torproject.org/torproject.org ${CODENAME} main
+deb-src     https://deb.torproject.org/torproject.org ${CODENAME} main
 EOF
 
 TOR_KEY_URL=https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc
