be07e8d8977df123c7602e557b0f991abe5134de
Ernesto Alfonso <erjoalgo@gmail.com>
2023-11-21 08:22:42 -0500

    add link to creality slicer

--- installs/3d-printing.sh
+++ installs/3d-printing.sh
@@ -35,3 +35,6 @@ fi
 octoprint-cli files list
 
 pip install trimesh
+
+# creality slicer
+# https://file2-cdn.creality.com/file/1946156fb58ee2564ae71424e44c05a3/Creality_Print-v3.11.1-Ubutu-x86_64-Release.AppImage?spm=..page_1995737.download_support_two_1.1
