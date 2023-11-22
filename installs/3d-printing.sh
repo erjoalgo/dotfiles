c5553e9eecf9d5eb2633df35795765175b3ecfbf
Ernesto Alfonso <erjoalgo@gmail.com>
2023-11-22 17:42:28 -0500

    pip install pystl

--- installs/3d-printing.sh
+++ installs/3d-printing.sh
@@ -38,3 +38,5 @@ pip install trimesh
 
 # creality slicer
 # https://file2-cdn.creality.com/file/1946156fb58ee2564ae71424e44c05a3/Creality_Print-v3.11.1-Ubutu-x86_64-Release.AppImage?spm=..page_1995737.download_support_two_1.1
+
+pip install git+https://github.com/erjoalgo/pystl
