ec9ace45e0179b369be6ebce9405db1cef4f16d8
Ernesto Alfonso <erjoalgo@gmail.com>
2023-12-15 19:51:37 -0500

    symlink to own InputAutoCfg.ini to make updating easier

--- installs/n64.sh
+++ installs/n64.sh
@@ -5,33 +5,14 @@ set -euo pipefail
 sudo apt-get install -y mupen64plus-qt
 
 INI=/usr/share/games/mupen64plus/InputAutoCfg.ini
+ORIG="${INI}.orig"
+if ! test -e "${ORIG}"; then
+    sudo cp "${INI}" "${ORIG}"
+fi
 
-sudo sed -i 's/^[[]Keyboard]$/[Original Keyboard]/g' "${INI}"
+OWN="${HOME}/git/dotfiles/inits/InputAutoCfg.ini"
 
-sudo insert-text-block '; c7ffec9c-8d9c-4044-a8e9-9b736d0bba2e-nintendo-64-keyboard' \
-     -b ${INI} <<EOF
-[Keyboard]
-plugged = True
-mouse = False
-DPad R = key(100)
-DPad L = key(97)
-DPad D = key(115)
-DPad U = key(119)
-Start = key(13)
-Z Trig = key(304)
-B Button = key(99)
-A Button = key(120)
-C Button R = key(103)
-C Button L = key(100)
-C Button D = key(102)
-C Button U = key(114)
-R Trig = key(122)
-L Trig = key(97)
-Mempak switch = key(44)
-Rumblepak switch = key(46)
-X Axis = key(276,275)
-Y Axis = key(273,274)
-EOF
+sudo ln -sf "${OWN}" "${INI}"
 
 sudo chgrp input $(which mupen64plus)
 sudo chmod g+s $(which mupen64plus)
