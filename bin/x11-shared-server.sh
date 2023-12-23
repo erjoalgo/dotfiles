7b8e24391fa1648f8294c18acf21e3aa58f6a2d1
Ernesto Alfonso <erjoalgo@gmail.com>
2023-12-23 12:11:00 -0500

    update x11-vnc command

--- bin/x11-shared-server.sh
+++ bin/x11-shared-server.sh
@@ -5,4 +5,7 @@ set -euo pipefail
 # from https://serverfault.com/a/146581/170017
 # and related comment
 XAUTH=$(ps wwwwaux | grep -Po '(?<=[-]auth )[^ ]+')
-x11vnc -xauth "${XAUTH}" -display :0 -shared
+while true; do
+    x11vnc -xauth "${XAUTH}" -display :0 -shared -noxrecord -noxfixes -noxkb
+    sleep 1
+done
