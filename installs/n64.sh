b4e2523ec815cc58185ac596c91a0f95f28a15e3
Ernesto Alfonso <erjoalgo@gmail.com>
2023-12-24 20:55:40 -0500

    apply all possible subscreen delay fixes
    it was removed from mupen64plus-core in:
    
    https://github.com/mupen64plus/mupen64plus-core/pull/125
    https://github.com/mupen64plus/mupen64plus-core/pull/125/files
    
    but not added to mupen64plus.ini, causing more pain for users

--- installs/n64.sh
+++ installs/n64.sh
@@ -16,3 +16,23 @@ sudo ln -sf "${OWN}" "${INI}"
 
 sudo chgrp input $(which mupen64plus)
 sudo chmod g+s $(which mupen64plus)
+
+sudo insert-text-block '; f62dcec4-1169-4ba2-b47f-6048170a50ad-apply-all-possible-subscren-delay-fixes' \
+     /usr/share/games/mupen64plus/mupen64plus.ini <<EOF
+[57A9719AD547C516342E1A15D5C28C3D]
+GoodName=Legend of Zelda, The - Ocarina of Time (U) (V1.2) [!]
+CRC=693BA2AE B7F14E9F
+Players=1
+SaveType=SRAM
+Rumble=Yes
+; Subscreen Delay Fixes
+Cheat0=801D860B 0002
+Cheat1=801D864B 0002
+Cheat2=801D8F4B 0002
+Cheat3=801D8F8B 0002
+Cheat4=801DA5CB 0002
+Cheat5=801DA78B 0002
+Cheat6=801DAE8B 0002
+Cheat7=801DB74B 0002
+Cheat8=801DB78B 0002
+EOF
