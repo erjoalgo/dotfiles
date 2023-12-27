708eff37fcd7ae6020bbb761008b0baedde83512
Ernesto Alfonso <erjoalgo@gmail.com>
2023-12-27 21:21:06 +0000

    run compose up before setting up USB connection

--- installs/octoprint/octoprint.sh
+++ installs/octoprint/octoprint.sh
@@ -8,6 +8,8 @@ cd "$(realpath $(dirname "${BASH_SOURCE[0]}"))"
 # pip3 install pyserial --break-system-packages
 # python3 -m serial.tools.miniterm
 
+docker compose up -d
+
 PRINTER_DEVICE=$(echo /dev/ttyUSB*)
 
 if ! test -e ${PRINTER_DEVICE}; then
@@ -19,8 +21,6 @@ sudo chown $(whoami):$(whoami) ${PRINTER_DEVICE}
 insert-text-block '# e1b3e892-395b-4db2-9061-dc8559f2e009-set-up-device-path' .env <<EOF
 PRINTER_DEVICE=${PRINTER_DEVICE}
 EOF
-
-docker compose up -d
 
 docker compose logs -f
 
