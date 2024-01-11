876c9d39b46026ec4c4f36ca0753dc726e682f20
Ernesto Alfonso <erjoalgo@gmail.com>
2024-01-10 19:09:23 -0500

    new file: installs/firetv-remote.sh

---
+++ installs/firetv-remote.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+REPO=${HOME}/git/firetv-remote
+
+test -d "${REPO}" || git clone github.com/erjoalgo/firetv-remote "${REPO}"
+cd "${REPO}"
+git checkout insignia
+git pull --ff-only
+
+
+install-systemd-service.sh -u firetv <<EOF
+[Unit]
+Description=redshift
+After=default.target
+
+[Service]
+ExecStart=$(which npm) run start
+SyslogIdentifier=firetv-remote
+Restart=always
+Type=simple
+Environment=PATH=$PATH:$(dirname $(which npm))
+WorkingDirectory=${REPO}
+
+[Install]
+WantedBy=default.target
+EOF
+
+
+# Local Variables:
+# mode: sh-mode
+# End:
