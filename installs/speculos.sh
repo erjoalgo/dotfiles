b9e2d51b7ac6b049487d31d97b7b35392ce8caf4
Ernesto Alfonso <erjoalgo@gmail.com>
2024-01-20 06:55:50 -0500

    new file: installs/speculos.sh

---
+++ installs/speculos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+sudo apt install -y \
+    cmake gcc-arm-linux-gnueabihf libc6-dev-armhf-cross gdb-multiarch \
+    python3-pyqt5 python3-construct python3-flask-restful python3-jsonschema \
+    python3-mnemonic python3-pil python3-pyelftools python3-requests \
+    qemu-user-static libvncserver-dev
+
+URL=https://github.com/LedgerHQ/speculos/
+REPO=${HOME}/git/$(basename "${URL}")
+test -d "${REPO}" || git clone "${URL}" "${REPO}"
+cd "${REPO}"
+
+cmake -B build/ -DCMAKE_BUILD_TYPE=Debug -DWITH_VNC=1 -S .
+
+pip3 install .
