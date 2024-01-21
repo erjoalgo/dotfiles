8b174a7c4811be7d9ff7c2769c45ade5061063fb
Ernesto Alfonso <erjoalgo@gmail.com>
2024-01-21 14:26:03 -0500

    provide simpler CLI for listing find extensions. refactor to use bash arrays

--- bin/adb-pull-rm.sh
+++ bin/adb-pull-rm.sh
@@ -6,8 +6,12 @@ function usage  {
     echo "usage: $(basename ${0}) <ANDROID_FILE_PATH>"
 }
 
+ADB_CMD=("adb")
+if test -n "${ADB_DEVICE:-}"; then
+    ADB_CMD+=("-s" "${ADB_DEVICE}")
+fi
+
 PATH_PREFIX=""
-ADB_CMD=${ADB_CMD:-"adb"}
 
 FILE="${*}"
 echo ${FILE}
@@ -15,7 +19,7 @@ ANDROID_PATH="${PATH_PREFIX}/$(tr -d '\r' <<< ${FILE})"
 LOCAL_NAME=$(sed 's|//*|/|g'  <<< "$(pwd)/${ANDROID_PATH}")
 
 # TODO make sure 'adb shell ls' succeeds
-OUT=$(${ADB_CMD} shell ls "\"${ANDROID_PATH}\"")
+OUT=$(${ADB_CMD[@]} shell ls "\"${ANDROID_PATH}\"")
 echo "${ANDROID_PATH}"
 
 if grep -i "no such file\|does not exist" <<< "${OUT}"; then
@@ -25,17 +29,17 @@ fi
 
 if ! test -e "${LOCAL_NAME}"; then
     mkdir -p $(dirname "${LOCAL_NAME}")
-    ${ADB_CMD} pull "${ANDROID_PATH}" "${LOCAL_NAME}"
+    ${ADB_CMD[@]} pull "${ANDROID_PATH}" "${LOCAL_NAME}"
 fi
 
 MD5_HDD=$(md5sum "${LOCAL_NAME}" | tr ' ' '\t' | cut -f1)
-MD5_ANDROID=$(${ADB_CMD} shell md5sum "\"${ANDROID_PATH}\"" | tr ' ' '\t' | cut -f1)
+MD5_ANDROID=$(${ADB_CMD[@]} shell md5sum "\"${ANDROID_PATH}\"" | tr ' ' '\t' | cut -f1)
 
 if test "${MD5_ANDROID}" = "${MD5_HDD}"; then
-    ${ADB_CMD} shell rm "\"${ANDROID_PATH}\""
+    ${ADB_CMD[@]} shell rm "\"${ANDROID_PATH}\""
     echo "success!"
 else
     echo "unexpected checksum for ${ANDROID_PATH}: ${MD5_ANDROID} vs ${MD5_HDD}"
-    echo "${ANDROID_PATH}\t$(${ADB_CMD} shell du -b ${ANDROID_PATH})"
+    echo "${ANDROID_PATH}\t$(${ADB_CMD[@]} shell du -b ${ANDROID_PATH})"
     echo "${LOCAL_NAME}\t$(du -b ${LOCAL_NAME})"
 fi
--- bin/android-find-pull-rm.sh
+++ bin/android-find-pull-rm.sh
@@ -1,13 +1,33 @@
 #!/bin/bash -x
 
-ADB_CMD=adb
-while getopts "n:s:h" OPT; do
+ADB_CMD=(adb)
+FIND_CMD=()
+FIND_ARGS=()
+ADB_DEVICE=
+
+while getopts "s:p:e:nh" OPT; do
     case ${OPT} in
-        n)
-            NO_PULL=true
+        p)
+            # find path
+            FIND_CMD=(find ${OPTARG})
             ;;
         s)
-            ADB_CMD="adb -s ${OPTARG}"
+            # device
+            export ADB_DEVICE=${OPTARG}
+            ADB_CMD+=("-s" "${OPTARG}")
+            ;;
+        e)
+            for EXT in $(sed 's/,/ /g' <<< ${OPTARG}); do
+                if test "${#FIND_ARGS[@]}" -gt 0; then
+                    FIND_ARGS+=("-o")
+                fi
+                FIND_ARGS+=("-name")
+                FIND_ARGS+=("*.${EXT}")
+            done
+            ;;
+        n)
+            # test mode
+            NO_PULL=true
             ;;
         h)
             # example to pull videos and images:
@@ -19,12 +39,15 @@ while getopts "n:s:h" OPT; do
 done
 shift $((OPTIND -1))
 
-${ADB_CMD} shell command -v find
+test -n "${FIND_PATH:-}"
+FIND_CMD+=("${FIND_ARGS[@]}")
 
-export ADB_CMD
+"${ADB_CMD[@]}" shell command -v find
 
 if test -z "${NO_PULL:-}"; then
-    ${ADB_CMD} shell "find ${@}" | xargs -L1 $(which adb-pull-rm.sh)
+    PULL=(xargs -L1 $(which adb-pull-rm.sh))
 else
-    ${ADB_CMD} shell "find ${@}"
+    PULL=(cat)
 fi
+
+"${ADB_CMD[@]}" shell "${FIND_CMD[@]}" | "${PULL[@]}"
