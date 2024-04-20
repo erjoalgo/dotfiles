130c207b35f333f90488134f7615935b3da16dc8
Ernesto Alfonso <erjoalgo@gmail.com>
2024-04-20 12:31:58 -0400

    fix missing space

--- bin/emulator-run.sh
+++ bin/emulator-run.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 ANDROID_DEFAULT_DEVICE=${ANDROID_DEFAULT_DEVICE:-$(emulator -list-avds | head -1)}
-emulator-avd "${ANDROID_DEFAULT_DEVICE}"
+emulator -avd "${ANDROID_DEFAULT_DEVICE}"
