a0b646b2c344d5f2c2b1fb990921360e535196cd
Ernesto Alfonso <erjoalgo@gmail.com>
2024-05-12 12:36:46 -0400

    new file: bin/chrome.sh

---
+++ bin/chrome.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+if ! EXE=$(which chromium-browser chrome google-chrome chromium | sort | head -1); then
+   command -v "${EXE}"
+fi
+
+ENABLE_FEATURES="AsyncDns"
+if test $(redshift -p | grep -Po '(?<=Period: ).*') != Daytime; then
+    ENABLE_FEATURES+=,WebContentsForceDark
+fi
+
+ARGS=( \
+       --high-dpi-support=1  \
+           --force-device-scale-factor=2 \
+           --disable-pings \
+           --enable-gpu-rasterization \
+           --enable-remote-extensions \
+           --force-device-scale-factor=2 \
+           --high-dpi-support=1 \
+           --load-extension=/usr/share/chromium/extensions/acofndgbcimipbpeoplfjcapdbebbmca.json,/usr/share/chromium/extensions/chemannjcbmbebomonaldbcjkmobopno.json,/usr/share/chromium/extensions/dbepggeogbaibhgnhhndojpepiihcmeb.json,/usr/share/chromium/extensions/eibefbdcoojolecpoehkpmgfaeapngjk.json,/usr/share/chromium/extensions/fimgfedafeadlieiabdeeaodndnlbhid.json,/usr/share/chromium/extensions/lfpjkncokllnfokkgpkobnkbkmelfefj.json,/usr/share/chromium/extensions/nigigpmchbpkjjgncmpiggfnikllldlh.json,/usr/share/chromium/extensions/related.json \
+           --media-router=0 \
+           --no-default-browser-check \
+           --show-component-extension-options \
+           --restart \
+           --flag-switches-begin \
+           --enable-features=${ENABLE_FEATURES} \
+           --flag-switches-end
+)
+
+${EXE} ${ARGS[@]}
