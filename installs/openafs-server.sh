29b7e56b1f7c6a46f45772e86d0b7b7ad354f9d2
Ernesto Alfonso <erjoalgo@gmail.com>
2024-05-17 11:55:24 -0400

    fmt

--- installs/openafs-server.sh
+++ installs/openafs-server.sh
@@ -2,5 +2,9 @@
 
 set -euo pipefail
 
-sudo apt-get install -y openafs-client openafs-modules-dkms openafs-krb5 \
-     openafs-fileserver openafs-dbserver
+sudo apt-get install -y  \
+     openafs-client  \
+     openafs-modules-dkms  \
+     openafs-krb5 \
+     openafs-fileserver  \
+     openafs-dbserver
