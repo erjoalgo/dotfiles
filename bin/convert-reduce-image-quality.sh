93407b87031b4a69db506fc1ac79d4439c4ab289
Ernesto Alfonso <erjoalgo@gmail.com>
2024-05-27 21:24:47 -0400

    provide getopt menu

--- bin/convert-reduce-image-quality.sh
+++ bin/convert-reduce-image-quality.sh
@@ -4,6 +4,23 @@ set -euo pipefail
 
 QUALITY=${QUALITY:-15}
 
+while getopts "hl:" OPT; do
+    case ${OPT} in
+    l)
+        QUALITY=${OPTARG}
+        ;;
+    h)
+        less "$0"
+        exit 0
+        ;;
+    *)
+        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+
 for INPUT in $*; do
     OUTPUT=$(sed 's/[.]/.low-quality./' <<< "${INPUT}")
     convert "${INPUT}" -quality ${QUALITY} "${OUTPUT}"
