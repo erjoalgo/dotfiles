7b9705055aebb5eef1917cec64322fd506377945
Ernesto Alfonso <erjoalgo@gmail.com>
2024-06-04 16:43:59 -0400

    install pyudevs as a user service

--- installs/pyudevs.sh
+++ installs/pyudevs.sh
@@ -6,7 +6,7 @@ cd "$(realpath $(dirname "${BASH_SOURCE[0]}"))"
 
 python3 -m pip install pyudev
 
-install-systemd-service.sh pyudevs <<EOF
+install-systemd-service.sh -u pyudevs <<EOF
 [Unit]
 Description=Run custom udev scripts via pyudev
 Requires=systemd-udevd.service
@@ -15,12 +15,10 @@ StartLimitInterval=0
 
 [Service]
 ExecStart=$(realpath $(pwd)/../bin/pyudevs.py)
-User=$(whoami)
 Restart=always
 RestartSec=5
 Environment=PATH=$PATH:$(pwd)/bin
 
 [Install]
-WantedBy=graphical.target
-
+WantedBy=default.target
 EOF
