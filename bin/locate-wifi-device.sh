984205d02448794b81f417f5c0b8627fbad8341f
Ernesto Alfonso <erjoalgo@gmail.com>
2024-06-08 22:10:17 -0400

    new file:   bin/locate-wifi-device.sh

---
+++ bin/locate-wifi-device.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+CMD=(sudo horst
+while getopts "i:c:e:h" OPT; do
+    case ${OPT} in
+    i)
+        CMD+=(-i ${OPTARG})
+        ;;
+    c)
+        CMD+=(-c ${OPTARG})
+        ;;
+    e)
+        CMD+=(-e ${OPTARG})
+        ;;
+    h)
+        less "$0"
+        exit 0
+        ;;
+    *)
+        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+${CMD[@]}
