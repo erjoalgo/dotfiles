da8d4154a7130c36ca6b3593f6c49a9f180714c9
Ernesto Alfonso <erjoalgo@gmail.com>
2024-06-09 13:08:53 -0400

    new file: bin/bt-scan-trust-pair-connect.sh

---
+++ bin/bt-scan-trust-pair-connect.sh
@@ -0,0 +1,83 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+while getopts "m:h" OPT; do
+    case ${OPT} in
+    m)
+        MAC=${OPTARG}
+        ;;
+    h)
+        less "$0"
+        exit 0
+        ;;
+    *)
+        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+BLE_COMPANY_IDS=/usr/share/bt/manufacturers.yaml
+if ! test -e "${BLE_COMPANY_IDS}"; then
+    sudo mkdir -p $(dirname "${BLE_COMPANY_IDS}")
+    URL=https://bitbucket.org/bluetooth-SIG/public/raw/main/assigned_numbers/company_identifiers/company_identifiers.yaml
+    curl "${URL}" | sudo tee "${BLE_COMPANY_IDS}"
+fi
+
+
+if test -z "${MAC:-}"; then
+    TMPFILE=$(mktemp)
+    expect -f - <<EOF &
+set timeout -1
+log_file ${TMPFILE}
+# exp_internal 1
+
+spawn bluetoothctl
+send -- "scan on\r"
+expect {
+    -re { Device ([A-Z0-9:]+)} {
+        set device \$expect_out(1,string);
+        send -- "info \$device\r"
+        exp_continue
+    }
+    -re {ManufacturerData Key: (0x[0-9a-zA-Z]+)} {
+        set key \$expect_out(1,string);
+        puts "key is \$key"
+        set status [catch {exec grep -A1 -F "\$key" "$BLE_COMPANY_IDS"} output]
+        puts "grep output: \$output"
+        exp_continue
+    }
+
+}
+EOF
+    SCAN_PID=$!
+    trap "kill $SCAN_PID" SIGINT
+    wait ${SCAN_PID} || true
+    trap - SIGINT
+
+    DEVICES=$(grep -Po '(?<=Device )[A-Z0-9:]+.*' < "${TMPFILE}" | sort | uniq)
+    # rm "${TMPFILE}"
+    echo "select device: " 1>&2
+    OLDIFS=$IFS
+    IFS=$'\n'
+    select LINE in $(sed 's/\r$//' <<< ${DEVICES}); do
+        MAC=$(cut -f1 -d' ' <<< "${LINE}")
+        echo "${MAC}"
+        break
+    done
+    IFS=$OLDIFS
+fi
+
+
+expect -f - <<EOF
+set timeout -1
+spawn bluetoothctl
+send -- "scan off\r"
+send -- "trust ${MAC}\r"
+expect -- "trust succeeded"
+send -- "pair ${MAC}\r"
+expect -- "Pairing successful"
+send -- "connect ${MAC}\r"
+expect -- "Connection successful"
+EOF
