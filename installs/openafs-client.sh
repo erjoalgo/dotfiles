65d3283ba6307c9f19f393f7677f2bb65775fedd
Ernesto Alfonso <erjoalgo@gmail.com>
2024-06-20 13:49:43 -0400

    add missing krb5-config dep and configuration

--- installs/openafs-client.sh
+++ installs/openafs-client.sh
@@ -54,10 +54,25 @@ openafs-client  openafs-client/dynroot  select  Yes
 openafs-client  openafs-client/fakestat boolean true
 # Run Openafs client now and at boot?
 openafs-client  openafs-client/run-client       boolean true
+
+
+# Add locations of default Kerberos servers to /etc/krb5.conf?
+krb5-config     krb5-config/add_servers boolean false
+# for internal use
+krb5-config     krb5-config/add_servers_realm   string  ${THIS_CELL}
+# Default Kerberos version 5 realm:
+krb5-config     krb5-config/default_realm       string  ${THIS_CELL^^}
+
+# Administrative server for your Kerberos realm:
+# krb5-config     krb5-config/admin_server        string
+# Kerberos servers for your realm:
+# krb5-config     krb5-config/kerberos_servers    string
+# For internal use only
+krb5-config     krb5-config/read_conf   boolean true
 EOF
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openafs-{modules-dkms,client,krb5}  \
-     krb5-user libpam-krb5
+     krb5-{config,user} libpam-krb5
 
 sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure openafs-client
 
