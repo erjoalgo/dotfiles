582af350f2dd6632742d9e4801efeff19a6083e5
Ernesto Alfonso <erjoalgo@gmail.com>
2024-07-31 16:11:01 -0400

    fmt

--- lisp/startup-apps.lisp
+++ lisp/startup-apps.lisp
@@ -8,8 +8,8 @@
 
 (defun startup-apps-run ()
   (let ((hostname-specific-apps-pathname
-         (merge-pathnames (format nil ".~A-apps.lisp" (SB-UNIX:UNIX-GETHOSTNAME))
-                          (user-homedir-pathname))))
+          (merge-pathnames (format nil ".~A-apps.lisp" (SB-UNIX:UNIX-GETHOSTNAME))
+                           (user-homedir-pathname))))
     (if (probe-file hostname-specific-apps-pathname)
         (load hostname-specific-apps-pathname)
         (start-apps-default))))
