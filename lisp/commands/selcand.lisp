1c2f597f9a01479e2921cd61c27c15a1b9b87f19
Ernesto Alfonso <erjoalgo@gmail.com>
2024-08-03 12:10:35 -0400

    fmt

--- lisp/commands/selcand.lisp
+++ lisp/commands/selcand.lisp
@@ -15,17 +15,17 @@
   (setf chars (or chars *default-hints*))
   (assert candidates)
   (loop
-     with hint-width = (ceiling (log (length candidates) (length chars)))
-     with hints = '("")
-     for wi below hint-width do
-       (setf hints
-             (loop for c across chars
-                append (mapcar (lambda (rest)
-                                 (format nil "~C~A" c rest))
-                               hints)))
-     finally (return (loop for hint in hints
-                        for cand in candidates
-                        collect (cons hint cand)))))
+    with hint-width = (ceiling (log (length candidates) (length chars)))
+    with hints = '("")
+    for wi below hint-width do
+      (setf hints
+            (loop for c across chars
+                  append (mapcar (lambda (rest)
+                                   (format nil "~C~A" c rest))
+                                 hints)))
+    finally (return (loop for hint in hints
+                          for cand in candidates
+                          collect (cons hint cand)))))
 
 (defun select (&key
                  candidates
@@ -42,35 +42,36 @@
   (assert (or candidates hints-candidates) nil on-empty-error)
   (assert (alexandria:xor candidates hints-candidates))
   (let* ((hints-cands
-          (cond
-            (hints-candidates hints-candidates)
-            (no-hints
-             (mapcar
-              (lambda (cand)
-                (cons (stumpwm::string-trim " " (funcall stringify-fn cand)) cand))
-              candidates))
-            (t
-             (loop for (hint . cand) in (hints candidates)
-                collect
-                  (cons
-                   (stumpwm::string-trim
-                    " "
-                    (concatenate
-                     'string hint
-                     ") " (funcall stringify-fn cand)))
-                   cand)))))
+           (cond
+             (hints-candidates hints-candidates)
+             (no-hints
+              (mapcar
+               (lambda (cand)
+                 (cons (stumpwm::string-trim " " (funcall stringify-fn cand)) cand))
+               candidates))
+             (t
+              (loop for (hint . cand) in (hints candidates)
+                    collect
+                    (cons
+                     (stumpwm::string-trim
+                      " "
+                      (concatenate
+                       'string hint
+                       ") " (funcall stringify-fn cand)))
+                     cand)))))
          (choices (mapcar #'car hints-cands))
          (prompt (if (not display-candidates) prompt
-                     (format nil "~A~%~{~A~^~%~}~%~% " prompt
+                     (format nil "~A~%~{~A~^~%~}~%~% "
+                             prompt
                              (if (eq display-candidates :include-values)
                                  hints-cands
                                  choices))))
          (hint-selected
-          (cond
-            ((and autoselect-if-single (null (cdr choices))) (car choices))
-            ((and read-char-if-possible
-                  (eq 1 (reduce #'max (mapcar #'length choices))))
-             (loop
+           (cond
+             ((and autoselect-if-single (null (cdr choices))) (car choices))
+             ((and read-char-if-possible
+                   (eq 1 (reduce #'max (mapcar #'length choices))))
+              (loop
                 for i below 3
                 as choice = (progn
                               (stumpwm:message "~D ~A" i prompt)
@@ -81,14 +82,14 @@
                                        (throw 'error "Abort."))))
                 while (not (member choice choices :test #'equal))
                 finally
-                  (progn
-                    (stumpwm::unmap-all-message-windows)
-                    (return choice))))
-            (t (or (stumpwm:completing-read
-                    (stumpwm:current-screen) prompt choices
-                    :initial-input (when initial-candidate-index
-                                     (nth initial-candidate-index choices)))
-                   (throw 'error "Abort.")))))
+                   (progn
+                     (stumpwm::unmap-all-message-windows)
+                     (return choice))))
+             (t (or (stumpwm:completing-read
+                     (stumpwm:current-screen) prompt choices
+                     :initial-input (when initial-candidate-index
+                                      (nth initial-candidate-index choices)))
+                    (throw 'error "Abort.")))))
          (cand (cdr (assoc hint-selected hints-cands :test #'equal))))
     (assert (or (null hint-selected) cand))
     cand))
