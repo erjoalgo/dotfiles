2b0a41eb7daeaf08e9f2e35ea26beaf9f7a15677
Ernesto Alfonso <erjoalgo@gmail.com>
2024-11-05 11:35:46 -0500

    add bindings to repeatedly send IR buttons to a specific device

--- lisp/top-map-bindings.lisp
+++ lisp/top-map-bindings.lisp
@@ -496,10 +496,17 @@
     *press-ir-button-map*
     `(
       ("v" ,(define-tv-buttons "VIZIO" *ir-buttons-vizio-tv*))
+      ("V" ,(run-keymap-repeatedly-command *ir-buttons-vizio-tv*))
+
       ("d" ,(define-tv-buttons "DYNEX" *ir-buttons-dynex-tv*))
+
       ("t" ,(define-tv-buttons "TCL" *ir-buttons-tcl-tv*))
+      ("T" ,(run-keymap-repeatedly-command *ir-buttons-tcl-tv*))
+
       ("s" ,(define-tv-buttons "SOUNDBAR" *ir-buttons-vizio-soundbar*))
-      ("a" ,(define-tv-buttons "AC" *ir-buttons-ac*))))
+
+      ("a" ,(define-tv-buttons "AC" *ir-buttons-ac*))
+      ("A" ,(run-keymap-repeatedly-command *ir-buttons-ac*))))
 
 
 (set-prefix-key (kbd "F19"))
