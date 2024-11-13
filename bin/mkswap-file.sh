afb71884bc248eadb2b551272046f3495e420a26
Ernesto Alfonso <erjoalgo@gmail.com>
2024-11-13 13:32:59 -0500

    fix swap file permissions before running mkswap

--- bin/mkswap-file.sh
+++ bin/mkswap-file.sh
@@ -31,7 +31,7 @@ CNT=$(expr 1024 \* ${GB})
 BS=1024k
 
 dd if=/dev/zero of=${SWAP} bs=${BS} count=${CNT}
-mkswap -f ${SWAP}
 chmod 0600 ${SWAP}
+mkswap -f ${SWAP}
 echo "${SWAP} none swap sw 0 0 " >> /etc/fstab
 swapon ${SWAP}
