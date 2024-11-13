650575ee27ff7277cd22ce43db92f432b9d517a4
Ernesto Alfonso <erjoalgo@gmail.com>
2024-11-13 13:33:24 -0500

    search for slicer exe under ~/bin

--- bin/openscad.sh
+++ bin/openscad.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
-EXE=$(which ${HOME}/Downloads/OpenSCAD-2024.08.05.ai20224-x86_64.AppImage  \
+EXE=$(which ${HOME}/bin/OpenSCAD*AppImage  \
+            ${HOME}/Downloads/OpenSCAD*AppImage \
             openscad | sed "1!d")
+
 command -v "${EXE}"
 
 QT_SCALE_FACTOR=3 "${EXE}" "$@"
