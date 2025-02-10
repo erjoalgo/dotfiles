bfcacb5d225a55392f1ff286dc823a6b133351c2
Ernesto Alfonso <erjoalgo@gmail.com>
2025-02-10 13:27:47 -0500

    default to the printer default destination

--- bin/lpr-print.sh
+++ bin/lpr-print.sh
@@ -2,13 +2,31 @@
 
 set -euo pipefail
 
+while getopts "hd" OPT; do
+    case ${OPT} in
+    d)
+        PRINTER=$(lpstat -p -d | grep -Po '(?<=system default destination: ).*')
+        ;;
+    h)
+        less "$0"
+        exit 0
+        ;;
+    *)
+        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
 PDF=${1} && shift
 
-PRINTERS=$(lpstat -t | grep -Po "^[^ ]+?(?= accepting requests)")
+if test -z "${PRINTER:-}"; then
+    PRINTERS=$(lpstat -t | grep -Po "^[^ ]+?(?= accepting requests)")
+    echo "${PRINTERS}" 1>&2
+    select PRINTER in ${PRINTERS}; do
+        break
+    done
+fi
 
-echo "${PRINTERS}" 1>&2
-select PRINTER in ${PRINTERS}; do
-    break
-done
 
-lpr ${PDF} -P ${PRINTER} -o outputorder=reverse
+lpr "${PDF}" -P "${PRINTER}" -o outputorder=reverse
