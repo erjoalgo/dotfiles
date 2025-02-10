f717d0ffed242d49d818ba2016d2b6f63075ea65
Ernesto Alfonso <erjoalgo@gmail.com>
2025-02-10 14:15:14 -0500

    new file: bin/find-last-modified-file-fast.sh

---
+++ bin/find-last-modified-file-fast.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# from https://stackoverflow.com/a/7448828/1941755
+
+# echo $0 ${*}
+FIND_CMD=(find)
+while getopts "he:d:" OPT; do
+    case ${OPT} in
+    e)
+        EXT="${OPTARG}"
+        ;;
+    d)
+        DIR=$(realpath "${OPTARG}")
+        test -d "${DIR}"
+        FIND_CMD+=("${DIR}")
+        ;;
+    c)
+        CMIN=${OPTARG}
+        ;;
+    h)
+        less "$0"
+        exit 0
+        ;;
+    *)
+        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+if test -n "${EXT:-}"; then
+    FIND_CMD+=(-name "*.${EXT}")
+fi
+
+CMIN=${CMIN:-100}
+
+FIND_CMD+=(-cmin -${CMIN} -type f \
+                 -exec stat --format '%Y :%y %n' "{}" \;)
+
+"${FIND_CMD[@]}" | \
+    sort -nr |  \
+    cut -d: -f2- |  \
+    cut -f4- -d' '
