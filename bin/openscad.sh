a181ef053444d229cfe84b30e0e20df38eb94fbf
Ernesto Alfonso <erjoalgo@gmail.com>
2025-02-10 13:30:16 -0500

    fix failing on the $(which ...) command

--- bin/openscad.sh
+++ bin/openscad.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 EXE=$(which ${HOME}/bin/OpenSCAD*AppImage  \
             ${HOME}/Downloads/OpenSCAD*AppImage \
-            openscad | sed "1!d")
+            openscad | sed "1!d") || true
 
 command -v "${EXE}"
 
