3ddda46a7690c18a5b5e65bbcae7240b5ed146c3
Ernesto Alfonso <erjoalgo@gmail.com>
2025-03-09 21:11:24 -0400

    support specifying a cursor directory instead of a name

--- installs/x-cursor.sh
+++ installs/x-cursor.sh
@@ -2,17 +2,34 @@
 
 set -euo pipefail
 
-CURSOR_THEME=${CURSOR_THEME:-noche-diamante}
+while getopts "d:h" OPT; do
+    case ${OPT} in
+    d)
+        CURSOR_DIR=${OPTARG}
+        ;;
+    h)
+        less "$0"
+        exit 0
+        ;;
+    *)
+        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+if test -z "${CURSOR_DIR:-}"; then
+    CURSOR_DIR=${1} && shift
+fi
+
+test -d "${CURSOR_DIR}"
+
+CURSOR_THEME=$(grep -Po '(?<=Inherits=).*' "${CURSOR_DIR}/cursor.theme")
 
 DEST="/usr/share/icons/${CURSOR_THEME}"
 
 if ! test -d "${DEST}"; then
-    git submodule init
-    git submodule update
-    git submodule sync
-    SRC=$(realpath ../submodules/${CURSOR_THEME})
-    test -d "${SRC}"
-    sudo cp -r "${SRC}" "${DEST}"
+    sudo cp -r "${CURSOR_DIR}" "${DEST}"
 fi
 
 sudo sed -i "s/^\(Inherits=\).*/\1${CURSOR_THEME}/" \
