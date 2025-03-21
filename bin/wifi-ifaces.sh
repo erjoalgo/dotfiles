510379d9a70ba87694735bc2fe90828b436f909d
Ernesto Alfonso <erjoalgo@gmail.com>
2025-03-21 13:25:13 -0400

    deleted:    bin/wifi-ifaces.sh

--- bin/wifi-ifaces.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# ip link | grep -Po '^wlan[0-9]|wlp[^:]*'
-
-find /sys/class/net -mindepth 1 -maxdepth 1 |  \
-    xargs -L 1 basename |  \
-    grep -Po '^(wlan|wlp|wlx).*'
