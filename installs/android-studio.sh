98a97a54eeb28658cb6ec30a51f052c8f1fe7cbc
Ernesto Alfonso <erjoalgo@gmail.com>
2025-04-03 18:47:50 -0400

    make it easier to update android studio from an archive

--- installs/android-studio.sh
+++ installs/android-studio.sh
@@ -2,15 +2,74 @@
 
 set -euo pipefail
 
+# default URL
 URL="https://dl.google.com/dl/android/studio"
 URL+="/ide-zips/3.4.1.0/android-studio-ide-183.5522156-linux.tar.gz"
 
-BASE=$(basename "${URL}")
-LOCAL=${HOME}/Downloads/${BASE}
-test -e "${LOCAL}" || curl "${URL}" -o "${LOCAL}"
+while getopts "u:z:d:rh" OPT; do
+    case ${OPT} in
+        u)
+            URL=${OPTARG}
+            ;;
+        z)
+            ARCHIVE_FILE=${OPTARG}
+            ;;
+        d)
+            EXTRACTED_DIRECTORY=${OPTARG}
+            ;;
+        r)
+            REPLACE=true
+            ;;
+        h)
+            less "$0"
+            exit 0
+            ;;
+        *)
+            echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+
+function fetch-archive-file {
+    if ! test -e "${ARCHIVE_FILE:-}" 1>&2; then
+        ARCHIVE_FILE=${HOME}/Downloads/$(basename "${URL}")
+        curl "${URL}" -o "${ARCHIVE_FILE}" 1>&2
+    else
+        echo "WARN: skipping archive download"
+    fi
+    echo "${ARCHIVE_FILE}"
+}
+
+function extract-directory {
+    DEST=${1} && shift
+    if test "${REPLACE:-}" = true; then
+        if test -d "${DEST}"; then
+            echo "WARN: removing old extracted directory"
+        fi
+        rm -rf "${DEST}"
+    fi
+    if ! test -d "${DEST:-}"; then
+        ARCHIVE_FILE=$(fetch-archive-file)
+        test -e "${ARCHIVE_FILE}"
+        DEST=${DEST:-${HOME}/Downloads/$(basename "${ARCHIVE_FILE}")}
+        tar -C "$(dirname ${SRC})" -axvf "${DEST}"
+    fi
+}
 
 SRC=${HOME}/src/android-studio
-test -d "${SRC}" || tar -C "$(dirname ${SRC})" -axvf "${LOCAL}"
+if test -d "${SRC}" -a "${REPLACE:-}" != true; then
+    echo "WARN: skipping replace of ${SRC}"
+else
+    rm -rf "${SRC}"
+    if test -d "${EXTRACTED_DIRECTORY:-}"; then
+        mv "${EXTRACTED_DIRECTORY}/android-studio" "${SRC}"
+    else
+        extract-directory "${SRC}"
+    fi
+fi
+
 cd "${SRC}"
 
 BIN_D="${SRC}/bin"
