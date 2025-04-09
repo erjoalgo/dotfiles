0705498615f3031dbfc93e5c8e27c486a7c59a8a
Ernesto Alfonso <erjoalgo@gmail.com>
2025-04-09 09:56:26 -0400

    new file: bin/convert-replace-color.sh

---
+++ bin/convert-replace-color.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+FUZZ_PERCENT=10
+ORIGINAL_COLOR=white
+
+while getopts "i:o:c:w:z:h" OPT; do
+    case ${OPT} in
+    i)
+        INPUT=${OPTARG}
+        ;;
+    o)
+        OUTPUT=${OPTARG}
+        ;;
+    c)
+        # new color
+        COLOR=${OPTARG}
+        ;;
+    w)
+        # old color
+        ORIGINAL_COLOR=white
+        ;;
+    z)
+        FUZZ_PERCENT=${OPTARG}
+        ;;
+    h)
+        less "$0"
+        exit 0
+        ;;
+    *)
+        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+
+test -e "${INPUT}"
+
+convert "${INPUT}" -fuzz "${FUZZ_PERCENT}%"  \
+        -fill "${COLOR}" \
+        -opaque "${ORIGINAL_COLOR}" \
+        -flatten \
+        "${OUTPUT}"
+
+qimgv "${OUTPUT}"
