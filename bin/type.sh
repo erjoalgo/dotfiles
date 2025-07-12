113a03b30fc04bb144828e4223c100e27234bfda
Ernesto Alfonso <erjoalgo@gmail.com>
2025-07-12 12:59:23 -0400

    use the /automation to support usage without X

--- bin/type.sh
+++ bin/type.sh
@@ -2,22 +2,32 @@
 
 set -euo pipefail
 
-DELAY_MS=${DELAY_MS:-50}
 PORT=5000
 . ${HOME}/afs/home/${USER}/.profile || true
-_USE_XDOTOOL=true
 
-while getopts "p:s:d:h" OPT; do
+MODE=curl
+DELAY_MS=${DELAY_MS:-50}
+
+while getopts "p:s:d:xcah" OPT; do
     case ${OPT} in
-    p)
-        PASS_ID=${OPTARG}
-        ;;
     s)
         SEED_FILE=${OPTARG}
         ;;
     # optional
     d)
         DELAY_MS=${OPTARG}
+        ;;
+    i)
+	INTERACTIVE=true;
+	;;
+    x)
+        MODE=xdotool
+        ;;
+    c)
+        MODE=curl
+        ;;
+    a)
+        MODE=automation
         ;;
     h)
         less "$0"
@@ -30,8 +40,14 @@ while getopts "p:s:d:h" OPT; do
 done
 shift $((OPTIND -1))
 
+PASS_ID=${1:-} && shift || true
 
 function validate-args {
+    if test -z "${PASS_ID:-}"; then
+        URL=$(current-url)
+        PASS_ID=$(url-to-pass-id "${URL}")
+        echo "${PASS_ID}" >&2
+    fi
     test -n "${PASS_ID:-}"
     test -e "${SEED_FILE:-}"
     EXT="${SEED_FILE##*.}"
@@ -42,32 +58,73 @@ function validate-args {
 function press {
     KEY=${1} && shift
     TIMES=${1:-1}
-    echo "moving ${KEY} ${TIMES} times"
+    echo "moving ${KEY} ${TIMES} times" >&2
     DELAY_SECS=$(bc <<< "scale=2; ${DELAY_MS} / 1000")
     case "${KEY}" in
         Left)
             BUTTON="left"
+            BUTTON_NUMBERS=(1)
             ;;
         Right)
             BUTTON="right"
+            BUTTON_NUMBERS=(2)
             ;;
         Left+Right)
             BUTTON="both"
+            BUTTON_NUMBERS=(1 2)
             ;;
         *)
-            echo "unknown key: ${KEY}"
+            echo "unknown key: ${KEY}" >&2
             return ${LINENO}
             ;;
     esac
     for _ in $(seq "${TIMES}"); do
-        if test "${_USE_XDOTOOL:-}" = true; then
+        if test "${MODE:-}" = xdotool; then
+            echo "DDEBUG TRACE type.sh t3ve"
             xdotool key "${KEY}"
             sleep "${DELAY_SECS}"
-        else
+        elif test "${MODE:-}" = curl; then
             curl -d '{"action":"press-and-release"}'  \
                  "http://127.0.0.1:5000/button/${BUTTON}"
+        elif test "${MODE:-}" = automation; then
+            FIRST=true
+            for PRESS in true false; do
+                for BUTTON_NUMBER in "${BUTTON_NUMBERS[@]}"; do
+                    TEXT=$'\n'
+                    if test "${FIRST}" = true; then
+                        TEXT=$'\n\n'
+                        FIRST=false
+                    else
+                        TEXT=$'\n';
+                    fi
+                    TEXT+="[\"button\", ${BUTTON_NUMBER}, ${PRESS}]"
+                    ACTIONS+=("${TEXT}")
+                done
+            done
+        else
+            echo "unsupported input mode: ${MODE}" >2&
+            exit ${LINENO}
         fi
     done
+}
+
+function exec-automation {
+    ACTION_DATA=$(cat <<EOF
+{
+  "version": 1,
+  "rules": [
+    {
+    "conditions": [[ "seen", false ]],
+      "actions": [
+        [ "setbool", "seen", true ],
+        $(IFS=','; echo "${ACTIONS[*]}")
+      ]
+    }
+  ]
+}
+EOF
+)
+    curl -i  "http://127.0.0.1:5000/automation" -d "${ACTION_DATA}"
 }
 
 function left {
@@ -84,7 +141,7 @@ function both {
 
 function section {
     # echo -e "\n\n"
-    echo ${*}
+    echo ${*} >&2
 }
 
 # https://unix.stackexchange.com/questions/92447/
@@ -113,7 +170,7 @@ function move-to-char {
 
 function type-string {
     PASS_ID=${1} && shift
-    echo "typing ${PASS_ID}"
+    echo "typing ${PASS_ID}" >&2
     LAST_CHAR='a'
     for CHAR in $(grep -o . <<< "${PASS_ID}"); do
         if  grep '[a-z]' <<< "${CHAR}"; then
@@ -126,7 +183,7 @@ function type-string {
 EOF
 )
             if ! OFFSET=$(grep -boF "${CHAR}" <<< "${STRING}" | cut -f1 -d:); then
-                echo "unsupported character: ${CHAR}"
+                echo "unsupported character: ${CHAR}" >&2
                 exit ${LINENO}
             fi
             move-to-char "a" "${LAST_CHAR}"
@@ -165,10 +222,19 @@ function select-menu {
             both
             right "${INDEX}" # scan to the password
             both
+            ;;
+        capture-pass)
+            ORIG_MODE=$MODE
+            export MODE=curl
+            right
+            left
             PASS=$(last-event-text)
             right # view page 2/2 of the password
             PASS+=$(last-event-text)
+            PASS=$(tail -1 <<< "${PASS}")
+            export MODE=$ORIG_MODE
             export PASS
+            echo "${PASS}"
             ;;
         qwerty)
             # assume we are at "new password -> keyboard selection"
@@ -203,7 +269,7 @@ function select-menu {
             right 9
             ;;
         *)
-            echo "unknown select-menu option"
+            echo "unknown select-menu option" >&2
             exit ${LINENO}
             ;;
     esac
@@ -237,7 +303,7 @@ function build-speculos-app {
            make DEBUG=1
     test -e "${ELF}"
     test -e "${APP_ELF}"
-    echo "pulled $(pwd)/$(basename ${ELF})"
+    echo "pulled $(pwd)/$(basename ${ELF})" >&2
     popd
 }
 
@@ -320,7 +386,7 @@ function ledger-menu {
         focus)
             section "focusing ledger"
             until wmctrl -a "Ledger Nano S"; do
-                echo "waiting for ledger window"
+                echo "waiting for ledger window" >&2
                 sleep 1
             done
             ;;
@@ -330,7 +396,7 @@ function ledger-menu {
             ;;
 
         *)
-            echo "unknown select-menu option"
+            echo "unknown select-menu option" >&2
             exit ${LINENO}
             ;;
     esac
@@ -347,12 +413,36 @@ function clipboard {
     x-service-curl /clipboard -d "${TEXT}"
 }
 
+function current-url {
+    curl localhost:19615/tabs/current/url
+}
+
+function url-to-pass-id {
+    URL=${1} && shift
+    HOST=$(URL=$URL python - <<EOF
+import os
+from urllib.parse import urlparse
+
+uri = os.environ["URL"]
+result = urlparse(uri)
+host = result.netloc.split('@')[-1]
+pass_id = host.split('.')[-2]
+print(pass_id)
+EOF
+)
+    echo "${HOST}"
+}
+
 function main {
     validate-args
     ledger-menu build-all
+
     ledger-menu kill
     ledger-menu start "${SEED_FILE}"
-    ledger-menu focus
+
+    if test "${MODE}" = xdotool; then
+        ledger-menu focus
+    fi
     select-menu skip-disclaimer
     select-menu qwerty
     select-menu new-pass
@@ -360,10 +450,17 @@ function main {
     type-string "${PASS_ID}"
     select-menu checkmark
     select-menu show-pass
+    if test "${MODE}" = automation; then
+        exec-automation
+    fi
+    # PASS=$(select-menu capture-pass)
+    select-menu capture-pass
     test -n "${PASS:-}"
     clipboard "${PASS}"
     log-pass-id
-    ledger-menu browse
+    if test "${BROWSE:-}" = true; then
+        ledger-menu browse
+    fi
     echo "${PASS}"
 }
 
