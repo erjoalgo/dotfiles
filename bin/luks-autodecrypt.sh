7088e1435ac3cc7d0926b9123b4a247598006329
Ernesto Alfonso <erjoalgo@gmail.com>
2025-09-11 12:00:30 -0400

    fix bash redirection syntax

--- bin/luks-autodecrypt.sh
+++ bin/luks-autodecrypt.sh
@@ -17,7 +17,7 @@ shift $((OPTIND -1))
 function select-luks-partition {
     sudo lsblk -o PATH,NAME,MODEL,SIZE,FSTYPE,MOUNTPOINT |  \
         grep --color=always -E '.*/crypto_LUKS|$' 1>&2
-    echo "select LUKS partition" > &2
+    echo "select LUKS partition" 1>&2
     select LUKS_PARTITION in $(sudo lsblk -o PATH,FSTYPE | grep -F crypto_LUKS | cut -f1 -d' '); do
         echo "${LUKS_PARTITION}"
         break
