90bf05df37b28590b69012a0b7d2ceac600d3b67
Ernesto Alfonso <erjoalgo@gmail.com>
2025-10-09 12:22:13 -0400

    also invert matplotlib figures

--- bin/invert-colors-toggle.sh
+++ bin/invert-colors-toggle.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 if pgrep compton; then
     pkill -9 compton
 else
-    compton --invert-color-include 'class_g="Chromium" || class_g="Zathura"'
+    compton --invert-color-include 'class_g="Chromium" || class_g="Zathura" || class_g="matplotlib"'
 fi
