698866383d2fc1df19ecef41042d7120cd7e258f
Ernesto Alfonso <erjoalgo@gmail.com>
2025-12-13 22:15:44 -0500

    update script to install app-passwords

--- installs/speculos-app-passwords.sh
+++ installs/speculos-app-passwords.sh
@@ -6,5 +6,19 @@ URL=https://github.com/LedgerHQ/app-passwords
 REPO=${HOME}/git/$(basename "${URL}")
 test -d "${REPO}" || git clone "${URL}" "${REPO}"
 cd "${REPO}"
+git pull --ff-only
 
-speculos-app-build.sh
+pwd
+
+ELF=$(pwd)/build/nanos/bin/app.elf
+
+test -e "${ELF}"
+
+docker pull ghcr.io/ledgerhq/ledger-app-builder/ledger-app-dev-tools:latest
+
+docker run --rm -it  \
+       -v $(dirname "${ELF}"):/speculos/apps \
+       --publish 5000:5000  \
+       ghcr.io/ledgerhq/speculos \
+       --display headless  \
+       apps/app.elf
