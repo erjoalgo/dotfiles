fdec91c8e4b3dd30306c11f187cc938e0d6b2c44
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-03 13:23:06 -0500

    for convenience, symlink .xinitrc.profile to the home directory

--- installs/migrate-xinitrc.sh
+++ installs/migrate-xinitrc.sh
@@ -9,6 +9,8 @@ if test -L "${XINITRC}"; then
     unlink "${XINITRC}"
 fi
 
+ln -sf "${XINITRC_PROFILE}" "${HOME}/"
+
 insert-text-block '# 9604209d-759e-4e6a-be37-770e1ddd39d6-source-xinitrc-profile' \
                   "${XINITRC}" <<EOF
 source "${XINITRC_PROFILE}"
