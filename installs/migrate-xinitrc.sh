efbffb47b58dd9226fc7e6a8900ac78e24d74b12
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-03 13:20:55 -0500

    use variables

--- installs/migrate-xinitrc.sh
+++ installs/migrate-xinitrc.sh
@@ -2,11 +2,14 @@
 
 set -euo pipefail
 
-if test -L ~/.xinitrc; then
-    unlink ~/.xinitrc
+XINITRC=${HOME}/.xinirc
+XINITRC_PROFILE=${HOME}/git/dotfiles/inits/.xinitrc.profile
+
+if test -L "${XINITRC}"; then
+    unlink "${XINITRC}"
 fi
 
 insert-text-block '# 9604209d-759e-4e6a-be37-770e1ddd39d6-source-xinitrc-profile' \
-                  ~/.xinitrc <<EOF
-source ${HOME}/git/dotfiles/inits/.xinitrc.profile
+                  "${XINITRC}" <<EOF
+source "${XINITRC_PROFILE}"
 EOF
