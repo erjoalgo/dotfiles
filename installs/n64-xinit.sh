49a0e59d8faef1caf2353e7c13a90147bbe665b2
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-05 11:48:11 -0500

    fix reference to the wrong files

--- installs/n64-xinit.sh
+++ installs/n64-xinit.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-sed -i 's/^exec/ exec/g' ~/.xinit
+sed -i 's/^exec/ exec/g' ~/.xinitrc
 
-insert-text-block '# b1a3634c-5085-4ace-b25a-472248bd3d54-n64-xinit'  \
-                  ~/.xinit <<EOF
+insert-text-block '# b1a3634c-5085-4ace-b25a-472248bd3d54-n64-xinitrc'  \
+                  ~/.xinitrc <<EOF
+. ${HOME}/.xinitrc.profile
 exec mupen64plus /afs/asus.erjoalgo.com/public/n64/roms/current-rom.z64
 EOF
 
