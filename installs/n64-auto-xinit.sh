76454562fbbc3c6b65f6185c5d7b6fc1b82a86fb
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-05 18:16:09 -0500

    ensure n64 is installed

--- installs/n64-auto-xinit.sh
+++ installs/n64-auto-xinit.sh
@@ -14,6 +14,8 @@ insert-text-block '# b1a3634c-5085-4ace-b25a-472248bd3d54-n64-xinitrc'  \
 exec mupen64plus /afs/asus.erjoalgo.com/public/n64/roms/current-rom.z64
 EOF
 
+./n64.sh
+
 ./autologin-on-tty.sh
 
 if systemctl list-units --full -all | grep lightdm.service; then
