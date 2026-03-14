ebae9c177d7622b43d4477e6922b610c1ec37ec5
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-13 23:52:05 -0400

    avoid issues with the required python dependencies and virtual environment

--- installs/tvpower/dmesg-listener.sh
+++ installs/tvpower/dmesg-listener.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 sudo dmesg -W | while IFS=$'\n' read -r LINE; do
     echo "Processing line: $LINE"
     if grep -F "now running tv power..." <<< "${LINE}"; then
-        /home/ealfonso/.stumpwmrc.d/bin/ir-remote.py -b LG_POWER,TCL_ENTER
+        # /home/ealfonso/.stumpwmrc.d/bin/ir-remote.py -b LG_POWER,TCL_ENTER
+        curl localhost:2727/LG_POWER,TCL_ENTER
     fi
 done
