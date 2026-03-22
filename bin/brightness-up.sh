7e994afaad9333f23b0a0d71cbe8409668decdfa
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-21 21:10:30 -0400

    maximize blue light

--- bin/brightness-up.sh
+++ bin/brightness-up.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 ir-remote.py -bTCL_BRIGHTNESS_UP,LG_BRIGHTNESS_UP
 
-redshift -l28.562871:-81.210339 -ov -PO 6000
+redshift -l28.562871:-81.210339 -ov -PO 25000
 
 pkill picom || true
