81dfbe816bd9509765de9d9c5b03a763414037dd
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-21 21:09:04 -0400

    fix arg outside of range

--- bin/brightness-down.sh
+++ bin/brightness-down.sh
@@ -2,7 +2,7 @@
 
 ir-remote.py -bTCL_BRIGHTNESS_DOWN,LG_BRIGHTNESS_DOWN &
 
-redshift -l28.562871:-81.210339 -ov -PO 500
+redshift -l28.562871:-81.210339 -ov -PO 1000
 
 pgrep picom || invert-colors-toggle.sh
 
