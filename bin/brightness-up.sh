e9fbdb72e36bfa6ba746988afc30a978c97cf1a3
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-23 11:40:32 -0400

    run the quicker commands first

--- bin/brightness-up.sh
+++ bin/brightness-up.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-ir-remote.py -bTCL_BRIGHTNESS_UP,LG_BRIGHTNESS_UP
-
 redshift -l28.562871:-81.210339 -ov -PO 25000
 
 pkill picom || true
+
+ir-remote.py -bTCL_BRIGHTNESS_UP,LG_BRIGHTNESS_UP
