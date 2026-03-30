8f7b2b6a906ba56543c4881e2dfce356e4134c53
Ernesto Alfonso <erjoalgo@gmail.com>
2026-03-30 19:00:10 -0400

    install pavucontrol on boot-x.sh

--- installs/boot-x.sh
+++ installs/boot-x.sh
@@ -149,7 +149,7 @@ done
 popd
 
 sudo apt-get purge -y pipewire-pulse
-sudo apt-get install -y pipewire-jack
+sudo apt-get install -y pipewire-jack pavucontrol
 
 sudo apt-get purge -y xdg-desktop-portal-gtk
 
