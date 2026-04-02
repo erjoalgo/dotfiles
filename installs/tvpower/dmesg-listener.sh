3fa4682e726e572f2884bdcf363e8602ace87d08
Ernesto Alfonso <erjoalgo@gmail.com>
2026-04-02 16:22:02 -0400

    prefer ir-remote in case the service is down

--- installs/tvpower/dmesg-listener.sh
+++ installs/tvpower/dmesg-listener.sh
@@ -6,7 +6,10 @@ set -euo pipefail
 sudo dmesg -W | while IFS=$'\n' read -r LINE; do
     echo "Processing line: $LINE"
     if grep -F "now running tv power..." <<< "${LINE}"; then
-        # /home/ealfonso/.stumpwmrc.d/bin/ir-remote.py -b LG_POWER,TCL_ENTER
-        curl localhost:2727/LG_POWER,TCL_ENTER
+        IR_REMOTE=/home/ealfonso/.stumpwmrc.d/bin/ir-remote.py
+        if ! { "${IR_REMOTE}" -b LG_POWER,TCL_ENTER ||
+                   curl -s localhost:2727/LG_POWER,TCL_ENTER ; } ; then
+            echo "IR remote failed!"
+        fi
     fi
 done
