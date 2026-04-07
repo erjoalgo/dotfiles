41c77369f34b46657f3846ed705a162ff47129d6
Ernesto Alfonso <erjoalgo@gmail.com>
2026-04-07 10:17:43 -0400

    also toggle emacs theme on brightness up/down

--- bin/brightness-down.sh
+++ bin/brightness-down.sh
@@ -4,5 +4,7 @@ redshift -l28.562871:-81.210339 -ov -PO 1000
 
 pgrep picom || invert-colors-toggle.sh
 
+emacsclient-wrapper.sh -e '(redshift-load-dark-theme)'
+
 ir-remote.py -bTCL_BRIGHTNESS_DOWN,LG_BRIGHTNESS_DOWN &
 
