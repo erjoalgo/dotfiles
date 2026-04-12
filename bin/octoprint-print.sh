95e33e9f10686418d4aa46e8e2f848619bfef181
Ernesto Alfonso <erjoalgo@gmail.com>
2026-04-12 13:22:01 -0400

    ensure octoprint-cli is setup correctly

--- bin/octoprint-print.sh
+++ bin/octoprint-print.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
-while getopts "hg:" OPT; do
+while getopts "s:g:h" OPT; do
     case ${OPT} in
+        s)
+            SERVER_ADDRESS=${OPTARG}
+            ;;
         g)
             GCODE=${OPTARG}
             ;;
@@ -14,6 +17,46 @@ while getopts "hg:" OPT; do
     esac
 done
 shift $((OPTIND -1))
+
+if ! command -v octoprint-cli; then
+    pip install octoprint-cli
+fi
+
+function write-ini {
+    INI=${HOME}/.config/octoprint-cli.ini
+    SERVER_ADDRESS=${1} && shift
+    API_KEY=${1} && shift
+
+    insert-text-block '# 0754c16b-b5aa-447d-81d6-b4fa758a56bc-octoprint-cli-config' \
+                      "${INI}" <<EOF
+[server]
+;Set OctoPrint server address and x-api-key
+ServerAddress = ${SERVER_ADDRESS}
+ApiKey = ${API_KEY}
+
+[preferences]
+;Set if the program uses colored or formatted text, this setting is turned off on windows due to cmd and powershell limitations
+FormattedText = true
+;Set if the program should check for updates
+UpdateCheck = true
+
+[printer]
+;Set maximum temperature that printer can be set to
+MaxExtruderTemp = 250
+MaxBedTemp = 85
+EOF
+}
+
+if test -n "${SERVER_ADDRESS:-}"; then
+    x-www-browser "${SERVER_ADDRESS}"
+    read -p"enter octorpint API key: " API_KEY
+    write-ini "${SERVER_ADDRESS}" "${API_KEY}"
+fi
+
+if ! octoprint-cli files list; then
+    echo "failed to connect? maybe update server address"
+    exit ${LINENO}
+fi
 
 test -e "${GCODE}"
 
