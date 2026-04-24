c58f4237b1e8033646d7e56c7737384ea7cb87ad
Ernesto Alfonso <erjoalgo@gmail.com>
2026-04-23 23:26:08 -0400

    don't wait for emacsclient

--- bin/brightness-up.sh
+++ bin/brightness-up.sh
@@ -6,6 +6,6 @@ redshift -l28.562871:-81.210339 -ov -PO 25000
 
 pkill picom || true
 
-emacsclient-wrapper.sh -e '(redshift-unload-dark-theme)'
+emacsclient-wrapper.sh -e '(redshift-unload-dark-theme)' &
 
 ir-remote.py -bTCL_BRIGHTNESS_UP,LG_BRIGHTNESS_UP
