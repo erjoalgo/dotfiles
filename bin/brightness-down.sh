df97f3d30ce6c1b81199f0d226641a5b848d3bda
Ernesto Alfonso <erjoalgo@gmail.com>
2026-05-16 22:44:10 -0400

    put invert-colors in the background

--- bin/brightness-down.sh
+++ bin/brightness-down.sh
@@ -2,7 +2,8 @@
 
 redshift -l28.562871:-81.210339 -ov -PO 1000
 
-pgrep picom || invert-colors-toggle.sh
+pgrep picom || invert-colors-toggle.sh &
+
 
 emacsclient-wrapper.sh -e '(redshift-load-dark-theme)' &
 
