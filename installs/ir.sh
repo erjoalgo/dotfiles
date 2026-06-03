e34c757c46282f5e10f82c1545c7797c9ad8669a
Ernesto Alfonso <erjoalgo@gmail.com>
2026-06-03 13:28:09 -0400

    point to the irremote script exe

--- installs/ir.sh
+++ installs/ir.sh
@@ -4,14 +4,12 @@ set -euo pipefail
 
 cd "$(realpath $(dirname "${BASH_SOURCE[0]}"))"
 
-pip3 install broadlink
-
 install-systemd-service.sh ir <<EOF
 [Unit]
 Description=Run the IR/RF http service
 
 [Service]
-ExecStart=$(which python3) $(realpath $(pwd)/../bin/ir-remote.py ) --port 2727
+ExecStart=$(which irremote)  --port 2727
 Restart=always
 RestartSec=5
 Environment=PATH=$PATH:$(realpath $(pwd)/../bin)
