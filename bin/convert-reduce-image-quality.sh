9fb9df819df5cc6f8301dcff425242ac1eef31d7
Ernesto Alfonso <erjoalgo@gmail.com>
2026-07-04 20:31:20 -0400

    fmt

--- bin/convert-reduce-image-quality.sh
+++ bin/convert-reduce-image-quality.sh
@@ -6,16 +6,16 @@ QUALITY=${QUALITY:-15}
 
 while getopts "hl:" OPT; do
     case ${OPT} in
-    l)
-        QUALITY=${OPTARG}
-        ;;
-    h)
-        less "$0"
-        exit 0
-        ;;
-    *)
-        echo "unrecognized flag: ${OPT}" && exit ${LINENO}
-        ;;
+        l)
+            QUALITY=${OPTARG}
+            ;;
+        h)
+            less "$0"
+            exit 0
+            ;;
+        *)
+            echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+            ;;
     esac
 done
 shift $((OPTIND -1))
