e7f88a2be9f7f62a90a25140fec4644e7d6b33e3
Ernesto Alfonso <erjoalgo@gmail.com>
2026-09-02 15:12:51 -0400

    new file: bin/rip-cdrom.sh

---
+++ bin/rip-cdrom.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+DEVICE=${DEVICE:-/dev/sr0}
+
+while getopts "d:p:" OPT; do
+    case ${OPT} in
+        d)
+            DEVICE=${OPTARG}
+            ;;
+        p)
+            OUTPUT_PATH=${OPTARG}
+            ;;
+        h)
+            less "$0"
+            exit 0
+            ;;
+        *)
+            echo "unrecognized flag: ${OPT}" && exit ${LINENO}
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+test -n "${OUTPUT_PATH:-}"
+
+TMP=$(mktemp -d)
+
+sudo mount -oloop "${DEVICE}" "${TMP}"
+
+mkdir -p "${OUTPUT_PATH}"
+
+rsync -arv "${TMP}" "${OUTPUT_PATH}"
