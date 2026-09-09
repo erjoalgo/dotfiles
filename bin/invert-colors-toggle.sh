f0be05069e8e27fda941f037123147b01bb37899
Ernesto Alfonso <erjoalgo@gmail.com>
2026-09-09 09:14:16 -0400

    move long picom window class list to a conf file

--- bin/invert-colors-toggle.sh
+++ bin/invert-colors-toggle.sh
@@ -10,35 +10,7 @@ if pgrep -af picom | grep -v defunct; then
     pkill -9 picom
     emacsclient-wrapper.sh -e '(redshift-unload-dark-theme)' &
 else
-    ARG=""
-    for CLASS in  \
-        Chromium \
-            matplotlib  \
-            OpenSCAD  \
-            Zathura \
-            Xournal \
-            TelegramDesktop \
-            qimgv \
-            Gsmartcontrol \
-            firefox-esr \
-            Pavucontrol \
-            pavucontrol \
-            Creality \
-            Wireshark \
-            'Tor Browser' \
-            Evince \
-            PrusaSlicer \
-            Cura \
-            cura \
-            Com.github.xournalpp.xournalpp \
-        ; do
-        if test -n "${ARG:-}"; then
-            ARG+=" || "
-        fi
-        ARG+="class_g=\"${CLASS}\""
-    done
-    # 'class_g="Chromium" || class_g="Zathura" || class_g="matplotlib"'
     emacsclient-wrapper.sh -e '(redshift-load-dark-theme)' &
-    picom --invert-color-include "${ARG}" &
+    picom --config ~/git/dotfiles/inits/picom.conf &
     disown
 fi
